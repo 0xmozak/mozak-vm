243c1dd6f0575a17e9bc2362a7baebec649e14b3
Vivek Pandya <vivekvpandya@gmail.com>
2023-04-19 18:15:49 +0530

    Add basic parsing of TEXT segment from ELF

--- vm/src/elf.rs
+++ vm/src/elf.rs
@@ -61,16 +61,33 @@ impl Program {
         }
         Ok(Program { entry, image })
     }
+
+    fn load_u32(&self, addr: u32) -> Result<&u32> {
+        self.image
+            .get(&addr)
+            .ok_or(anyhow!("Address invalid for image"))
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::elf::Program;
+    use crate::{elf::Program, opcode::decode};
     #[test]
     fn check() {
         let elf = std::fs::read("src/test.elf").unwrap();
         let max_mem_size = 1 * 1024 * 1024 * 1024; // 1GB
         let program = Program::load_elf(&elf, max_mem_size);
         assert!(program.is_ok());
+        let program = program.unwrap();
+        let mut pc = program.entry;
+        loop {
+            let word = program.load_u32(pc);
+            if word.is_err() {
+                break;
+            }
+            let opcode = decode(*word.unwrap());
+            println!("OpCode: {:?}", opcode);
+            pc += 4;
+        }
     }
 }
--- vm/src/lib.rs
+++ vm/src/lib.rs
@@ -1,3 +1,4 @@
 pub mod elf;
+pub mod opcode;
 
 extern crate alloc;
