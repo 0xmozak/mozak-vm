9b89183f633d7665aa53ce2cf782c81b53d78afc
Matthias Goergens <matthias.goergens@gmail.com>
2023-05-26 14:31:01 +0800

    clean up

--- vm/build.rs
+++ vm/build.rs
@@ -1,10 +1,6 @@
 use std::process::Command;
 
-// Example custom build script.
 fn main() {
-    // Tell Cargo that if the given file changes, to rerun this build script.
-    // println!("cargo:rerun-if-changed=src/hello.c");
-    // Use the `cc` crate to build a C file and statically link it.
     println!("cargo:rerun-if-changed=../Dockerfile");
     println!("cargo:rerun-if-changed=tests/testdata/.testdata_generated_from_this_commit");
     if !std::path::Path::new("tests/testdata/.testdata_generated_from_this_commit").exists() {
