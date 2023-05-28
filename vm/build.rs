5805c9d1f940ee6bf8f1a98abff118a0dd6fd486
Matthias Goergens <matthias.goergens@gmail.com>
2023-05-28 12:07:38 +0800

    Remodel

--- vm/build.rs
+++ vm/build.rs
@@ -1,11 +1,18 @@
 use std::process::Command;
 
 fn main() {
+    let stamp_actual = "tests/testdata/.testdata_generated_from_this_commit";
+    let stamp_wanted = "tests/create_testdata/generate_testdata_from_this_commit";
+
     println!("cargo:rerun-if-changed=../Dockerfile");
-    println!("cargo:rerun-if-changed=tests/testdata/.testdata_generated_from_this_commit");
-    if !std::path::Path::new("tests/testdata/.testdata_generated_from_this_commit").exists() {
+    println!("cargo:rerun-if-changed={}", stamp_actual);
+    println!("cargo:rerun-if-changed={}", stamp_wanted);
+
+    if std::fs::read_to_string(stamp_wanted).unwrap()
+        != std::fs::read_to_string(stamp_actual).unwrap_or_default()
+    {
         assert!(Command::new("docker")
-            .args(["buildx", "build", "--output", "tests/testdata", ".."])
+            .args(["buildx", "build", "--output", "tests/testdata", "tests/create_testdata/"])
             .status()
             .unwrap()
             .success())
