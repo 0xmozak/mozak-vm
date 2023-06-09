58cd3dd9ca4211ed7de917967d705ffcdd7f04fb
Vivek Pandya <vivekvpandya@gmail.com>
2023-06-09 12:02:50 +0530

    chore(proof-system): derive Default for MozakStark

--- circuits/src/stark/mozak_stark.rs
+++ circuits/src/stark/mozak_stark.rs
@@ -2,17 +2,9 @@ use plonky2::{field::extension::Extendable, hash::hash_types::RichField};
 
 use crate::cpu::cpu_stark::CpuStark;
 
-#[derive(Clone)]
+#[derive(Clone, Default)]
 pub struct MozakStark<F: RichField + Extendable<D>, const D: usize> {
     pub cpu_stark: CpuStark<F, D>,
-}
-
-impl<F: RichField + Extendable<D>, const D: usize> Default for MozakStark<F, D> {
-    fn default() -> Self {
-        Self {
-            cpu_stark: CpuStark::default(),
-        }
-    }
 }
 
 pub(crate) const NUM_TABLES: usize = 1;
