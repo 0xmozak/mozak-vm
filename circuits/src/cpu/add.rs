82770cb4e80dd1608113eb63a4f9ba90d800af24
Matthias Goergens <matthias.goergens@gmail.com>
2023-06-12 09:20:20 +0800

    multiply, not add

--- circuits/src/cpu/add.rs
+++ circuits/src/cpu/add.rs
@@ -1,4 +1,5 @@
 use plonky2::field::packed::PackedField;
+use plonky2::field::types::Field;
 use starky::constraint_consumer::ConstraintConsumer;
 
 use super::columns::*;
@@ -12,6 +13,6 @@ pub(crate) fn eval_packed_generic<P: PackedField>(
         .constraint(lv[COL_S_ADD] * (lv[COL_DST_VALUE] - (lv[COL_OP1_VALUE] + lv[COL_OP2_VALUE])));
 
     // pc ticks up
-    let incr_wo_branch = P::ONES + P::ONES + P::ONES + P::ONES;
+    let incr_wo_branch = P::Scalar::from_canonical_u32(4) * P::ONES;
     yield_constr.constraint((lv[COL_S_ADD]) * (nv[COL_PC] - lv[COL_PC] + incr_wo_branch));
 }
