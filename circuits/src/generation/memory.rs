4f28f98d731b33b91102ebf208cbe6fe4dfab164
Sai Deng <dengsai@hotmail.com>
2023-06-12 12:08:37 +0800

    add functions for memory trace generation

---
+++ circuits/src/generation/memory.rs
@@ -0,0 +1,75 @@
+use mozak_vm::instruction::Op;
+use mozak_vm::vm::Row;
+use plonky2::hash::hash_types::RichField;
+
+use crate::memory::columns as mem_cols;
+use crate::memory::trace::{
+    get_memory_inst_addr, get_memory_inst_clk, get_memory_inst_op, get_memory_load_inst_value,
+    get_memory_store_inst_value,
+};
+
+// Suppose that the memory trace comes in the order of the instruction address
+pub fn generate_memory_trace<F: RichField>(
+    step_rows: Vec<Row>,
+) -> [Vec<F>; mem_cols::NUM_MEM_COLS] {
+    let trace_len = step_rows.len();
+    let ext_trace_len = if !trace_len.is_power_of_two() {
+        trace_len.next_power_of_two()
+    } else {
+        trace_len
+    };
+
+    let mut trace: Vec<Vec<F>> = vec![vec![F::ZERO; ext_trace_len]; mem_cols::NUM_MEM_COLS];
+    for (i, s) in step_rows.iter().enumerate() {
+        trace[mem_cols::COL_MEM_PADDING][i] = F::ZERO;
+        trace[mem_cols::COL_MEM_ADDR][i] = get_memory_inst_addr(s);
+        trace[mem_cols::COL_MEM_CLK][i] = get_memory_inst_clk(s);
+        trace[mem_cols::COL_MEM_OP][i] = get_memory_inst_op(&s.inst);
+
+        trace[mem_cols::COL_MEM_VALUE][i] = match s.inst.op {
+            Op::LB => get_memory_load_inst_value(s),
+            Op::SB => get_memory_store_inst_value(s),
+            _ => F::ZERO,
+        };
+
+        trace[mem_cols::COL_MEM_DIFF_ADDR][i] = if i == 0 {
+            F::ZERO
+        } else {
+            trace[mem_cols::COL_MEM_ADDR][i] - trace[mem_cols::COL_MEM_ADDR][i - 1]
+        };
+
+        trace[mem_cols::COL_MEM_DIFF_CLK][i] =
+            if i == 0 || trace[mem_cols::COL_MEM_ADDR][i] != trace[mem_cols::COL_MEM_ADDR][i - 1] {
+                F::ZERO
+            } else {
+                trace[mem_cols::COL_MEM_CLK][i] - trace[mem_cols::COL_MEM_CLK][i - 1]
+            };
+    }
+
+    // For expanded trace from `trace_len` to `trace_len's power of two`,
+    // we use last row to pad them.
+    if trace_len != ext_trace_len {
+        trace[mem_cols::COL_MEM_PADDING..mem_cols::NUM_MEM_COLS]
+            .iter_mut()
+            .for_each(|row| {
+                let last = row[trace_len - 1];
+                row[trace_len..].fill(last);
+            });
+    }
+
+    trace.try_into().unwrap_or_else(|v: Vec<Vec<F>>| {
+        panic!(
+            "Expected a Vec of length {} but it was {}",
+            mem_cols::NUM_MEM_COLS,
+            v.len()
+        )
+    })
+}
+
+#[cfg(test)]
+mod test {
+    use mozak_vm::test_utils::simple_test;
+
+    fn generate_memory_trace_test() {
+    }
+}
--- circuits/src/memory/trace.rs
+++ circuits/src/memory/trace.rs
@@ -1,6 +1,6 @@
-use plonky2::hash::hash_types::RichField;
 use mozak_vm::instruction::{Instruction, Op};
 use mozak_vm::vm::Row;
+use plonky2::hash::hash_types::RichField;
 
 pub(crate) const OPCODE_LB: usize = 0;
 pub(crate) const OPCODE_SB: usize = 1;
@@ -14,7 +14,8 @@ pub fn get_memory_inst_op<F: RichField>(inst: &Instruction) -> F {
 }
 
 pub fn get_memory_inst_addr<F: RichField>(row: &Row) -> F {
-    let addr = row.state
+    let addr = row
+        .state
         .get_register_value(row.inst.data.rs1.into())
         .wrapping_add(row.inst.data.imm);
     F::from_canonical_u32(addr)
@@ -30,11 +31,11 @@ pub fn get_memory_load_inst_value<F: RichField>(row: &Row) -> F {
     let addr = state
         .get_register_value(inst.data.rs1.into())
         .wrapping_add(inst.data.imm);
-    F::from_canonical_u32(state.load_u8(addr))
+    F::from_canonical_u8(state.load_u8(addr))
 }
 
 pub fn get_memory_store_inst_value<F: RichField>(row: &Row) -> F {
     let state = &row.state;
     let inst = &row.inst;
-    F::from_canonical_u32(state.get_register_value(inst.rs2.into()))
+    F::from_canonical_u32(state.get_register_value(inst.data.rs2.into()))
 }
