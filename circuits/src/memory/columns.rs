eb38ea5a43790476cbf071668301db6de29bb97d
Sai Deng <dengsai@hotmail.com>
2023-06-12 11:08:35 +0800

    add comments for memory columns

--- circuits/src/memory/columns.rs
+++ circuits/src/memory/columns.rs
@@ -1,9 +1,23 @@
+// Indicates if memory is padding.
 pub(crate) const COL_MEM_PADDING: usize = 0;
-pub(crate) const COL_MEM_ADDR: usize = COL_MEM_PADDING + 1;
-pub(crate) const COL_MEM_CLK: usize = COL_MEM_ADDR + 1;
-pub(crate) const COL_MEM_OP: usize = COL_MEM_CLK + 1;
-pub(crate) const COL_MEM_VALUE: usize = COL_MEM_OP + 1;
-pub(crate) const COL_MEM_DIFF_ADDR: usize = COL_MEM_VALUE + 1;
-pub(crate) const COL_MEM_DIFF_CLK: usize = COL_MEM_DIFF_ADDR + 1;
 
-pub(crate) const NUM_MEM_COLS: usize = COL_MEM_DIFF_CLK + 1;
+// Memory address.
+pub(crate) const COL_MEM_ADDR: usize = 1;
+
+// Clock at memory access.
+pub(crate) const COL_MEM_CLK: usize = 2;
+
+// Opcode of memory access.
+pub(crate) const COL_MEM_OP: usize = 3;
+
+// Value of memory access.
+pub(crate) const COL_MEM_VALUE: usize = 4;
+
+// Difference between current and previous address.
+pub(crate) const COL_MEM_DIFF_ADDR: usize = 5;
+
+// Difference between current and previous clock.
+pub(crate) const COL_MEM_DIFF_CLK: usize = 6;
+
+// Total number of columns.
+pub(crate) const NUM_MEM_COLS: usize = 7;
