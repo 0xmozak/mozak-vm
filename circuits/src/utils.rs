67a70cfc8b7ac2daac7b4a535bdcc38ae98b56d1
Matthias Goergens <matthias.goergens@gmail.com>
2023-06-13 17:06:38 +0800

    fix: pad_trace needs to increment the first new clock, too

--- circuits/src/utils.rs
+++ circuits/src/utils.rs
@@ -30,7 +30,7 @@ pub fn pad_trace<F: Field>(mut trace: Vec<Vec<F>>, clk_col: Option<usize>) -> Ve
         {
             let extra = padded_len - col.len();
             if clk_col == Some(i) {
-                col.extend((0_u64..).take(extra).map(|j| last + from_(j)));
+                col.extend((1_u64..).take(extra).map(|j| last + from_(j)));
             } else {
                 col.extend(vec![last; extra]);
             }
