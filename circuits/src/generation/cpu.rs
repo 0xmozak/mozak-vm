2e313be2681813782c3fcfb8d02ad79cdecd69a7
Vivek Pandya <vivekvpandya@gmail.com>
2023-06-15 10:00:18 +0530

    feat(proof-system): Fix CPU trace generation

--- circuits/src/generation/cpu.rs
+++ circuits/src/generation/cpu.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use mozak_vm::instruction::Op;
 use mozak_vm::vm::Row;
 use plonky2::hash::hash_types::RichField;
@@ -6,10 +7,10 @@ use crate::cpu::columns as cpu_cols;
 use crate::utils::{from_, pad_trace};
 
 #[allow(clippy::missing_panics_doc)]
-pub fn generate_cpu_trace<F: RichField>(step_rows: &Vec<Row>) -> [Vec<F>; cpu_cols::NUM_CPU_COLS] {
-    let trace_len = step_rows.len();
+pub fn generate_cpu_trace<F: RichField>(step_rows: &[Row]) -> [Vec<F>; cpu_cols::NUM_CPU_COLS] {
+    let trace_len = step_rows.len() - 1;
     let mut trace: Vec<Vec<F>> = vec![vec![F::ZERO; trace_len]; cpu_cols::NUM_CPU_COLS];
-    for (i, s) in step_rows.iter().enumerate() {
+    for (i, (s, ns)) in step_rows.iter().tuple_windows().enumerate() {
         trace[cpu_cols::COL_CLK][i] = from_(s.state.clk);
         trace[cpu_cols::COL_PC][i] = from_(s.state.get_pc());
 
@@ -20,13 +21,14 @@ pub fn generate_cpu_trace<F: RichField>(step_rows: &Vec<Row>) -> [Vec<F>; cpu_co
         trace[cpu_cols::COL_RD][i] = from_(inst.data.rd);
         trace[cpu_cols::COL_OP1_VALUE][i] =
             from_(s.state.get_register_value(usize::from(inst.data.rs1)));
-        // TODO(Vivek): Soon we support immediate values as opd2 in some instructions.
-        // So below line will change accordingly.
         trace[cpu_cols::COL_OP2_VALUE][i] =
             from_(s.state.get_register_value(usize::from(inst.data.rs2)));
+        // NOTE: Updated value of DST register is next step.
         trace[cpu_cols::COL_DST_VALUE][i] =
-            from_(s.state.get_register_value(usize::from(inst.data.rd)));
-        trace[cpu_cols::COL_S_HALT][i] = from_(s.state.has_halted());
+            from_(ns.state.get_register_value(usize::from(inst.data.rd)));
+        trace[cpu_cols::COL_IMM_VALUE][i] = from_(inst.data.imm);
+        // NOTE: Updated halted status is in next step.
+        trace[cpu_cols::COL_S_HALT][i] = from_(ns.state.has_halted());
         for j in 0..32_usize {
             trace[cpu_cols::COL_START_REG + j][i] = from_(s.state.get_register_value(j));
         }
@@ -34,6 +36,7 @@ pub fn generate_cpu_trace<F: RichField>(step_rows: &Vec<Row>) -> [Vec<F>; cpu_co
         match inst.op {
             Op::ADD => trace[cpu_cols::COL_S_ADD][i] = F::ONE,
             Op::BEQ => trace[cpu_cols::COL_S_BEQ][i] = F::ONE,
+            Op::ECALL => trace[cpu_cols::COL_S_ECALL][i] = F::ONE,
             _ => {}
         }
     }
@@ -42,6 +45,7 @@ pub fn generate_cpu_trace<F: RichField>(step_rows: &Vec<Row>) -> [Vec<F>; cpu_co
     // we use last row `HALT` to pad them.
     let trace = pad_trace(trace, Some(cpu_cols::COL_CLK));
 
+    log::trace!("trace {:?}", trace);
     trace.try_into().unwrap_or_else(|v: Vec<Vec<F>>| {
         panic!(
             "Expected a Vec of length {} but it was {}",
