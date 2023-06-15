4aad7a3a26fb7d6cdab7f42b0803288359d0710d
Sai Deng <dengsai@hotmail.com>
2023-06-15 20:51:42 +0800

    factor out the test case

--- circuits/src/generation/memory.rs
+++ circuits/src/generation/memory.rs
@@ -104,6 +104,8 @@ mod test {
     use plonky2::field::types::Field;
     use plonky2::plonk::config::{GenericConfig, PoseidonGoldilocksConfig};
 
+    use crate::memory::test_utils::memory_trace_test_case;
+
     // This test simulates the scenario of a set of instructions
     // which perform store byte (SB) and load byte (LB) operations
     // to memory and then checks if the memory trace is generated correctly.
@@ -120,31 +122,8 @@ mod test {
         // 0        100   20    LB  10     0         0          4
         // 0        200   8     SB  15     1         100        0
         // 0        200   12    LB  15     0         0          4
-        let ExecutionRecord {
-            executed,
-            last_state: state,
-        } = simple_test(
-            24,
-            &[
-                // Store Byte: M[rs1 + imm] = rs2
-                // imm[11:5]  rs2    rs1    funct3  imm[4:0]  opcode
-                // Load Byte: rd = M[rs1 + imm]
-                // imm[11:0]         rs1    funct3  rd        opcode
-                // 0000011    00001  00000  000     00100     0100011   sb r1, 100(r0)
-                // 000001100100      00000  000     00100     0000011   lb r4, 100(r0)
-                // 0000110    00011  00000  000     01000     0100011   sb r3, 200(r0)
-                // 000011001000      00000  000     00110     0000011   lb r6, 200(r0)
-                // 0000011    00010  00000  000     00100     0100011   sb r2, 100(r0)
-                // 000001100100      00000  000     00101     0000011   lb r5, 100(r0)
-                (0_u32, 0b0000_0110_0001_0000_0000_0010_0010_0011),
-                (4_u32, 0b0000_0110_0100_0000_0000_0010_0000_0011),
-                (8_u32, 0b0000_1100_0011_0000_0000_0100_0010_0011),
-                (12_u32, 0b0000_1100_1000_0000_0000_0011_0000_0011),
-                (16_u32, 0b0000_0110_0010_0000_0000_0010_0010_0011),
-                (20_u32, 0b0000_0110_0100_0000_0000_0010_1000_0011),
-            ],
-            &[(1, 5), (2, 10), (3, 15)],
-        );
+        let (exit_at, mem, reg) = memory_trace_test_case();
+        let (rows, state) = simple_test(exit_at, &*mem, &*reg);
         assert_eq!(state.load_u8(100), 10);
         assert_eq!(state.get_register_value(4), 5);
         assert_eq!(state.get_register_value(5), 10);
--- circuits/src/memory/mod.rs
+++ circuits/src/memory/mod.rs
@@ -1,3 +1,4 @@
 pub mod columns;
 pub mod stark;
+pub mod test_utils;
 pub mod trace;
