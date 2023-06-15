8b3f0d783821c73cd1913321a7b2ac4c06acb2aa
Matthias Goergens <matthias.goergens@gmail.com>
2023-06-15 14:23:26 +0800

    fix: Augment dst-value

--- circuits/src/generation/cpu.rs
+++ circuits/src/generation/cpu.rs
@@ -1,4 +1,3 @@
-use itertools::Itertools;
 use mozak_vm::instruction::Op;
 use mozak_vm::vm::Row;
 use plonky2::hash::hash_types::RichField;
@@ -6,11 +5,21 @@ use plonky2::hash::hash_types::RichField;
 use crate::cpu::columns as cpu_cols;
 use crate::utils::{from_, pad_trace};
 
+fn augment_dst(rows: &[Row]) -> impl Iterator<Item = (&Row, u32)> {
+    rows.iter().zip(
+        rows.iter()
+            .map(|s| s.state.current_instruction().data.rd)
+            .zip(rows.iter().skip(1))
+            .map(|(rd, row)| row.state.get_register_value(usize::from(rd)))
+            .chain(std::iter::once(0_u32)),
+    )
+}
+
 #[allow(clippy::missing_panics_doc)]
 pub fn generate_cpu_trace<F: RichField>(step_rows: &[Row]) -> [Vec<F>; cpu_cols::NUM_CPU_COLS] {
     let trace_len = step_rows.len() - 1;
     let mut trace: Vec<Vec<F>> = vec![vec![F::ZERO; trace_len]; cpu_cols::NUM_CPU_COLS];
-    for (i, (s, ns)) in step_rows.iter().tuple_windows().enumerate() {
+    for (i, (s, dst_val)) in augment_dst(step_rows).enumerate() {
         trace[cpu_cols::COL_CLK][i] = from_(s.state.clk);
         trace[cpu_cols::COL_PC][i] = from_(s.state.get_pc());
 
@@ -24,11 +33,10 @@ pub fn generate_cpu_trace<F: RichField>(step_rows: &[Row]) -> [Vec<F>; cpu_cols:
         trace[cpu_cols::COL_OP2_VALUE][i] =
             from_(s.state.get_register_value(usize::from(inst.data.rs2)));
         // NOTE: Updated value of DST register is next step.
-        trace[cpu_cols::COL_DST_VALUE][i] =
-            from_(ns.state.get_register_value(usize::from(inst.data.rd)));
+        trace[cpu_cols::COL_DST_VALUE][i] = from_(dst_val);
         trace[cpu_cols::COL_IMM_VALUE][i] = from_(inst.data.imm);
         // NOTE: Updated halted status is in next step.
-        trace[cpu_cols::COL_S_HALT][i] = from_(ns.state.has_halted());
+        trace[cpu_cols::COL_S_HALT][i] = from_(s.state.has_halted());
         for j in 0..32_usize {
             trace[cpu_cols::COL_START_REG + j][i] = from_(s.state.get_register_value(j));
         }
