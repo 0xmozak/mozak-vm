e99aaf2fb71c830e2a5293f0fb397d2b01ba3837
bing <bingcicle@proton.me>
2023-06-16 15:04:40 +0800

    rename trait to Lookups

--- circuits/src/stark/mozak_stark.rs
+++ circuits/src/stark/mozak_stark.rs
@@ -1,20 +1,17 @@
 use plonky2::{field::extension::Extendable, hash::hash_types::RichField};
 
-use crate::{
-    cpu::stark::CpuStark,
-    cross_table_lookup::{CrossTableLookup, LinkLookupTables, RangecheckCpuTable},
-};
+use crate::cpu::stark::CpuStark;
 #[derive(Clone)]
 pub struct MozakStark<F: RichField + Extendable<D>, const D: usize> {
     pub cpu_stark: CpuStark<F, D>,
-    pub cross_table_lookups: [CrossTableLookup<F>; 1],
+    // pub cross_table_lookups: [CrossTableLookup<F>; 1],
 }
 
 impl<F: RichField + Extendable<D>, const D: usize> Default for MozakStark<F, D> {
     fn default() -> Self {
         Self {
             cpu_stark: CpuStark::default(),
-            cross_table_lookups: [RangecheckCpuTable::link_lookup_tables(); 1],
+            // cross_table_lookups: [RangecheckCpuTable::lookups(); 1],
         }
     }
 }
