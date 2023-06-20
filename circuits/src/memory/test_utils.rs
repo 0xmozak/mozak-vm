d5cd2383a8a6072b8e78e646c1d1ff2a14a6c603
Matthias Görgens <matthias.goergens@mozak.com>
2023-06-20 10:21:37 +0800

    Make memory trace test easier to read (#205)
    And change struct definition field order for `Instruction::Data` to be
    more consistent with how people usually write Risc-V assembly.

--- circuits/src/memory/test_utils.rs
+++ circuits/src/memory/test_utils.rs
@@ -1,4 +1,8 @@
-use mozak_vm::test_utils::simple_test;
+use mozak_vm::instruction::{
+    Instruction,
+    Op::{LB, SB},
+};
+use mozak_vm::test_utils::simple_test_code;
 use mozak_vm::vm::{ExecutionRecord, Row};
 
 /// # Panics
@@ -11,33 +15,23 @@ use mozak_vm::vm::{ExecutionRecord, Row};
 /// * The value of register 6 is not 15.
 #[must_use]
 pub fn memory_trace_test_case() -> Vec<Row> {
-    // Store Byte: M[rs1 + imm] = rs2
-    // imm[11:5]  rs2    rs1    funct3  imm[4:0]  opcode
-    // Load Byte: rd = M[rs1 + imm]
-    // imm[11:0]         rs1    funct3  rd        opcode
-    // 0000011    00001  00000  000     00100     0100011   sb r1, 100(r0)
-    // 000001100100      00000  000     00100     0000011   lb r4, 100(r0)
-    // 0000110    00011  00000  000     01000     0100011   sb r3, 200(r0)
-    // 000011001000      00000  000     00110     0000011   lb r6, 200(r0)
-    // 0000011    00010  00000  000     00100     0100011   sb r2, 100(r0)
-    // 000001100100      00000  000     00101     0000011   lb r5, 100(r0)
-    let (exit_at, mem, reg) = (
-        24,
-        [
-            (0_u32, 0b0000_0110_0001_0000_0000_0010_0010_0011),
-            (4_u32, 0b0000_0110_0100_0000_0000_0010_0000_0011),
-            (8_u32, 0b0000_1100_0011_0000_0000_0100_0010_0011),
-            (12_u32, 0b0000_1100_1000_0000_0000_0011_0000_0011),
-            (16_u32, 0b0000_0110_0010_0000_0000_0010_0010_0011),
-            (20_u32, 0b0000_0110_0100_0000_0000_0010_1000_0011),
-        ],
-        [(1, 5), (2, 10), (3, 15)],
-    );
-
+    let new = Instruction::new;
     let ExecutionRecord {
         executed,
         last_state: state,
-    } = simple_test(exit_at, &mem, &reg);
+    } = simple_test_code(
+        &[
+            new(SB, 0, 0, 1, 100),
+            new(LB, 4, 0, 0, 100),
+            new(SB, 0, 0, 3, 200),
+            new(LB, 6, 0, 0, 200),
+            new(SB, 0, 0, 2, 100),
+            new(LB, 5, 0, 0, 100),
+        ],
+        &[],
+        &[(1, 5), (2, 10), (3, 15)],
+    );
+
     assert_eq!(state.load_u8(100), 10);
     assert_eq!(state.get_register_value(4), 5);
     assert_eq!(state.get_register_value(5), 10);
