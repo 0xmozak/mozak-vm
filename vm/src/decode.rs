1da837cec6dc96cc5e175c40561cb26157ab4d8c
Vivek Pandya <vivekvpandya@gmail.com>
2023-06-20 11:07:09 +0530

    feat(vm): Combine shifts with register and shifts with immediate semantics

--- vm/src/decode.rs
+++ vm/src/decode.rs
@@ -146,25 +146,22 @@ pub fn decode_instruction(pc: u32, word: u32) -> Instruction {
         0b001_0011 => match bf.func3() {
             // For Risc-V its ADDI but we handle it as ADD.
             0x0 => (Op::ADD, itype),
-            0x1 if 0 == itype.imm & !0b1_1111 => (Op::SLLI, itype),
+            // For Risc-V its SLLI but we handle it as SLL.
+            0x1 if 0 == itype.imm & !0b1_1111 => (Op::SLL, itype),
             // For Risc-V its SLTI but we handle it as SLT.
             0x2 => (Op::SLT, itype),
             // For Risc-V its SLTIU but we handle it as SLTU.
             0x3 => (Op::SLTU, itype),
             0x4 => (Op::XORI, itype),
             0x5 => {
-                let imm = itype.imm;
-                let imm_masked: u32 = imm.bit_range(4, 0);
-                let itype = Data {
-                    imm: imm_masked,
-                    ..itype
-                };
                 // Masks the first 7 bits in a word to differentiate between an
                 // SRAI/SRLI instruction. They have the same funct3 value and are
                 // differentiated by their 30th bit, for which SRAI = 1 and SRLI = 0.
-                match imm.bit_range(11, 5) {
-                    0b010_0000 => (Op::SRAI, itype),
-                    0 => (Op::SRLI, itype),
+                match itype.imm.bit_range(11, 5) {
+                    // For Risc-V its SRAI but we handle it as SRA.
+                    0b010_0000 => (Op::SRA, itype),
+                    // For Risc-V its SRLI but we handle it as SRL.
+                    0 => (Op::SRL, itype),
                     #[tarpaulin::skip]
                     _ => Default::default(),
                 }
@@ -286,7 +283,7 @@ mod test {
     fn slli(word: u32, rd: u8, rs1: u8, shamt: u8) {
         let ins: Instruction = decode_instruction(0, word);
         let match_ins = Instruction {
-            op: Op::SLLI,
+            op: Op::SLL,
             data: Data {
                 rd,
                 rs1,
@@ -342,11 +339,13 @@ mod test {
         assert_eq!(ins, match_ins);
     }
 
-    #[test_case(0x41f9_5293, 5, 18, 31; "srai r5, r18, 31")]
+    #[test_case(0x41f9_5293, 5, 18, 1024 + 31; "srai r5, r18, 31")]
     fn srai(word: u32, rd: u8, rs1: u8, imm: u32) {
+        // NOTE: we match imm value 0100000_11111 where upper bits 0100000 differentiate
+        // between SRAI and SRLI
         let ins: Instruction = decode_instruction(0, word);
         let match_ins = Instruction {
-            op: Op::SRAI,
+            op: Op::SRA,
             data: Data {
                 rd,
                 rs1,
@@ -361,7 +360,7 @@ mod test {
     fn srli(word: u32, rd: u8, rs1: u8, imm: u32) {
         let ins: Instruction = decode_instruction(0, word);
         let match_ins = Instruction {
-            op: Op::SRLI,
+            op: Op::SRL,
             data: Data {
                 rd,
                 rs1,
