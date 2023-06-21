07f0c8eb31bfc90d0d913073b7fc5796ee4efdb7
Matthias Goergens <matthias.goergens@gmail.com>
2023-06-21 15:08:41 +0800

    Proptest set up

--- circuits/src/lookup.rs
+++ circuits/src/lookup.rs
@@ -168,3 +168,110 @@ pub fn permute_cols<F: PrimeField64>(col_input: &[F], col_table: &[F]) -> (Vec<F
 
     (col_input_sorted, col_table_permuted)
 }
+
+pub fn permute_cols_<F: PrimeField64>(col_input: &[F], col_table: &[F]) -> (Vec<F>, Vec<F>) {
+    let n = col_input.len();
+
+    // The permuted inputs do not have to be ordered, but we found that sorting was
+    // faster than hash-based grouping. We also sort the table, as this helps us
+    // identify "unused" table elements efficiently.
+
+    // To compare elements, e.g. for sorting, we first need them in canonical form.
+    // It would be wasteful to canonicalize in each comparison, as a single
+    // element may be involved in many comparisons. So we will canonicalize once
+    // upfront, then use `to_noncanonical_u64` when comparing elements.
+
+    let col_input_sorted = col_input
+        .iter()
+        .map(PrimeField64::to_canonical)
+        .sorted_unstable_by_key(PrimeField64::to_noncanonical_u64)
+        .collect_vec();
+    let col_table_sorted = col_table
+        .iter()
+        .map(PrimeField64::to_canonical)
+        .sorted_unstable_by_key(PrimeField64::to_noncanonical_u64)
+        .collect_vec();
+
+    let mut unused_table_inds = Vec::with_capacity(n);
+    let mut unused_table_vals = Vec::with_capacity(n);
+    let mut col_table_permuted = vec![F::ZERO; n];
+    let mut i = 0;
+    let mut j = 0;
+    while (j < n) && (i < n) {
+        let input_val = col_input_sorted[i].to_noncanonical_u64();
+        let table_val = col_table_sorted[j].to_noncanonical_u64();
+        match input_val.cmp(&table_val) {
+            // In the below tables, we ignore the original input column `col_input` (A),
+            // and only care about `col_input_sorted` (A'), `col_table_permuted` (S'), and
+            // `col_table_sorted` (S).
+            //
+            // -------------
+            // | A'| S'| S |
+            // |---|---|---|
+            // | 4 | . | 3 | <- push 3 to `unused_table_vals` since
+            // |   |   |   |    A' (col_input_sorted) > S (col_table_sorted)
+            Ordering::Greater => {
+                unused_table_vals.push(col_table_sorted[j]);
+                j += 1;
+            }
+
+            // -------------
+            // | A'| S'| S |    if `unused_table_vals` has some value, insert
+            // |---|---|---|    into S' (col_table_permuted), else save its index to be
+            // | 2 | . | 3 | <- populated later. It does not matter what is in S',
+            // |   |   |   |    as long as it belongs in S (col_table_sorted).
+            //                  This case also means that our lookup constraint later will
+            //                  rely on the previous A' to be equal to the current A'
+            //                  to hold (diff_input_prev = next_perm_input - local_perm_input).
+            Ordering::Less => {
+                if let Some(x) = unused_table_vals.pop() {
+                    col_table_permuted[i] = x;
+                } else {
+                    unused_table_inds.push(i);
+                }
+                i += 1;
+            }
+            // -------------
+            // | A'| S'| S |    if A' (col_input_sorted) == S (col_table_sorted),
+            // |---|---|---|    insert into S' (col_table_permuted). This case also
+            // | 2 | 2 | 2 | <- means that our lookup constraint holds,
+            // |   |   |   |    since horizontally, diff_input_table = next_perm_input -
+            //                  next_perm_table.
+            Ordering::Equal => {
+                col_table_permuted[i] = col_table_sorted[j];
+                i += 1;
+                j += 1;
+            }
+        }
+    }
+
+    unused_table_vals.extend_from_slice(&col_table_sorted[j..n]);
+    unused_table_inds.extend(i..n);
+
+    // Populate all the empty `S'` values found in the 2nd case above.
+    for (ind, val) in unused_table_inds.into_iter().zip_eq(unused_table_vals) {
+        col_table_permuted[ind] = val;
+    }
+
+    (col_input_sorted, col_table_permuted)
+}
+
+#[cfg(test)]
+mod test {
+    use proptest::prelude::*;
+    use plonky2::field::types::Field64;
+    type F = plonky2::field::goldilocks_field::GoldilocksField;
+
+    proptest! {
+
+        #[test]
+        fn oracle(col_input in any::<Vec<u64>>(), col_table in any::<Vec<u64>>())  {
+            let col_input = col_input.into_iter().map(F::from_noncanonical_u64).collect::<Vec<_>>();
+            let col_table = col_table.into_iter().map(F::from_noncanonical_u64).collect::<Vec<_>>();
+            // pub fn permute_cols_<F: PrimeField64>(col_input: &[F], col_table: &[F]) -> (Vec<F>, Vec<F>) {
+            let old = super::permute_cols::<F>(&col_input, &col_table);
+            let new = super::permute_cols_::<F>(&col_input, &col_table);
+            prop_assert!(old == new);
+        }
+    }
+}
