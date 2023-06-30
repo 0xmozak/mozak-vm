368a1b48ad708692a4e21a90073facc9335a681f
Matthias Goergens <matthias.goergens@gmail.com>
2023-06-30 18:19:01 +0800

    Compiles

--- circuits/src/stark/verifier.rs
+++ circuits/src/stark/verifier.rs
@@ -12,9 +12,33 @@ use super::proof::AllProof;
 // use crate::bitwise;
 use crate::bitwise::stark::BitwiseStark;
 use crate::cpu::stark::CpuStark;
+use crate::stark::mozak_stark::NUM_TABLES;
 
 #[allow(clippy::missing_errors_doc)]
-pub fn verify_proof<F, C, const D: usize>(
+pub fn verify_proof_cpu<F, C, const D: usize>(
+    mozak_stark: MozakStark<F, D>,
+    all_proof: AllProof<F, C, D>,
+    config: &StarkConfig,
+) -> Result<()>
+where
+    F: RichField + Extendable<D>,
+    C: GenericConfig<D, F = F>,
+    [(); CpuStark::<F, D>::PUBLIC_INPUTS]:,
+    [(); CpuStark::<F, D>::COLUMNS]:,
+
+    [(); C::Hasher::HASH_SIZE]:, {
+    let MozakStark {
+        cpu_stark,
+        bitwise_stark: _,
+    } = mozak_stark;
+    let [cpu_proof, _]: [StarkProofWithPublicInputs<F, C, D>; NUM_TABLES] = all_proof.stark_proofs;
+
+    verify_stark_proof::<F, C, CpuStark<F, D>, D>(cpu_stark, cpu_proof, config)
+    // verify_stark_proof::<F, C, BitwiseStark<F, D>, D>(bitwise_stark, bitwise_proof, config)
+}
+
+#[allow(clippy::missing_errors_doc)]
+pub fn verify_proof_bitwise<F, C, const D: usize>(
     mozak_stark: MozakStark<F, D>,
     all_proof: AllProof<F, C, D>,
     config: &StarkConfig,
@@ -23,20 +47,13 @@ where
     F: RichField + Extendable<D>,
     C: GenericConfig<D, F = F>,
     [(); BitwiseStark::<F, D>::PUBLIC_INPUTS]:,
-    [(); CpuStark::<F, D>::PUBLIC_INPUTS]:,
-
     [(); BitwiseStark::<F, D>::COLUMNS]:,
-    [(); CpuStark::<F, D>::COLUMNS]:,
     [(); C::Hasher::HASH_SIZE]:, {
     let MozakStark {
-        cpu_stark,
+        cpu_stark: _,
         bitwise_stark,
     } = mozak_stark;
-    let [cpu_proof, _bitwise_proof] = all_proof.stark_proofs;
-    let [_cpu_proof, bitwise_proof] = all_proof.stark_proofs;
-    let cpu_proof: StarkProofWithPublicInputs<F, C, D> = cpu_proof;
-    let bitwise_proof: StarkProofWithPublicInputs<F, C, D> = bitwise_proof;
+    let [_, bitwise_proof]: [StarkProofWithPublicInputs<F, C, D>; NUM_TABLES] = all_proof.stark_proofs;
 
-    verify_stark_proof::<F, C, CpuStark<F, D>, D>(cpu_stark, cpu_proof, config)?;
     verify_stark_proof::<F, C, BitwiseStark<F, D>, D>(bitwise_stark, bitwise_proof, config)
 }
