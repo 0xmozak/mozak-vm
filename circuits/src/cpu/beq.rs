9ff73165a0707746d254133c7858c64160031fc7
Vivek Pandya <vivekvpandya@gmail.com>
2023-07-05 14:05:06 +0530

    Fix BEQ constraints.

--- circuits/src/cpu/beq.rs
+++ circuits/src/cpu/beq.rs
@@ -19,16 +19,24 @@ pub(crate) fn constraints<P: PackedField>(
 
     let diff = op1 - op2;
     let diff_inv = lv[COL_CMP_DIFF_INV];
-    let branch = diff * diff_inv;
+    let branch = P::ONES - diff * diff_inv;
     let equal = lv[COL_EQUAL];
-    yield_constr.constraint(is_beq * (branch - (P::ONES - equal)));
     let updated_pc = nv[COL_PC];
-    // diff_inv is 0 either inst is not BEQ or both opds are same
-    yield_constr.constraint(diff_inv * (P::ONES - diff * diff_inv));
+
+    // check equal is 0 or 1
+    yield_constr.constraint(is_beq * equal * (P::ONES - equal));
+
+    // if equal is 1 then diff should be 0
+    yield_constr.constraint(is_beq * equal * diff);
+
+    // equal should match with branch
+    yield_constr.constraint(is_beq * (branch - equal));
+
+    // check PC updated correctly
     let pc_next: P = (P::ONES - equal) * (updated_pc - (lv[COL_PC] + column_of_xs::<P>(4)));
     let pc_branch: P = equal * (updated_pc - lv[COL_IMM_VALUE]);
-    let pc_cons: P = pc_next + pc_branch;
-    yield_constr.constraint(is_beq * pc_cons);
+    yield_constr.constraint(is_beq * pc_next);
+    yield_constr.constraint(is_beq * pc_branch);
 }
 
 #[cfg(test)]
@@ -37,12 +45,22 @@ mod test {
     use mozak_vm::instruction::{Args, Instruction, Op};
     use mozak_vm::test_utils::simple_test_code;
     use proptest::prelude::any;
-    use proptest::proptest;
+    use proptest::strategy::{Just, Strategy};
+    use proptest::{prop_oneof, proptest};
 
     use crate::test_utils::simple_proof_test;
+    #[allow(clippy::cast_sign_loss)]
+    fn u32_extra() -> impl Strategy<Value = u32> {
+        prop_oneof![
+            Just(0_u32),
+            Just(1_u32),
+            Just(i32::MIN as u32),
+            any::<u32>()
+        ]
+    }
     proptest! {
             #[test]
-            fn prove_beq_proptest(a in any::<u32>(), b in any::<u32>()) {
+            fn prove_beq_proptest(a in u32_extra(), b in u32_extra()) {
                 let record = simple_test_code(
                     &[Instruction {
                         op: Op::BEQ,
@@ -67,7 +85,9 @@ mod test {
                     &[],
                     &[(6, a), (7, b)],
                 );
-                    if a != b {
+                    if a == b {
+                        assert_eq!(record.last_state.get_register_value(1), 0);
+                    } else {
                         assert_eq!(record.last_state.get_register_value(1), 10);
                     }
                 simple_proof_test(&record.executed).unwrap();
