da456408bb3839ceb97ab7956e8271827d794301
bing <bingcicle@proton.me>
2023-07-07 15:04:43 +0800

    remove cross_table_lookups entirely from MozakStark

--- circuits/src/stark/mozak_stark.rs
+++ circuits/src/stark/mozak_stark.rs
@@ -2,17 +2,9 @@ use plonky2::field::extension::Extendable;
 use plonky2::hash::hash_types::RichField;
 
 use crate::cpu::stark::CpuStark;
-#[derive(Clone)]
+#[derive(Clone, Default)]
 pub struct MozakStark<F: RichField + Extendable<D>, const D: usize> {
     pub cpu_stark: CpuStark<F, D>,
-}
-
-impl<F: RichField + Extendable<D>, const D: usize> Default for MozakStark<F, D> {
-    fn default() -> Self {
-        Self {
-            cpu_stark: CpuStark::default(),
-        }
-    }
 }
 
 pub(crate) const NUM_TABLES: usize = 1;
