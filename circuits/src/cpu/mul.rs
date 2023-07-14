07d11930b95e5938b7200de1546248c9b599318d
Matthias Goergens <matthias.goergens@gmail.com>
2023-07-14 15:01:54 +0800

    Unify MUL and MULHU

--- circuits/src/cpu/mul.rs
+++ circuits/src/cpu/mul.rs
@@ -2,64 +2,97 @@ use plonky2::field::packed::PackedField;
 use starky::constraint_consumer::ConstraintConsumer;
 
 use super::columns::{
-    COL_DST_VALUE, COL_OP1_VALUE, COL_OP2_VALUE, COL_S_MUL, MUL_HIGH_BITS, MUL_HIGH_DIFF_INV,
-    NUM_CPU_COLS,
+    COL_DST_VALUE, COL_OP1_VALUE, COL_OP2_VALUE, COL_S_MUL, COL_S_MULHU, MUL_HIGH_BITS,
+    MUL_HIGH_DIFF_INV, MUL_LOW_BITS, NUM_CPU_COLS,
 };
-use crate::utils::{column_of_xs, from_};
+use crate::utils::column_of_xs;
 
 pub(crate) fn constraints<P: PackedField>(
     lv: &[P; NUM_CPU_COLS],
     yield_constr: &mut ConstraintConsumer<P>,
 ) {
-    // TODO: Need range check for COL_OP1_VALUE, COL_OP2_VALUE and MUL_HIGH_BITS.
-    let base: P::Scalar = from_(1_u128 << 32);
-    let multiplied = lv[COL_OP1_VALUE] * lv[COL_OP2_VALUE];
-    let u32_max = column_of_xs::<P>(u64::from(u32::MAX));
-    let diff = u32_max - lv[MUL_HIGH_BITS];
-    // MUL_HIGH_BITS should not be equal to u32::MAX
-    // as for u32::MAX * u32::MAX MUL_HIGH_BITS will be 0xFFFF_FFFE
+    // TODO: MUL_LOW_BITS and MUL_HIGH_BITS need range checking.
+
+    // The Goldilocks field is carefully chosen to allow multiplication of u32
+    // values without overflow.
+    let base: P = column_of_xs::<P>(1_u64 << 32);
+
+    let op1 = lv[COL_OP1_VALUE];
+    let op2 = lv[COL_OP2_VALUE];
+    let low_limb = lv[MUL_LOW_BITS];
+    let high_limb = lv[MUL_HIGH_BITS];
+    let product = low_limb + base * high_limb;
+
+    yield_constr.constraint(product - op1 * op2);
+
+    // Now, let's copy our results to the destination register:
+    let is_mul = lv[COL_S_MUL];
+    let is_mulhu = lv[COL_S_MULHU];
+
+    let destination = lv[COL_DST_VALUE];
+    yield_constr.constraint(is_mul * (destination - low_limb));
+    yield_constr.constraint(is_mulhu * (destination - high_limb));
+
+    // The constraints above would be enough, if our field was large enough.
+    // However Goldilocks field is just a bit too small at order 2^64 - 2^32 + 1.
+    //
+    // Specifically, (1<<32) * (u32::MAX) === -1 (mod 2^64 - 2^32 + 1).
+    // That means a high_limb of u32::MAX would behave like -1.  And hijinx ensues:
+    //      let product = low_limb + base * high_limb;
+    // would be equivalent to
+    //      let product = low_limb - P::ONES;
+    //
+    // However, the largest combined result that an honest prover can produce is
+    // u32::MAX * u32::MAX = 0xFFFF_FFFE_0000_0001.  So we can add a constraint
+    // that high_limb is != 0xFFFF_FFFF == u32::MAX range.
+    //
+    // That curtails the exploit without invalidating any honest proofs.
+
+    let diff = column_of_xs::<P>(u64::from(u32::MAX)) - lv[MUL_HIGH_BITS];
     yield_constr.constraint(diff * lv[MUL_HIGH_DIFF_INV] - P::ONES);
-    let high_part = lv[MUL_HIGH_BITS] * base;
-
-    yield_constr.constraint(lv[COL_S_MUL] * (multiplied - (lv[COL_DST_VALUE] + high_part)));
-    // op1 * op2 - res - high_bits = 0
-    // if malicious user wants to prove op1* op2 = X (where X != res)
-    // then set high_bits = (op1 * op2 - X) / 2^32
-
-    // however we apply constraints such that high_bits is in [0, 0xFFFF_FFFE]
-    // hence above exploit is not possible.
 }
 
 #[cfg(test)]
 #[allow(clippy::cast_possible_wrap)]
 mod test {
     use mozak_vm::instruction::{Args, Instruction, Op};
-    use mozak_vm::test_utils::simple_test_code;
-    use proptest::prelude::{any, ProptestConfig};
+    use mozak_vm::test_utils::{simple_test_code, u32_extra};
+    use proptest::prelude::ProptestConfig;
     use proptest::proptest;
 
     use crate::test_utils::simple_proof_test;
     proptest! {
-            #![proptest_config(ProptestConfig::with_cases(4))]
-            #[test]
-            fn prove_mul_proptest(a in any::<u32>(), b in any::<u32>(), rd in 0_u8..32) {
-                let record = simple_test_code(
-                    &[Instruction {
+        #![proptest_config(ProptestConfig::with_cases(4))]
+        #[test]
+        fn prove_mul_proptest(a in u32_extra(), b in u32_extra()) {
+            let record = simple_test_code(
+                &[
+                    Instruction {
                         op: Op::MUL,
                         args: Args {
-                            rd,
+                            rd: 8,
                             rs1: 6,
                             rs2: 7,
                             ..Args::default()
                         },
-                    }],
-                    &[],
-                    &[(6, a), (7, b)],
-                );
-                if rd != 0 {
-                    assert_eq!(record.executed[1].state.get_register_value(rd), a.wrapping_mul(b));
-                }
-                simple_proof_test(&record.executed).unwrap();
-            }
+                    },
+                    Instruction {
+                        op: Op::MULHU,
+                        args: Args {
+                            rd: 9,
+                            rs1: 6,
+                            rs2: 7,
+                            ..Args::default()
+                        },
+                    },
+                ],
+                &[],
+                &[(6, a), (7, b)],
+            );
+            let (low, high) = a.widening_mul(b);
+            assert_eq!(record.executed[1].state.get_register_value(8), low);
+            assert_eq!(record.executed[2].state.get_register_value(9), high);
+            simple_proof_test(&record.executed).unwrap();
+        }
     }
 }
--- circuits/src/cpu/stark.rs
+++ circuits/src/cpu/stark.rs
@@ -11,7 +11,7 @@ use starky::vars::{StarkEvaluationTargets, StarkEvaluationVars};
 use super::columns::{
     COL_CLK, COL_DST_VALUE, COL_OP1_VALUE, COL_OP2_VALUE, COL_PC, COL_RD_SELECT, COL_REGS,
     COL_RS1_SELECT, COL_RS2_SELECT, COL_S_ADD, COL_S_BEQ, COL_S_DIVU, COL_S_ECALL, COL_S_HALT,
-    COL_S_MUL, COL_S_REMU, COL_S_SLT, COL_S_SLTU, COL_S_SRL, COL_S_SUB, NUM_CPU_COLS,
+    COL_S_MUL, COL_S_MULHU, COL_S_REMU, COL_S_SLT, COL_S_SLTU, COL_S_SRL, COL_S_SUB, NUM_CPU_COLS,
 };
 use super::{add, div, mul, slt, sub};
 use crate::utils::column_of_xs;
@@ -24,8 +24,16 @@ pub struct CpuStark<F, const D: usize> {
 
 use array_concat::{concat_arrays, concat_arrays_size};
 
-pub const STRAIGHTLINE_OPCODES: [usize; 8] = [
-    COL_S_ADD, COL_S_SUB, COL_S_DIVU, COL_S_MUL, COL_S_REMU, COL_S_SLT, COL_S_SLTU, COL_S_SRL,
+pub const STRAIGHTLINE_OPCODES: [usize; 9] = [
+    COL_S_ADD,
+    COL_S_SUB,
+    COL_S_DIVU,
+    COL_S_MUL,
+    COL_S_MULHU,
+    COL_S_REMU,
+    COL_S_SLT,
+    COL_S_SLTU,
+    COL_S_SRL,
 ];
 pub const JUMPING_OPCODES: [usize; 2] = [COL_S_BEQ, COL_S_ECALL];
 pub const OPCODES: [usize; concat_arrays_size!(STRAIGHTLINE_OPCODES, JUMPING_OPCODES)] =
