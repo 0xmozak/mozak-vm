62f4f2d97196907a09fbf31daec564054a8746d0
Sai Deng <deng.sai@gmail.com>
2023-07-20 23:25:07 +0900

    add an e2e test

--- circuits/src/stark/mozak_stark.rs
+++ circuits/src/stark/mozak_stark.rs
@@ -118,3 +118,55 @@ impl<F: Field> Lookups<F> for RangecheckCpuTable<F> {
         )
     }
 }
+
+#[cfg(test)]
+#[allow(clippy::cast_possible_wrap)]
+mod tests {
+    use mozak_vm::test_utils::simple_test;
+    use plonky2::fri::FriConfig;
+    use plonky2::plonk::config::{GenericConfig, PoseidonGoldilocksConfig};
+    use plonky2::util::log2_ceil;
+    use plonky2::util::timing::TimingTree;
+    use starky::config::StarkConfig;
+    use starky::stark::Stark;
+
+    use crate::stark::mozak_stark::MozakStark;
+    use crate::stark::prover::prove;
+    use crate::stark::verifier::verify_proof;
+
+    #[test]
+    fn mozak_e2e_test() {
+        let inst = 0x0073_02b3 /* add r5, r6, r7 */;
+        let mut mem = vec![];
+        let u16max: u32 = u32::from(u16::MAX);
+        for i in 0..u16max {
+            mem.push((i * 4, inst));
+        }
+        let record = simple_test(4 * u16max, &mem, &[(6, 100), (7, 100)]);
+        let step_rows = &record.executed;
+
+        const D: usize = 2;
+        type C = PoseidonGoldilocksConfig;
+        type F = <C as GenericConfig<D>>::F;
+        type S = MozakStark<F, D>;
+        let mut stark = S::default();
+        let config = StarkConfig::standard_fast_config();
+        let config = StarkConfig {
+            security_bits: 1,
+            num_challenges: 2,
+            fri_config: FriConfig {
+                // Plonky2 says: "Having constraints of degree higher than the rate is not supported
+                // yet." So we automatically set the rate here as required by plonky2.
+                rate_bits: log2_ceil(stark.cpu_stark.constraint_degree()),
+                cap_height: 0,
+                proof_of_work_bits: 0,
+                num_query_rounds: 5,
+                ..config.fri_config
+            },
+        };
+
+        let all_proof =
+            prove::<F, C, D>(step_rows, &mut stark, &config, &mut TimingTree::default());
+        verify_proof(stark, all_proof.unwrap(), &config).unwrap();
+    }
+}
