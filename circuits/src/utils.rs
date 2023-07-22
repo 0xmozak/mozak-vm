88851ad3fcf62fcf377678ef5bef0fe2451b9f47
Matthias Goergens <matthias.goergens@gmail.com>
2023-07-22 09:17:28 +0800

    Move boilerplate to utils

--- circuits/src/utils.rs
+++ circuits/src/utils.rs
@@ -56,3 +56,71 @@ pub(crate) const fn indices_arr<const N: usize>() -> [usize; N] {
     }
     indices_arr
 }
+
+macro_rules! boilerplate_implementations {
+    ($s: ident, $num: ident) => {
+        // `u8` is guaranteed to have a `size_of` of 1.
+        pub(crate) const $num: usize = size_of::<$s<u8>>();
+
+        impl<F: Field> Default for $s<F> {
+            fn default() -> Self { Self::from([F::ZERO; $num]) }
+        }
+
+        impl<T: Copy> From<[T; $num]> for $s<T> {
+            fn from(value: [T; $num]) -> Self {
+                unsafe { transmute_without_compile_time_size_checks(value) }
+            }
+        }
+
+        impl<T: Copy> From<$s<T>> for [T; $num] {
+            fn from(value: $s<T>) -> Self {
+                unsafe { transmute_without_compile_time_size_checks(value) }
+            }
+        }
+
+        impl<T: Copy> Borrow<$s<T>> for [T; $num] {
+            fn borrow(&self) -> &$s<T> { unsafe { &*(self as *const [T; $num]).cast::<$s<T>>() } }
+        }
+
+        impl<T: Copy> BorrowMut<$s<T>> for [T; $num] {
+            fn borrow_mut(&mut self) -> &mut $s<T> {
+                unsafe { &mut *(self as *mut [T; $num]).cast::<$s<T>>() }
+            }
+        }
+
+        impl<T: Copy> Borrow<[T; $num]> for $s<T> {
+            fn borrow(&self) -> &[T; $num] {
+                unsafe { &*(self as *const $s<T>).cast::<[T; $num]>() }
+            }
+        }
+
+        impl<T: Copy> BorrowMut<[T; $num]> for $s<T> {
+            fn borrow_mut(&mut self) -> &mut [T; $num] {
+                unsafe { &mut *(self as *mut $s<T>).cast::<[T; $num]>() }
+            }
+        }
+
+        impl<T: Copy, I> Index<I> for $s<T>
+        where
+            [T]: Index<I>,
+        {
+            type Output = <[T] as Index<I>>::Output;
+
+            fn index(&self, index: I) -> &Self::Output {
+                let arr: &[T; $num] = self.borrow();
+                <[T] as Index<I>>::index(arr, index)
+            }
+        }
+
+        impl<T: Copy, I> IndexMut<I> for $s<T>
+        where
+            [T]: IndexMut<I>,
+        {
+            fn index_mut(&mut self, index: I) -> &mut Self::Output {
+                let arr: &mut [T; $num] = self.borrow_mut();
+                <[T] as IndexMut<I>>::index_mut(arr, index)
+            }
+        }
+    };
+}
+pub(crate) use boilerplate_implementations;
