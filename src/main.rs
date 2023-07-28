430e79031138251396081d8fc9db22f398be8452
Vivek Pandya <vivekvpandya@gmail.com>
2023-07-28 13:53:56 +0530

    Simplify main.rs with having std library enabled

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,9 @@
-#![no_std]
-#![no_main]
+#![feature(restricted_std)]
+
 use core::assert_eq;
 
-
-#[no_mangle]
-pub extern "C" fn _start() {
+pub fn main() {
     let a = 10;
     let b = a * 10;
     assert_eq!(b, a * 10);
-}
-use core::panic::PanicInfo;
-
-/// This function is called on panic.
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
-
-// TODO: We should not require code below as it fixed with https://github.com/rust-lang/rust/issues/92897
-// However without following function linker fails.
-#[no_mangle]
-pub fn __atomic_load_4(arg: *const usize, _ordering: usize) -> usize {
-    unsafe { *arg }
 }
