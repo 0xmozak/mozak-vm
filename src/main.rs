67f9a1b5db6a289acf11bd5d7cd7aeb06e322ea8
Vivek Pandya <vivekvpandya@gmail.com>
2023-07-31 17:09:22 +0530

    Add iterative Fibonacci code as example

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,31 @@
 
 use core::arch::asm;
 
+fn fibonacci(n: u32) -> u64 {
+    if n == 0 {
+        return 0;
+    }
+    if n == 1 {
+        return 1;
+    }
+    let mut sum = 0;
+    let mut last = 0;
+    let mut curr = 1;
+    for _i in 0..(n - 2) {
+        sum = last + curr;
+        last = curr;
+        curr = sum;
+    }
+    sum
+}
+
 #[no_mangle]
 pub fn _start() -> ! {
-    let a = 10;
-    let b = a * 10;
-    exit(0)
+    exit(fibonacci(80));
 }
 
 /// Exit syscall
-pub fn exit(_code: i8) -> ! {
+pub fn exit(_code: u64) -> ! {
     unsafe {
         // a0 is _code
         asm!("li a7, 93");
