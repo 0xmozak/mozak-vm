a46561d0688f24570a13d628cc47002c119da1ee
Matthias Goergens <matthias.goergens@gmail.com>
2023-08-01 15:57:19 +0800

    Minimize diff

--- circuits/src/utils.rs
+++ circuits/src/utils.rs
@@ -21,16 +21,5 @@ pub fn pad_trace<F: Field>(mut trace: Vec<Vec<F>>) -> Vec<Vec<F>> {
     trace
 }
 
-/// Pad the trace to a power of 2.
-///
-/// # Panics
-/// There's an assert that makes sure all columns passed in have the same
-/// length.
-#[must_use]
-pub fn pad_trace_with_last<Row: Copy>(mut trace: Vec<Row>) -> Vec<Row> {
-    trace.resize(trace.len().next_power_of_two(), *trace.last().unwrap());
-    trace
-}
-
 #[must_use]
 pub(crate) fn from_u32<F: Field>(x: u32) -> F { Field::from_noncanonical_u64(x.into()) }
