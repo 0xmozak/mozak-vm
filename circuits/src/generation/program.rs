ae177d1d9f576afdb356445091fc96c13f530448
Matthias Goergens <matthias.goergens@gmail.com>
2023-08-03 14:47:42 +0800

    Pad to power of two

--- circuits/src/generation/program.rs
+++ circuits/src/generation/program.rs
@@ -5,6 +5,7 @@ use plonky2::hash::hash_types::RichField;
 
 use crate::cpu::columns::{CpuColumnsView, InstructionView};
 use crate::program::columns::{InstColumnsView, ProgramColumnsView};
+use crate::utils::pad_trace_with_default;
 
 /// Generates a program ROM trace
 #[must_use]
@@ -15,14 +16,16 @@ pub fn generate_program_rom_trace<F: RichField>(
 ) -> Vec<ProgramColumnsView<F>> {
     let used_pcs: HashSet<F> = cpu_trace.iter().map(|row| row.inst.pc).collect();
 
-    program
-        .code
-        .iter()
-        .map(|(&pc, &inst)| ProgramColumnsView {
-            filter: F::from_bool(used_pcs.contains(&F::from_canonical_u32(pc))),
-            inst: InstColumnsView::from(
-                InstructionView::from((pc, inst)).map(F::from_canonical_u32),
-            ),
-        })
-        .collect()
+    pad_trace_with_default(
+        program
+            .code
+            .iter()
+            .map(|(&pc, &inst)| ProgramColumnsView {
+                filter: F::from_bool(used_pcs.contains(&F::from_canonical_u32(pc))),
+                inst: InstColumnsView::from(
+                    InstructionView::from((pc, inst)).map(F::from_canonical_u32),
+                ),
+            })
+            .collect(),
+    )
 }
