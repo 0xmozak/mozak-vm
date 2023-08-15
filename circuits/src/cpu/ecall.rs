356d571a29b12da472325cb1c18c77ca743ccadc
Artem <48178500+ElusAegis@users.noreply.github.com>
2023-08-15 15:04:26 +0300

    docs: ECALL docs ready

--- circuits/src/cpu/ecall.rs
+++ circuits/src/cpu/ecall.rs
@@ -1,3 +1,5 @@
+//! This module implements the ECALL operation constraints
+
 use plonky2::field::packed::PackedField;
 use plonky2::field::types::Field;
 use starky::constraint_consumer::ConstraintConsumer;
@@ -9,13 +11,14 @@ pub(crate) fn constraints<P: PackedField>(
     nv: &CpuState<P>,
     yield_constr: &mut ConstraintConsumer<P>,
 ) {
-    // At the moment, the only system call we support is 'halt', ie ecall with x17 =
+    // Note that the only system call we support now is 'halt', ie ecall with x17 =
     // 93. Everything else is invalid.
+    // Check: `ecall` happened when x17 register evaluated to 93.
     yield_constr.constraint(lv.inst.ops.ecall * (lv.regs[17] - P::Scalar::from_canonical_u8(93)));
-    // Thus we can equate ecall with halt:
+    // Check: `ecall` happened and we `halt`.
     yield_constr.constraint(lv.inst.ops.ecall - lv.halted);
 
-    // 'halt' means: no bumping of pc anymore ever.
+    // Check: after halting we do not bump the pc.
     yield_constr.constraint_transition(lv.halted * (nv.inst.pc - lv.inst.pc));
 }
 
