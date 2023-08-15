de2260517331926abe8f51e76c012ca69d290852
bing <bingcicle@proton.me>
2023-08-15 06:26:46 +0000

    fix `op2_value` comment
    Co-authored-by: Matthias Görgens <matthias.goergens@mozak.com>

--- circuits/src/cpu/columns.rs
+++ circuits/src/cpu/columns.rs
@@ -63,7 +63,7 @@ pub struct CpuState<T> {
 
     pub op1_value: T,
     // The sum of the value of the second operand register and the
-    // immediate value. This will not overflow.
+    // immediate value. Wrapped around to fit in a `u32`.
     pub op2_value: T,
     /// The sum of the value of the second operand
     /// register and the immediate value with possible overflow.
