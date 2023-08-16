19d98097ea1bc1fe77ed4b7fe16f1bcef63511fa
Artem <48178500+ElusAegis@users.noreply.github.com>
2023-08-16 17:21:58 +0300

    Update circuits/src/rangecheck/mod.rs
    Co-authored-by: bing <bingcicle@proton.me>

--- circuits/src/rangecheck/mod.rs
+++ circuits/src/rangecheck/mod.rs
@@ -1,5 +1,5 @@
 //! This module contains the **`RangeCheck` STARK Table**.
-//! It is used to check that values between 0 and 2^32.
+//! It is used to check that values are between 0 and 2^32 (exclusive), i.e. [0, 2^32).
 //!
 //! This is done by further splitting the 32-bit value into two 16-bit limbs,
 //! and then checking that each limb is in the range 0 through [RANGE_CHECK_U16_SIZE](crate::generation::rangecheck::RANGE_CHECK_U16_SIZE) exclusive, i.e. [0..2^16)
