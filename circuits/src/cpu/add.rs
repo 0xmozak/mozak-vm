e5999a974c38d6727b3faa6cd3498e527a420c63
Matthias Goergens <matthias.goergens@gmail.com>
2023-09-07 10:15:26 +0800

    Test `ADD` with full proof and CPU proof
    Implement machinery to test proving and verifying our constraints for
    the ADD instruction with a small number of examples for the full proof,
    and a few more examples for the CPU-only proof.
    
    Supports https://github.com/0xmozak/mozak-vm/pull/593

--- circuits/src/cpu/add.rs
+++ circuits/src/cpu/add.rs
@@ -23,54 +23,50 @@ pub(crate) fn constraints<P: PackedField>(
 #[cfg(test)]
 #[allow(clippy::cast_possible_wrap)]
 mod tests {
-    use anyhow::Result;
     use mozak_vm::instruction::{Args, Instruction, Op};
-    use mozak_vm::test_utils::{simple_test_code, u32_extra};
+    use mozak_vm::test_utils::{simple_test_code, u32_extra, reg};
 
     use crate::cpu::stark::CpuStark;
     use crate::stark::mozak_stark::MozakStark;
-    use crate::test_utils::ProveAndVerify;
-    #[test]
-    fn prove_add_example() -> Result<()> {
+    use crate::test_utils::{ProveAndVerify, D, F};
+
+    fn prove_add<Stark: ProveAndVerify>(a: u32, b: u32, rd: u8) {
         let (program, record) = simple_test_code(
             &[Instruction {
                 op: Op::ADD,
                 args: Args {
-                    rd: 5,
+                    rd,
                     rs1: 6,
                     rs2: 7,
                     ..Args::default()
                 },
             }],
             &[],
-            &[(6, 100), (7, 100)],
+            &[(6, a), (7, b)],
         );
-        assert_eq!(record.last_state.get_register_value(5), 100 + 100);
-        MozakStark::prove_and_verify(&program, &record)
+        if rd != 0 {
+            assert_eq!(
+                record.executed[1].state.get_register_value(rd),
+                a.wrapping_add(b)
+            );
+        }
+        Stark::prove_and_verify(&program, &record).unwrap();
     }
+
     use proptest::prelude::ProptestConfig;
     use proptest::proptest;
     proptest! {
-            #![proptest_config(ProptestConfig::with_cases(4))]
+        #![proptest_config(ProptestConfig::with_cases(4))]
+        #[test]
+        fn prove_add_cpu(a in u32_extra(), b in u32_extra(), rd in reg()) {
+            prove_add::<CpuStark<F, D>>(a, b, rd);
+        }
+    }
+    proptest! {
+            #![proptest_config(ProptestConfig::with_cases(1))]
             #[test]
-            fn prove_add_proptest(a in u32_extra(), b in u32_extra(), rd in 0_u8..32) {
-                let (program, record) = simple_test_code(
-                    &[Instruction {
-                        op: Op::ADD,
-                        args: Args {
-                            rd,
-                            rs1: 6,
-                            rs2: 7,
-                            ..Args::default()
-                        },
-                    }],
-                    &[],
-                    &[(6, a), (7, b)],
-                );
-                if rd != 0 {
-                    assert_eq!(record.executed[1].state.get_register_value(rd), a.wrapping_add(b));
-                }
-                CpuStark::prove_and_verify(&program, &record).unwrap();
+            fn prove_add_mozak(a in u32_extra(), b in u32_extra(), rd in reg()) {
+                prove_add::<MozakStark<F, D>>(a, b, rd);
             }
     }
 }
