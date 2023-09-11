c47dadd684116f76d4de9747bd88c19b3d1e572c
codeblooded1729 <kapilpause007@gmail.com>
2023-09-11 19:01:50 +0530

    opt: implement SRLI as DIV

--- vm/src/decode.rs
+++ vm/src/decode.rs
@@ -171,10 +171,13 @@ pub fn decode_instruction(pc: u32, word: u32) -> Instruction {
                 // SRAI/SRLI instruction. They have the same funct3 value and are
                 // differentiated by their 30th bit, for which SRAI = 1 and SRLI = 0.
                 match imm.bit_range(11, 5) {
-                    // For Risc-V its SRAI but we handle it as SRA.
+                    // For Risc-V it's SRAI, but we handle it as SRA.
                     0b010_0000 => (Op::SRA, itype),
-                    // For Risc-V its SRLI but we handle it as SRL.
-                    0 => (Op::SRL, itype),
+                    // For Risc-V it's SRLI, but we handle it as DIV.
+                    0 => (Op::DIV, Args {
+                        imm: 1 << itype.imm,
+                        ..itype
+                    }),
                     #[tarpaulin::skip]
                     _ => Default::default(),
                 }
@@ -399,11 +402,11 @@ mod tests {
     fn srli(word: u32, rd: u8, rs1: u8, imm: u32) {
         let ins: Instruction = decode_instruction(0, word);
         let match_ins = Instruction {
-            op: Op::SRL,
+            op: Op::DIV,
             args: Args {
                 rd,
                 rs1,
-                imm,
+                imm: 1 << imm,
                 ..Default::default()
             },
         };
