75ae8819827d83e30cb4aa0798889f61fa933879
Matthias Goergens <matthias.goergens@gmail.com>
2023-09-19 14:28:04 +0800

    Benchmark with lto=fat

--- circuits/benches/simple_prover.rs
+++ circuits/benches/simple_prover.rs
@@ -3,6 +3,7 @@ use std::time::Duration;
 use criterion::{criterion_group, criterion_main, Criterion};
 use mozak_circuits::stark::mozak_stark::MozakStark;
 use mozak_circuits::test_utils::ProveAndVerify;
+use mozak_runner::instruction::{Args, Instruction, Op};
 use mozak_runner::test_utils::simple_test_code;
 
 fn bench_prove_verify_all(c: &mut Criterion) {
@@ -11,7 +12,27 @@ fn bench_prove_verify_all(c: &mut Criterion) {
     group.measurement_time(Duration::new(10, 0));
     group.bench_function("prove_verify_all", |b| {
         b.iter(|| {
-            let (program, record) = simple_test_code(&[], &[], &[]);
+            let instructions = &[
+                Instruction {
+                    op: Op::ADD,
+                    args: Args {
+                        rd: 1,
+                        rs1: 1,
+                        imm: 1_u32.wrapping_neg(),
+                        ..Args::default()
+                    },
+                },
+                Instruction {
+                    op: Op::BLT,
+                    args: Args {
+                        rs1: 0,
+                        rs2: 1,
+                        imm: 0,
+                        ..Args::default()
+                    },
+                },
+            ];
+            let (program, record) = simple_test_code(instructions, &[], &[(1, 1 << 16)]);
             MozakStark::prove_and_verify(&program, &record)
         })
     });
