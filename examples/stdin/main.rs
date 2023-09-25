17fef1cb3d1f3f56944715606a743e0fd7d5c65e
Vivek Pandya <vivekvpandya@gmail.com>
2023-09-25 13:51:01 +0530

    Fix comment

--- examples/stdin/main.rs
+++ examples/stdin/main.rs
@@ -21,7 +21,7 @@ pub fn main() {
     let n = mozak_io.read(buffer.as_mut()).expect("READ failed");
     assert!(n == 5); // but iotape generated with native run has total 11 bytes only
     let mut buffer = [0_u8; 1];
-    // by nothing left on iotape so read below must return 0 bytes
+    // by now nothing left on iotape so read below must return 0 bytes
     let n = mozak_io.read(buffer.as_mut()).expect("READ failed");
     assert!(n == 0);
     guest::env::write(&n.to_be_bytes());
