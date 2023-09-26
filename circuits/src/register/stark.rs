443b5f1bab30d0520bdf38ca6fce7fa2ac16edcb
bing <bingcicle@proton.me>
2023-09-26 11:04:20 +0800

    init

--- circuits/src/register/stark.rs
+++ circuits/src/register/stark.rs
@@ -1,3 +1,4 @@
+use std::borrow::Borrow;
 use std::marker::PhantomData;
 
 use plonky2::field::extension::{Extendable, FieldExtension};
@@ -10,6 +11,7 @@ use starky::vars::{StarkEvaluationTargets, StarkEvaluationVars};
 
 use super::columns::Register;
 use crate::columns_view::NumberOfColumns;
+use crate::cpu::stark::is_binary;
 
 #[derive(Clone, Copy, Default)]
 #[allow(clippy::module_name_repetitions)]
@@ -24,12 +26,48 @@ impl<F: RichField + Extendable<D>, const D: usize> Stark<F, D> for RegisterStark
     /// Constraints for the [`RegisterStark`].
     fn eval_packed_generic<FE, P, const D2: usize>(
         &self,
-        _vars: StarkEvaluationVars<FE, P, { Self::COLUMNS }, { Self::PUBLIC_INPUTS }>,
-        _yield_constr: &mut ConstraintConsumer<P>,
+        vars: StarkEvaluationVars<FE, P, { Self::COLUMNS }, { Self::PUBLIC_INPUTS }>,
+        yield_constr: &mut ConstraintConsumer<P>,
     ) where
         FE: FieldExtension<D2, BaseField = F>,
         P: PackedField<Scalar = FE>, {
-        unimplemented!()
+        let lv: &Register<P> = vars.local_values.borrow();
+        let nv: &Register<P> = vars.next_values.borrow();
+
+        // Virtual dummy column to differentiate between real rows and padding rows.
+        // TODO: CTL dummy column against `RegisterInit`
+        let lv_dummy = lv.is_init + lv.is_read + lv.is_write;
+        let nv_dummy = nv.is_init + nv.is_read + nv.is_write;
+
+        // Check: first register address == 1, i.e. we do not have register address 0
+        // in our trace.
+        yield_constr.constraint_first_row(lv.addr - P::ONES);
+
+        // Check: filter columns take 0 or 1 values only.
+        is_binary(yield_constr, lv.is_init);
+        is_binary(yield_constr, lv.is_read);
+        is_binary(yield_constr, lv.is_write);
+        is_binary(yield_constr, lv_dummy);
+
+        // Check: for any register, `is_read` should not change the value of the
+        // register. Only `is_write`, `is_init` or `is_dummy` should be able to
+        // change the values.
+        yield_constr.constraint_transition(
+            lv.is_read * (nv.value - lv.value) * (nv.addr - lv.addr - P::ONES),
+        );
+
+        // Check: virtual dummy column can flip between 1 or 0
+        // (lv_dummy - nv_dummy - 1) is expressed as such, because
+        // lv_dummy = 1 in the last real row, and
+        // nv_dummy = 0 in the first padding row.
+        yield_constr.constraint_transition((nv_dummy - lv_dummy) * (lv_dummy - nv_dummy - P::ONES));
+
+        // Only when next row is an init row, i.e. `is_init` == 1,
+        // then the register entry can change address.
+        yield_constr.constraint_transition((nv.is_read + nv.is_write) * (nv.addr - lv.addr));
+
+        // Check: last register address == 31
+        yield_constr.constraint_last_row(lv.addr - P::from(FE::from_canonical_u8(31)));
     }
 
     #[no_coverage]
@@ -43,4 +81,55 @@ impl<F: RichField + Extendable<D>, const D: usize> Stark<F, D> for RegisterStark
     }
 
     fn constraint_degree(&self) -> usize { 3 }
+}
+
+#[cfg(test)]
+mod tests {
+    use anyhow::Result;
+    use mozak_runner::instruction::{Args, Instruction, Op};
+    use mozak_runner::test_utils::simple_test_code;
+    use plonky2::plonk::config::{GenericConfig, PoseidonGoldilocksConfig};
+    use starky::stark_testing::test_stark_low_degree;
+
+    use super::*;
+    use crate::test_utils::ProveAndVerify;
+
+    const D: usize = 2;
+    type C = PoseidonGoldilocksConfig;
+    type F = <C as GenericConfig<D>>::F;
+    type S = RegisterStark<F, D>;
+
+    #[test]
+    fn test_degree() -> Result<()> {
+        let stark = S::default();
+        test_stark_low_degree(stark)
+    }
+
+    #[test]
+    fn prove_reg() -> Result<()> {
+        let instructions = [
+            Instruction::new(Op::ADD, Args {
+                rs1: 6,
+                rs2: 7,
+                rd: 4,
+                ..Args::default()
+            }),
+            Instruction::new(Op::ADD, Args {
+                rs1: 4,
+                rs2: 6,
+                rd: 5,
+                ..Args::default()
+            }),
+            Instruction::new(Op::ADD, Args {
+                rs1: 5,
+                rd: 4,
+                imm: 100,
+                ..Args::default()
+            }),
+        ];
+
+        let (program, record) = simple_test_code(&instructions, &[], &[(6, 100), (7, 200)]);
+        RegisterStark::prove_and_verify(&program, &record)?;
+        Ok(())
+    }
 }
