695c0e6cadebb37089ea9ceb84e88310e77205c4
Supragya Raj <supragyaraj@gmail.com>
2023-09-28 09:20:38 +0530

    add: suggestions

--- circuits/src/generation/memory.rs
+++ circuits/src/generation/memory.rs
@@ -6,6 +6,7 @@ use plonky2::hash::hash_types::RichField;
 
 use crate::memory::columns::Memory;
 use crate::memory::trace::{get_memory_inst_addr, get_memory_inst_clk};
+use crate::stark::utils::merge_by_key;
 
 /// Pad the memory trace to a power of 2.
 #[must_use]
@@ -32,16 +33,19 @@ fn pad_mem_trace<F: RichField>(mut trace: Vec<Memory<F>>) -> Vec<Memory<F>> {
 pub fn generate_memory_trace_from_execution<F: RichField>(
     program: &Program,
     step_rows: &[Row],
-) -> Vec<Memory<F>> {
+) -> impl Iterator<Item = Memory<F>> {
     step_rows
         .iter()
         .filter(|row| row.aux.mem_addr.is_some())
         .map(|row| {
             let addr: F = get_memory_inst_addr(row);
-            let addr_u32: Result<u32, _> = addr.to_canonical_u64().try_into();
+            let addr_u32: u32 = addr
+                .to_canonical_u64()
+                .try_into()
+                .expect("casting addr (F) to u32 should not fail");
             let op = &(row.state).current_instruction(program).op;
             Memory {
-                is_writable: F::from_bool(program.rw_memory.contains_key(&addr_u32.unwrap())),
+                is_writable: F::from_bool(program.rw_memory.contains_key(&addr_u32)),
                 addr,
                 clk: get_memory_inst_clk(row),
                 is_sb: F::from_bool(matches!(op, Op::SB)),
@@ -52,7 +56,6 @@ pub fn generate_memory_trace_from_execution<F: RichField>(
             }
         })
         .sorted_by_key(|memory| memory.addr.to_canonical_u64())
-        .collect()
 }
 
 /// Generates Memory trace from static `Program` for both read-only
@@ -60,7 +63,9 @@ pub fn generate_memory_trace_from_execution<F: RichField>(
 /// interleaved with runtime memory trace generated from VM
 /// execution for final memory trace.
 #[must_use]
-pub fn generate_memory_init_trace_from_program<F: RichField>(program: &Program) -> impl Iterator<Item = Memory<F>> {
+pub fn generate_memory_init_trace_from_program<F: RichField>(
+    program: &Program,
+) -> impl Iterator<Item = Memory<F>> {
     [(F::ZERO, &program.ro_memory), (F::ONE, &program.rw_memory)]
         .into_iter()
         .flat_map(|(is_writable, mem)| {
@@ -88,20 +93,12 @@ pub fn generate_memory_trace<F: RichField>(program: &Program, step_rows: &[Row])
     // `merged_trace` is address sorted combination of static and
     // dynamic memory trace components of program (ELF and execution)
     // `merge` operation is expected to be stable
-    let mut merged_trace: Vec<Memory<F>> = generate_memory_init_trace_from_program::<F>(program)
-        .into_iter()
-        .merge_by(
-            generate_memory_trace_from_execution(program, step_rows),
-            |x, y| {
-                let (x_u64, y_u64) = (x.addr.to_canonical_u64(), y.addr.to_canonical_u64());
-                if x_u64 == y_u64 {
-                    x.is_init.to_canonical_u64() == 1
-                } else {
-                    x_u64 < y_u64
-                }
-            },
-        )
-        .collect();
+    let mut merged_trace: Vec<Memory<F>> = merge_by_key(
+        generate_memory_init_trace_from_program::<F>(program).into_iter(),
+        generate_memory_trace_from_execution(program, step_rows),
+        |memory| (memory.addr.to_canonical_u64(), memory.is_init.is_zero()),
+    )
+    .collect();
 
     // Ensures constraints by filling remaining inter-row
     // relation values: clock difference and addr difference
@@ -127,40 +124,14 @@ mod tests {
     use im::hashmap::HashMap;
     use mozak_runner::elf::{Data, Program};
     use plonky2::field::goldilocks_field::GoldilocksField;
-    use plonky2::hash::hash_types::RichField;
     use plonky2::plonk::config::{GenericConfig, PoseidonGoldilocksConfig};
 
-    use crate::memory::columns::Memory;
     use crate::memory::test_utils::memory_trace_test_case;
     use crate::test_utils::{inv, prep_table};
 
     const D: usize = 2;
     type C = PoseidonGoldilocksConfig;
     type F = <C as GenericConfig<D>>::F;
-
-    fn expected_trace<F: RichField>() -> Vec<Memory<F>> {
-        let inv = inv::<F>;
-        #[rustfmt::skip]
-        prep_table(vec![
-            //is_writable  addr   clk  is_sb, is_lbu, is_init  value  diff_addr  diff_addr_inv  diff_clk
-            [       1,     100,   0,     0,     0,       1,        0,    100,     inv(100),            0],  // Memory Init: 100
-            [       1,     100,   1,     1,     0,       0,      255,      0,           0,             1],  // Operations:  100
-            [       1,     100,   2,     0,     1,       0,      255,      0,           0,             1],  // Operations:  100
-            [       1,     100,   5,     1,     0,       0,       10,      0,           0,             3],  // Operations:  100
-            [       1,     100,   6,     0,     1,       0,       10,      0,           0,             1],  // Operations:  100
-            [       1,     101,   0,     0,     0,       1,        0,      1,      inv(1),             0],  // Memory Init: 101
-            [       1,     102,   0,     0,     0,       1,        0,      1,      inv(1),             0],  // Memory Init: 102
-            [       1,     103,   0,     0,     0,       1,        0,      1,      inv(1),             0],  // Memory Init: 103
-            [       1,     200,   0,     0,     0,       1,        0,     97,     inv(97),             0],  // Memory Init: 200
-            [       1,     200,   3,     1,     0,       0,       15,      0,           0,             3],  // Operations:  200
-            [       1,     200,   4,     0,     1,       0,       15,      0,           0,             1],  // Operations:  200
-            [       1,     201,   0,     0,     0,       1,        0,      1,      inv(1),             0],  // Memory Init: 201
-            [       1,     202,   0,     0,     0,       1,        0,      1,      inv(1),             0],  // Memory Init: 202
-            [       1,     203,   0,     0,     0,       1,        0,      1,      inv(1),             0],  // Memory Init: 203
-            [       1,     203,   0,     0,     0,       0,        0,      0,           0,             0],  // Padding
-            [       1,     203,   0,     0,     0,       0,        0,      0,           0,             0],  // Padding
-        ])
-    }
 
     // This test simulates the scenario of a set of instructions
     // which perform store byte (SB) and load byte unsigned (LBU) operations
@@ -170,7 +141,30 @@ mod tests {
         let (program, record) = memory_trace_test_case(1);
 
         let trace = super::generate_memory_trace::<GoldilocksField>(&program, &record.executed);
-        assert_eq!(trace, expected_trace());
+        let inv = inv::<F>;
+        assert_eq!(
+            trace,
+            #[rustfmt::skip]
+            prep_table(vec![
+                //is_writable  addr   clk  is_sb, is_lbu, is_init  value  diff_addr  diff_addr_inv  diff_clk
+                [       1,     100,   0,     0,     0,       1,        0,    100,     inv(100),            0],  // Memory Init: 100
+                [       1,     100,   1,     1,     0,       0,      255,      0,           0,             1],  // Operations:  100
+                [       1,     100,   2,     0,     1,       0,      255,      0,           0,             1],  // Operations:  100
+                [       1,     100,   5,     1,     0,       0,       10,      0,           0,             3],  // Operations:  100
+                [       1,     100,   6,     0,     1,       0,       10,      0,           0,             1],  // Operations:  100
+                [       1,     101,   0,     0,     0,       1,        0,      1,      inv(1),             0],  // Memory Init: 101
+                [       1,     102,   0,     0,     0,       1,        0,      1,      inv(1),             0],  // Memory Init: 102
+                [       1,     103,   0,     0,     0,       1,        0,      1,      inv(1),             0],  // Memory Init: 103
+                [       1,     200,   0,     0,     0,       1,        0,     97,     inv(97),             0],  // Memory Init: 200
+                [       1,     200,   3,     1,     0,       0,       15,      0,           0,             3],  // Operations:  200
+                [       1,     200,   4,     0,     1,       0,       15,      0,           0,             1],  // Operations:  200
+                [       1,     201,   0,     0,     0,       1,        0,      1,      inv(1),             0],  // Memory Init: 201
+                [       1,     202,   0,     0,     0,       1,        0,      1,      inv(1),             0],  // Memory Init: 202
+                [       1,     203,   0,     0,     0,       1,        0,      1,      inv(1),             0],  // Memory Init: 203
+                [       1,     203,   0,     0,     0,       0,        0,      0,           0,             0],  // Padding
+                [       1,     203,   0,     0,     0,       0,        0,      0,           0,             0],  // Padding
+            ])
+        );
     }
 
     #[test]
