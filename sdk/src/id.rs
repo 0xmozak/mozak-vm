76e35cc26002a16b6f2b573b577f18023a7f5a95
Matthias Görgens <matthias.goergens@gmail.com>
2023-09-29 18:55:48 +0800

    Derive deref (#16)

--- sdk/src/id.rs
+++ sdk/src/id.rs
@@ -1,5 +1,4 @@
-use core::ops::Deref;
-
+use derive_more::Deref;
 #[cfg(all(feature = "dummy-system", feature = "std"))]
 use rand::distributions::Standard;
 #[cfg(all(feature = "dummy-system", feature = "std"))]
@@ -9,14 +8,8 @@ use rand::Rng;
 use serde::{Deserialize, Serialize};
 
 /// ID is a unique identifier for any part of the system.
-#[derive(Debug, Default, Clone, Copy, Eq, PartialEq, Hash, Serialize, Deserialize)]
+#[derive(Debug, Default, Clone, Copy, Eq, PartialEq, Hash, Serialize, Deserialize, Deref)]
 pub struct Id(pub [u8; 32]);
-
-impl Deref for Id {
-    type Target = [u8; 32];
-
-    fn deref(&self) -> &Self::Target { &self.0 }
-}
 
 #[cfg(all(feature = "dummy-system", feature = "std"))]
 impl Distribution<Id> for Standard {
