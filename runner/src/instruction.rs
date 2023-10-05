214526c4dbb9a3746bb2112f01129952205df415
ethan-000 <70556667+Ethan-000@users.noreply.github.com>
2023-10-05 10:01:42 +0100

    remove immediate range

--- runner/src/instruction.rs
+++ runner/src/instruction.rs
@@ -4,16 +4,16 @@ use derive_more::Display;
 use serde::{Deserialize, Serialize};
 
 /// Arguments of a Risc-V instruction
-///
-/// rd: destination register
-/// rs1 & rs2: source registers
-/// imm: immediate
 #[derive(Debug, Clone, Copy, Eq, PartialEq, Default)]
 #[cfg_attr(feature = "serialize", derive(Serialize, Deserialize))]
 pub struct Args {
+    /// Destination Register
     pub rd: u8,
+    /// Source Register
     pub rs1: u8,
+    /// Source Register
     pub rs2: u8,
+    /// Extracted Immediate
     pub imm: u32,
 }
 
@@ -38,13 +38,13 @@ pub enum Op {
     /// AND Immediate: rd = rs1 & imm
     AND,
     /// Shift Left Logical: rd = rs1 << rs2
-    /// Shift Left Logical Immediate: rd = rs1 << imm[0:4]
+    /// Shift Left Logical Immediate: rd = rs1 << imm
     SLL,
     /// Shift Right Logical: rd = rs1 >> rs2
-    /// Shift Right Logical Immediate: rd = rs1 >> imm[0:4]
+    /// Shift Right Logical Immediate: rd = rs1 >> imm
     SRL,
     /// Shift Right Arithmetic: rd = rs1 >> rs2
-    /// Shift Right Arithmetic Immediate: rd = rs1 >> imm[0:4]
+    /// Shift Right Arithmetic Immediate: rd = rs1 >> imm
     SRA,
     /// Set Less Than: rd = (rs1 < rs2)?1:0
     /// Set Less Than Immediate: rd = (rs1 < imm)?1:0
@@ -52,21 +52,21 @@ pub enum Op {
     /// Set Less Than (U): rd = (rs1 < rs2)?1:0
     /// Set Less Than Immediate (U): rd = (rs1 < imm)?1:0
     SLTU,
-    /// Load Byte: rd = M[rs1+imm][0:7]
+    /// Load Byte: rd = M[rs1+imm]
     LB,
-    /// Load Half: rd = M[rs1+imm][0:15]
+    /// Load Half: rd = M[rs1+imm]
     LH,
-    /// Load Word: rd = M[rs1+imm][0:31]
+    /// Load Word: rd = M[rs1+imm]
     LW,
-    /// Load Byte (U): rd = M[rs1+imm][0:7]
+    /// Load Byte (U): rd = M[rs1+imm]
     LBU,
-    /// Load Half (U): rd = M[rs1+imm][0:15]
+    /// Load Half (U): rd = M[rs1+imm]
     LHU,
-    /// Store Byte: M[rs1+imm][0:7] = rs2[0:7]
+    /// Store Byte: M[rs1+imm] = rs2
     SB,
-    /// Store Half: M[rs1+imm][0:15] = rs2[0:15]
+    /// Store Half: M[rs1+imm] = rs2
     SH,
-    /// Store Word: M[rs1+imm][0:31] = rs2[0:31]
+    /// Store Word: M[rs1+imm] = rs2
     SW,
     /// Branch == : if(rs1 == rs2) PC += imm
     BEQ,
@@ -87,13 +87,13 @@ pub enum Op {
     ECALL,
 
     // RV32M Multiply Extension
-    /// MUL: rd = (rs1 * rs2)[31:0]
+    /// MUL: rd = (rs1 * rs2)
     MUL,
-    /// MUL High: rd = (rs1 * rs2)[63:32]
+    /// MUL High: rd = (rs1 * rs2)
     MULH,
-    /// MUL High (S) (U): rd = (rs1 * rs2)[63:32]
+    /// MUL High (S) (U): rd = (rs1 * rs2)
     MULHU,
-    /// MUL High (U): rd = (rs1 * rs2)[63:32]
+    /// MUL High (U): rd = (rs1 * rs2)
     MULHSU,
     /// DIV: rd = (rs1 / rs2)
     DIV,
