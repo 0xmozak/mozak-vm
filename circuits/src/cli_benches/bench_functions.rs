a1a6f6100a98d266788ec96a7546b902a3f2a99a
codeblooded1729 <kapilpause007@gmail.com>
2023-10-10 15:45:03 +0530

    can bench function sample_bench

--- circuits/src/cli_benches/bench_functions.rs
+++ circuits/src/cli_benches/bench_functions.rs
@@ -1,3 +1,4 @@
+use anyhow::Result;
 use mozak_runner::instruction::{Args, Instruction, Op};
 use mozak_runner::test_utils::simple_test_code;
 
@@ -29,10 +30,57 @@ pub fn sample_bench(reg_value: u32) -> Result<(), anyhow::Error> {
     MozakStark::prove_and_verify(&program, &record)
 }
 
+#[derive(PartialEq, Debug)]
+pub enum BenchFunction {
+    SampleBench(u32),
+}
+
+impl BenchFunction {
+    pub fn run(&self) -> Result<(), anyhow::Error> {
+        match self {
+            BenchFunction::SampleBench(reg_value) => sample_bench(*reg_value),
+        }
+    }
+
+    /// helper function to extract a parameter from a string
+    fn extract_field<'a, 'b>(input: &'a str, field: &'b str) -> Result<&'a str> {
+        input
+            .split('&')
+            .find(|param| param.starts_with(field))
+            .map(|param| {
+                param
+                    .split('=')
+                    .nth(1)
+                    .ok_or(anyhow::anyhow!("param not of format field=value"))
+            })
+            .unwrap_or(Err(anyhow::anyhow!("Invalid input")))
+    }
+
+    pub fn from_name_and_params(function_name: &str, parameters: &str) -> Result<Self> {
+        match function_name {
+            "sample_bench" => {
+                // expect paramter string "reg=reg_value"
+                let reg_value: u32 = Self::extract_field(parameters, "reg")?.parse()?;
+                Ok(BenchFunction::SampleBench(reg_value))
+            }
+            _ => Err(anyhow::anyhow!("Invalid bench function")),
+        }
+    }
+}
+
+// Input is string of form "name=name&age=age". Write a function to extract the
+// u8 value name.
+
 /// Mostly intended just to debug the bench functions
 mod tests {
     #[test]
-    fn test_sample_bench() {
-        super::sample_bench(123).unwrap();
+    fn test_sample_bench() { super::sample_bench(123).unwrap(); }
+
+    #[test]
+    fn test_from_string() {
+        assert_eq!(
+            super::BenchFunction::from_name_and_params("sample_bench", "reg=123").unwrap(),
+            super::BenchFunction::SampleBench(123)
+        );
     }
 }
