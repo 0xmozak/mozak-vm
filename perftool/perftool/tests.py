db39c049e981763ab22ad0be7ef91eb82fb824e0
codeblooded1729 <kapilpause007@gmail.com>
2023-10-11 15:52:34 +0530

    checkpoint

--- perftool/perftool/tests.py
+++ perftool/perftool/tests.py
@@ -1,15 +1,25 @@
 import os
-from utils import build_release, create_repo_from_commmit, run_bench
+from utils import *
 import tempfile
+
+commit = "5cc4cb0aebb35c6ae97c8fbf9e946d99ba6e8178"
 
 
 def test_sample_bench():
     with tempfile.TemporaryDirectory() as tmpfolder:
         # commit with cli bench functions
-        create_repo_from_commmit("c1de304b57c0e2df70a07d4ebea33e7b70aa79f9", tmpfolder)
+        create_repo_from_commmit(commit, tmpfolder)
         cli_repo = os.path.join(tmpfolder, "cli")
         build_release(cli_repo)
-        run_bench("sample_bench", 12, cli_repo)
+        time_taken = bench("sample_bench", 12, cli_repo)
+        print(f"time taken is {time_taken} ")
 
 
 test_sample_bench()
+# tmpfolder = "tmp"
+# os.mkdir(tmpfolder)
+# create_repo_from_commmit(commit, tmpfolder)
+# cli_repo = os.path.join("tmp", "cli")
+# build_release(cli_repo)
+# time_taken = bench("sample_bench", 12, cli_repo)
+# print(f"time taken is {time_taken} ")
