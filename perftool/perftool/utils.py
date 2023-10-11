de4531014778e1270bbbf2407b4a498e4a368fce
codeblooded1729 <kapilpause007@gmail.com>
2023-10-11 16:21:36 +0530

    experiment: create csv file from data

--- perftool/perftool/utils.py
+++ perftool/perftool/utils.py
@@ -1,6 +1,7 @@
 import os
 import re
 import subprocess
+import csv
 
 
 def create_repo_from_commmit(commit: str, tmpfolder) -> None:
@@ -23,3 +24,11 @@ def bench(bench_function: str, parameter: int, cli_repo: str) -> float:
     pattern = r"\d+\.\d+"
     time_taken = re.findall(pattern, stdout.decode())[0]
     return float(time_taken)
+
+
+def write_into_csv(data: dict, csv_file_path) -> None:
+    with open(csv_file_path, "w+", newline="") as csv_file:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(data.keys())
+        rows = zip(*data.values())  # Transpose the data to match rows
+        csv_writer.writerows(rows)
