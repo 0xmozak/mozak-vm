5a6416d6002b356d099107755b167c4796aad853
Matthias Goergens <matthias.goergens@gmail.com>
2023-10-12 15:04:28 +0800

    Some suggestions for less string handling

--- circuits/src/cli_benches/bench_functions.rs
+++ circuits/src/cli_benches/bench_functions.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use clap::{Args as Args_, Subcommand};
 use mozak_runner::instruction::{Args, Instruction, Op};
 use mozak_runner::test_utils::simple_test_code;
 
@@ -30,24 +31,24 @@ pub fn sample_bench(reg_value: u32) -> Result<(), anyhow::Error> {
     MozakStark::prove_and_verify(&program, &record)
 }
 
-/// Currently we support only the bench
-/// functions that take one argument
-#[derive(PartialEq, Debug)]
-pub enum BenchFunction {
-    SampleBench,
+#[derive(Debug, Args_, Clone)]
+#[command(args_conflicts_with_subcommands = true)]
+pub struct BenchArgs {
+    #[command(subcommand)]
+    pub function: BenchFunction,
 }
 
-impl BenchFunction {
-    pub fn run(&self, parameter: u32) -> Result<(), anyhow::Error> {
-        match self {
-            BenchFunction::SampleBench => sample_bench(parameter),
-        }
-    }
+/// Currently we support only the bench
+/// functions that take one argument
+#[derive(PartialEq, Debug, Subcommand, Clone)]
+pub enum BenchFunction {
+    SampleBench { iterations: u32 },
+}
 
-    pub fn from_name(function_name: &str) -> Result<Self> {
-        match function_name {
-            "sample_bench" => Ok(BenchFunction::SampleBench),
-            _ => Err(anyhow::anyhow!("Invalid bench function")),
+impl BenchArgs {
+    pub fn run(&self) -> Result<(), anyhow::Error> {
+        match self.function {
+            BenchFunction::SampleBench { iterations } => sample_bench(iterations),
         }
     }
 }
@@ -58,16 +59,10 @@ mod tests {
     fn test_sample_bench() { super::sample_bench(123).unwrap(); }
 
     #[test]
-    fn test_from_string() {
-        assert_eq!(
-            super::BenchFunction::from_name("sample_bench").unwrap(),
-            super::BenchFunction::SampleBench
-        );
-    }
-
-    #[test]
     fn test_run() {
-        let function = super::BenchFunction::SampleBench;
-        assert!(function.run(123).is_ok());
+        let bench = super::BenchArgs {
+            function: super::BenchFunction::SampleBench { iterations: 123 },
+        };
+        bench.run().unwrap();
     }
 }
