6c8707f7918783fee714579830e62f05c5646e5c
Matthias Goergens <matthias.goergens@gmail.com>
2023-10-16 12:35:21 +0800

    Run shorter bench for debugging

--- circuits/benches/simple_prover.rs
+++ circuits/benches/simple_prover.rs
@@ -36,7 +36,7 @@ fn bench_prove_verify_all(c: &mut Criterion) {
                     },
                 },
             ];
-            let (program, record) = simple_test_code(instructions, &[], &[(1, 1 << 16)]);
+            let (program, record) = simple_test_code(instructions, &[], &[(1, 1 << 5)]);
             MozakStark::prove_and_verify(&program, &record)
         })
     });
