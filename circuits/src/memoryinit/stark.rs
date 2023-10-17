b626418c0f4d6c7ac8738ec74a5f783cda9dfad4
Matthias Goergens <matthias.goergens@gmail.com>
2023-10-17 13:54:43 +0800

    Fix up

--- circuits/src/memoryinit/stark.rs
+++ circuits/src/memoryinit/stark.rs
@@ -12,20 +12,14 @@ use starky::stark::Stark;
 
 use super::columns::MemoryInit;
 use crate::columns_view::NumberOfColumns;
-// use crate::display::derive_display_stark_name;
+use crate::display::derive_display_stark_name;
 use crate::stark::utils::is_binary;
 
-// derive_display_stark_name!(MemoryInitStark);
+derive_display_stark_name!(MemoryInitStark);
 #[derive(Clone, Copy, Default)]
 #[allow(clippy::module_name_repetitions)]
 pub struct MemoryInitStark<F, const D: usize> {
     pub _f: PhantomData<F>,
-}
-
-impl<F, const D: usize> Display for MemoryInitStark<F, D> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", std::stringify!(MemoryInitStark))
-    }
 }
 
 const COLUMNS: usize = MemoryInit::<()>::NUMBER_OF_COLUMNS;
