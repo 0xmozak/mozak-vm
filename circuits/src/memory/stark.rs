e9eacd6a77fc8c86230d55c8518ab839bb847065
Ro Ma <dimdumon@outlook.com>
2023-10-23 15:46:48 +0300

    fix: memory address init

--- circuits/src/memory/stark.rs
+++ circuits/src/memory/stark.rs
@@ -244,7 +244,11 @@ mod tests {
                 },
             },
         ];
-        let (program, record) = simple_test_code(instructions, &[], &[(1, iterations)]);
+        let (program, record) = simple_test_code(
+            instructions,
+            &[(1_u32.wrapping_neg().wrapping_add(addr_offset), 0)],
+            &[(1, iterations)],
+        );
         Stark::prove_and_verify(&program, &record)
     }
 
