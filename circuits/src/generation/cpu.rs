ed9315acb208f845a4afe64a7346258934ffe1b7
Ro Ma <dimdumon@outlook.com>
2023-10-24 12:59:02 +0300

    IO Memory Store (#764)
    Based on #762 and #763
    
    ---------
    
    Co-authored-by: Matthias Goergens <matthias.goergens@gmail.com>
    Co-authored-by: Matthias Görgens <matthias.goergens@mozak.com>

--- circuits/src/generation/cpu.rs
+++ circuits/src/generation/cpu.rs
@@ -3,7 +3,9 @@ use std::collections::HashSet;
 use itertools::{chain, Itertools};
 use mozak_runner::elf::Program;
 use mozak_runner::instruction::{Instruction, Op};
-use mozak_runner::state::{Aux, State};
+use mozak_runner::state::{Aux, IoEntry, IoOpcode, State};
+use mozak_runner::system::ecall;
+use mozak_runner::system::reg_abi::REG_A0;
 use mozak_runner::vm::{ExecutionRecord, Row};
 use plonky2::hash::hash_types::RichField;
 
@@ -53,9 +55,10 @@ pub fn generate_cpu_trace<F: RichField>(
         aux: executed.last().unwrap().aux.clone(),
     }];
 
+    let default_io_entry = IoEntry::default();
     for Row { state, aux } in chain![executed, last_row] {
         let inst = state.current_instruction(program);
-
+        let io = aux.io.as_ref().unwrap_or(&default_io_entry);
         let mut row = CpuState {
             clk: F::from_noncanonical_u64(state.clk),
             inst: cpu_cols::Instruction::from((state.get_pc(), inst)).map(from_u32),
@@ -74,6 +77,13 @@ pub fn generate_cpu_trace<F: RichField>(
             xor: generate_xor_row(&inst, state),
             mem_addr: F::from_canonical_u32(aux.mem.unwrap_or_default().addr),
             mem_value_raw: from_u32(aux.mem.unwrap_or_default().raw_value),
+            io_addr: F::from_canonical_u32(io.addr),
+            io_size: F::from_canonical_usize(io.data.len()),
+            is_io_store: F::from_bool(matches!((inst.op, io.op), (Op::ECALL, IoOpcode::Store))),
+            is_halt: F::from_bool(matches!(
+                (inst.op, state.registers[usize::try_from(REG_A0).unwrap()]),
+                (Op::ECALL, ecall::HALT)
+            )),
             ..CpuState::default()
         };
 
--- circuits/src/generation/fullword_memory.rs
+++ circuits/src/generation/fullword_memory.rs
@@ -68,12 +68,12 @@ pub fn generate_fullword_memory_trace<F: RichField>(
 }
 #[cfg(test)]
 mod tests {
-
     use plonky2::field::goldilocks_field::GoldilocksField;
     use plonky2::plonk::config::{GenericConfig, Poseidon2GoldilocksConfig};
 
     use crate::generation::fullword_memory::generate_fullword_memory_trace;
     use crate::generation::halfword_memory::generate_halfword_memory_trace;
+    use crate::generation::io_memory::generate_io_memory_trace;
     use crate::generation::memory::generate_memory_trace;
     use crate::generation::memoryinit::generate_memory_init_trace;
     use crate::memory_fullword::test_utils::fullword_memory_trace_test_case;
@@ -94,6 +94,7 @@ mod tests {
         let memory_init = generate_memory_init_trace(&program);
         let halfword_memory = generate_halfword_memory_trace(&program, &record.executed);
         let fullword_memory = generate_fullword_memory_trace(&program, &record.executed);
+        let io_memory_rows = generate_io_memory_trace(&program, &record.executed);
 
         let trace = generate_memory_trace::<GoldilocksField>(
             &program,
@@ -101,6 +102,7 @@ mod tests {
             &memory_init,
             &halfword_memory,
             &fullword_memory,
+            &io_memory_rows,
         );
         let inv = inv::<F>;
         assert_eq!(
