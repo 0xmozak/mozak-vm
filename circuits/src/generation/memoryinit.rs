c57d8c8f253bb21ef36ffd4ccedf40c42a2b7b6e
Sai Deng <deng.sai@gmail.com>
2023-10-24 13:21:06 -0700

    fix

--- circuits/src/generation/memoryinit.rs
+++ circuits/src/generation/memoryinit.rs
@@ -7,19 +7,24 @@ use crate::utils::pad_trace_with_default;
 /// Generates a memory init ROM trace
 #[must_use]
 pub fn generate_memory_init_trace<F: RichField>(program: &Program) -> Vec<MemoryInit<F>> {
-    pad_trace_with_default(
-        [(F::ZERO, &program.ro_memory), (F::ONE, &program.rw_memory)]
-            .into_iter()
-            .flat_map(|(is_writable, mem)| {
-                mem.iter().map(move |(&addr, &value)| MemoryInit {
-                    filter: F::ONE,
-                    is_writable,
-                    element: MemElement {
-                        address: F::from_canonical_u32(addr),
-                        value: F::from_canonical_u8(value),
-                    },
-                })
-            })
-            .collect(),
-    )
+    let mut combined_memory: Vec<(F, u32, u8)> = Vec::new();
+
+    for (is_writable, mem) in &[(F::ZERO, &program.ro_memory), (F::ONE, &program.rw_memory)] {
+        let mut sorted_mem: Vec<(&u32, &u8)> = mem.iter().collect();
+        sorted_mem.sort_by_key(|&(addr, _)| *addr);
+        for (&addr, &value) in sorted_mem.iter() {
+            combined_memory.push((is_writable.clone(), addr, value));
+        }
+    }
+
+    let trace: Vec<MemoryInit<F>> = combined_memory.into_iter().map(|(is_writable, addr, value)| MemoryInit {
+        filter: F::ONE,
+        is_writable,
+        element: MemElement {
+            address: F::from_canonical_u32(addr),
+            value: F::from_canonical_u8(value),
+        },
+    }).collect();
+
+    pad_trace_with_default(trace)
 }
