b1a5630bab6367db13e16231357e0f4233948353
bing <bingcicle@proton.me>
2023-11-07 17:31:13 +0800

    generate trace for limbs

--- circuits/src/generation/rangecheck_limb.rs
+++ circuits/src/generation/rangecheck_limb.rs
@@ -66,3 +66,67 @@ pub(crate) fn generate_rangecheck_limb_trace<F: RichField>(
 
     trace
 }
+
+#[cfg(test)]
+mod tests {
+    use mozak_runner::instruction::{Args, Instruction, Op};
+    use mozak_runner::test_utils::simple_test_code;
+    use plonky2::field::goldilocks_field::GoldilocksField;
+    use plonky2::field::types::PrimeField64;
+
+    use super::*;
+    use crate::generation::cpu::generate_cpu_trace;
+    use crate::generation::fullword_memory::generate_fullword_memory_trace;
+    use crate::generation::halfword_memory::generate_halfword_memory_trace;
+    use crate::generation::io_memory::generate_io_memory_trace;
+    use crate::generation::memory::generate_memory_trace;
+    use crate::generation::memoryinit::generate_memory_init_trace;
+    use crate::generation::poseidon2_sponge::generate_poseidon2_sponge_trace;
+    use crate::generation::rangecheck::generate_rangecheck_trace;
+
+    #[test]
+    fn test_generate_trace() {
+        type F = GoldilocksField;
+        let (program, record) = simple_test_code(
+            &[Instruction {
+                op: Op::SB,
+                args: Args {
+                    rs1: 1,
+                    imm: u32::MAX,
+                    ..Args::default()
+                },
+            }],
+            // Use values that would become limbs later
+            &[],
+            &[(1, u32::MAX)],
+        );
+
+        let cpu_rows = generate_cpu_trace::<F>(&program, &record);
+        let memory_init = generate_memory_init_trace(&program);
+        let halfword_memory = generate_halfword_memory_trace(&program, &record.executed);
+        let fullword_memory = generate_fullword_memory_trace(&program, &record.executed);
+        let io_memory = generate_io_memory_trace(&program, &record.executed);
+        let poseidon2_trace = generate_poseidon2_sponge_trace(&record.executed);
+        let memory_rows = generate_memory_trace::<F>(
+            &program,
+            &record.executed,
+            &memory_init,
+            &halfword_memory,
+            &fullword_memory,
+            &io_memory,
+            &poseidon2_trace,
+        );
+        let rangecheck_rows = generate_rangecheck_trace::<F>(&cpu_rows, &memory_rows);
+
+        let trace = generate_rangecheck_limb_trace(&cpu_rows, &rangecheck_rows);
+
+        for row in &trace {
+            // TODO(bing): more comprehensive test once we rip out the old trace gen logic.
+            // For now, just assert that all values are capped by u8::MAX.
+            assert!(u8::try_from(
+                u16::try_from(row.multiplicity_view.value.to_canonical_u64()).unwrap()
+            )
+            .is_ok());
+        }
+    }
+}
