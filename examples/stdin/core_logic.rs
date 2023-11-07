59975571b3c89410a6ab09ab99da626f45a67e3c
Ro Ma <dimdumon@outlook.com>
2023-11-08 00:10:41 +0200

    fix: todo - read-public

--- examples/stdin/core_logic.rs
+++ examples/stdin/core_logic.rs
@@ -8,31 +8,11 @@ pub struct MozakIo<'a> {
 }
 
 impl<'a> Read for MozakIo<'a> {
-    fn read_private(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+    /// TODO(Roman): how to add read-public ?
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
         #[cfg(target_os = "zkvm")]
         {
             mozak_system::system::syscall_ioread_private(buf.as_mut_ptr(), buf.len());
-            Ok(buf.len())
-        }
-        #[cfg(not(target_os = "zkvm"))]
-        {
-            let n_bytes = self.stdin.read(buf).expect("read should not fail");
-            // open I/O log file in append mode.
-            use std::io::Write;
-            let mut io_tape = std::fs::OpenOptions::new()
-                .append(true)
-                .create(true)
-                .open(self.io_tape_file.as_str())
-                .expect("cannot open tape");
-            io_tape.write(buf).expect("write failed");
-            Ok(n_bytes)
-        }
-    }
-
-    fn read_public(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        #[cfg(target_os = "zkvm")]
-        {
-            mozak_system::system::syscall_ioread_public(buf.as_mut_ptr(), buf.len());
             Ok(buf.len())
         }
         #[cfg(not(target_os = "zkvm"))]
