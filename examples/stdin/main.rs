4affec4ba62471b1d8ea6838d8b8774f61382ec2
Vivek Pandya <vivekvpandya@gmail.com>
2023-11-07 08:56:29 +0530

    Fix io_read and stdin example (#821)
    Co-authored-by: Matthias Görgens <matthias.goergens@mozak.com>

--- examples/stdin/main.rs
+++ examples/stdin/main.rs
@@ -17,13 +17,9 @@ pub fn main() {
     let mut buffer = [0_u8; 5];
     let n = mozak_io.read(buffer.as_mut()).expect("READ failed");
     assert!(n == 5);
-    let mut buffer = [0_u8; 10]; // requesting more bytes than native run
+    let mut buffer = [0_u8; 5];
     let n = mozak_io.read(buffer.as_mut()).expect("READ failed");
-    assert!(n == 5); // but iotape generated with native run has total 11 bytes only
-    let mut buffer = [0_u8; 1];
-    // by now nothing left on iotape so read below must return 0 bytes
-    let n = mozak_io.read(buffer.as_mut()).expect("READ failed");
-    assert!(n == 0);
+    assert!(n == 5);
     guest::env::write(&n.to_be_bytes());
 }
 
