058bab6665a1605ea1d15034c2c9f51303a88de1
codeblooded1729 <kapilpause007@gmail.com>
2023-11-10 13:47:30 +0530

    fix stuff after commit

--- runner/src/elf.rs
+++ runner/src/elf.rs
@@ -149,36 +149,41 @@ impl Program {
             .ok_or_else(|| anyhow!("Missing segment table"))?;
         ensure!(segments.len() <= 256, "Too many program headers");
 
-        let extract = |check_flags: fn(u64) -> bool| {
-            segments
-                .iter()
-                // It is OK to cast this as u32 because we already check that we're reading a 32-bit ELF.
-                // The elf parsing crate simply does an `as u64` after parsing `sh_flags` as a u32:
-                // https://docs.rs/elf/latest/src/elf/section.rs.html#82
-                .filter(|s| check_flags(u32::try_from(s.sh_flags).expect("cast from u64 sh_flags should succeed if reading a 32-bit ELF")))
-                .map(|segment| -> Result<_> {
-                    let file_size: usize = segment.sh_size.try_into()?;
-                    let vaddr: u32 = segment.sh_addr.try_into()?;
-                    let offset = segment.sh_offset.try_into()?;
-                    Ok((vaddr..).zip(input[offset..].iter().take(file_size).copied()))
-                })
-                .flatten_ok()
-                .try_collect()
-        };
+        let extract =
+            |check_flags: fn(u32) -> bool| {
+                segments
+                    .iter()
+                    // It is OK to cast this as u32 because we already check that we're reading a
+                    // 32-bit ELF. The elf parsing crate simply does an `as u64`
+                    // after parsing `sh_flags` as a u32: https://docs.rs/elf/latest/src/elf/section.rs.html#82
+                    .filter(|s| {
+                        check_flags(u32::try_from(s.sh_flags).expect(
+                            "cast from u64 sh_flags should succeed if reading a 32-bit ELF",
+                        ))
+                    })
+                    .map(|segment| -> Result<_> {
+                        let file_size: usize = segment.sh_size.try_into()?;
+                        let vaddr: u32 = segment.sh_addr.try_into()?;
+                        let offset = segment.sh_offset.try_into()?;
+                        Ok((vaddr..).zip(input[offset..].iter().take(file_size).copied()))
+                    })
+                    .flatten_ok()
+                    .try_collect()
+            };
 
         let ro_memory = Data(extract(|flags| {
-            flags & u64::from(elf::abi::SHF_WRITE) == u64::from(elf::abi::SHF_NONE)
+            flags & elf::abi::SHF_WRITE == elf::abi::SHF_NONE
         })?);
         let rw_memory = Data(extract(|flags| {
-            (flags & u64::from(elf::abi::SHF_ALLOC) == u64::from(elf::abi::SHF_ALLOC))
-                && (flags & u64::from(elf::abi::SHF_WRITE) == u64::from(elf::abi::SHF_WRITE))
+            (flags & elf::abi::SHF_ALLOC == elf::abi::SHF_ALLOC)
+                && (flags & elf::abi::SHF_WRITE == elf::abi::SHF_WRITE)
         })?);
         // Because we are implementing a modified Harvard Architecture, we make an
         // independent copy of the executable segments. In practice,
         // instructions will be in a R_X segment, so their data will show up in ro_code
         // and ro_memory. (RWX segments would show up in ro_code and rw_memory.)
         let ro_code = Code::from(&extract(|flags| {
-            flags & u64::from(elf::abi::SHF_EXECINSTR) == u64::from(elf::abi::SHF_EXECINSTR)
+            flags & elf::abi::SHF_EXECINSTR == elf::abi::SHF_EXECINSTR
         })?);
 
         Ok(Program {
