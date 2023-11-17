1a4f7f6e190ea24f9517b3825adfb60b07687570
Matthias Görgens <matthias.goergens@mozak.com>
2023-11-17 13:44:52 +0800

    Suggestion: Move Risc-V test data to its own top level directory (#892)

--- circuits/tests/riscv_tests.rs
+++ circuits/tests/riscv_tests.rs
@@ -1,3 +1,5 @@
+use std::include_bytes;
+
 use anyhow::Result;
 use mozak_circuits::test_utils::prove_and_verify_mozak_stark;
 use mozak_runner::elf::Program;
@@ -6,9 +8,9 @@ use mozak_runner::vm::step;
 use plonky2::field::goldilocks_field::GoldilocksField;
 use starky::config::StarkConfig;
 
-/// This macro takes in an identifier as the test name and the file name of a
-/// compiled ELF and runs it through the Mozak VM to ensure correctness of the
-/// base RISC-V implementation.
+/// This function takes the contents of a compiled ELF and runs it through the
+/// Mozak VM runner to ensure correctness of the base RISC-V implementation.
+/// Afterwards, we prove and verify the execution.
 ///
 /// Below, we use a set of test files compiled from https://github.com/riscv-software-src/riscv-tests,
 /// specifically the rv32ui and rv32um tests.
@@ -16,29 +18,36 @@ use starky::config::StarkConfig;
 /// These files are generated on the first `cargo build` using Docker which
 /// downloads the RISC-V toolchain and compiles these test files into ELFs.
 ///
-/// To use these tests, this macro specifically asserts that the value of
-/// x10 = 0 at the end of a run, as defined by RVTEST_PASS here: https://github.com/riscv/riscv-test-env/blob/4fabfb4e0d3eacc1dc791da70e342e4b68ea7e46/p/riscv_test.h#L247-L252
+/// To use these tests, this function specifically asserts that the value of
+/// x10 == 0 at the end of a run, as defined by RVTEST_PASS here: https://github.com/riscv/riscv-test-env/blob/4fabfb4e0d3eacc1dc791da70e342e4b68ea7e46/p/riscv_test.h#L247-L252
 /// Custom tests may be added as long as the assertion is respected.
+fn run_test(elf: &[u8]) -> Result<()> {
+    let _ = env_logger::try_init();
+    let program = Program::load_elf(elf)?;
+    let state = State::<GoldilocksField>::from(&program);
+    let record = step(&program, state)?;
+    let state = record.last_state.clone();
+    // At the end of every test,
+    // register a0(x10) is set to 0 before an ECALL if it passes
+    assert_eq!(state.get_register_value(10), 0);
+    assert_eq!(state.get_register_value(17), 93);
+    assert!(state.has_halted());
+
+    let config = StarkConfig::standard_fast_config();
+    prove_and_verify_mozak_stark(&program, &record, &config)?;
+    Ok(())
+}
+
+/// This macro takes in an identifier as the test name and the file name of a
+/// compiled ELF, and sets up a `run_test` for it.
 macro_rules! test_elf {
     ($test_name:ident, $file_name:tt) => {
         #[test]
         fn $test_name() -> Result<()> {
-            let _ = env_logger::try_init();
-            let elf_name = format!("tests/testdata/{}", $file_name);
-            let elf = std::fs::read(elf_name)?;
-            let program = Program::load_elf(&elf)?;
-            let state = State::<GoldilocksField>::from(&program);
-            let record = step(&program, state)?;
-            let state = record.last_state.clone();
-            // At the end of every test,
-            // register a0(x10) is set to 0 before an ECALL if it passes
-            assert_eq!(state.get_register_value(10), 0);
-            assert_eq!(state.get_register_value(17), 93);
-            assert!(state.has_halted());
-
-            let config = StarkConfig::standard_fast_config();
-            prove_and_verify_mozak_stark(&program, &record, &config)?;
-            Ok(())
+            run_test(include_bytes!(concat!(
+                "../../riscv-testdata/testdata/",
+                $file_name
+            )))
         }
     };
 }
