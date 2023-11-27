b7eb2c7791249874e4eac9dd7142d606dbb1c411
Matthias Goergens <matthias.goergens@gmail.com>
2023-11-27 15:32:56 +0800

    Suggestions

--- circuits3/src/generation/bitshift.rs
+++ circuits3/src/generation/bitshift.rs
@@ -1,3 +1,4 @@
+use log::debug;
 use p3_field::Field;
 use p3_matrix::dense::RowMajorMatrix;
 
@@ -6,17 +7,15 @@ use crate::columns_view::NumberOfColumns;
 
 pub fn generate_bitshift_trace<F: Field>() -> RowMajorMatrix<F> {
     // find better way to extract the const
-    const NUM_COLS: usize = BitShift::<usize>::NUMBER_OF_COLUMNS;
-    let mut trace_values = Vec::with_capacity(NUM_COLS * 32);
-    for i in 0..32 {
-        let slice: [F; NUM_COLS] = BitShift {
-            amount: i as u32,
-            multiplier: 1 << i,
-        }
+    let trace_values: Vec<F> = (0..32)
+        .flat_map(|i| {
+            BitShift {
+                amount: i,
+                multiplier: 1 << i,
+            }
+        })
         .map(F::from_canonical_u32)
-        .into();
-        trace_values.extend(slice);
-    }
-    println!("{:?}", trace_values);
-    RowMajorMatrix::new(trace_values, NUM_COLS)
+        .collect();
+    debug!("{trace_values:?}");
+    RowMajorMatrix::new(trace_values, BitShift::<usize>::NUMBER_OF_COLUMNS)
 }
