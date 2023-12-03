6c119399d7db0cf4eeeae5e59ebdbb339878a03e
Daniel Bloom <7810950-Daniel.Aaron.Bloom@users.noreply.gitlab.com>
2023-12-03 12:47:34 -0800

    refactor+fix: columns_view

--- circuits/src/columns_view.rs
+++ circuits/src/columns_view.rs
@@ -4,15 +4,32 @@
 //!
 //! This way, they can be nested to group columns by logic they handle.
 
-use std::mem::{size_of, transmute_copy, ManuallyDrop};
+use std::marker::PhantomData;
+use std::mem::{size_of, ManuallyDrop};
 use std::ops::IndexMut;
 
-pub(crate) unsafe fn transmute_without_compile_time_size_checks<T, U>(t: T) -> U {
-    debug_assert_eq!(size_of::<T>(), size_of::<U>());
+pub(crate) const unsafe fn transmute_without_compile_time_size_checks<T, U>(t: T) -> U {
+    #[repr(C)]
+    union MyUnion<T, U> {
+        t: ManuallyDrop<T>,
+        u: ManuallyDrop<U>,
+    }
+
+    debug_assert!(size_of::<T>() == size_of::<U>());
+
     // We need to avoid `t` being dropped automatically, so we use ManuallyDrop.
     // We copy the bit pattern.  The original `t` is no longer safe to use,
     // (and that's why we pass it by move, not by reference).
-    transmute_copy(&ManuallyDrop::<T>::new(t))
+    let t = ManuallyDrop::new(t);
+    ManuallyDrop::into_inner(MyUnion { t }.u)
+}
+pub(crate) const unsafe fn transmute_ref<T, U>(t: &T) -> &U {
+    debug_assert!(size_of::<T>() == size_of::<U>());
+    &*((t as *const T).cast::<U>())
+}
+pub(crate) unsafe fn transmute_mut<T, U>(t: &mut T) -> &mut U {
+    debug_assert!(size_of::<T>() == size_of::<U>());
+    &mut *((t as *mut T).cast::<U>())
 }
 
 pub trait HasNamedColumns {
@@ -22,6 +39,10 @@ pub trait HasNamedColumns {
 pub trait NumberOfColumns {
     const NUMBER_OF_COLUMNS: usize;
 }
+
+/// This structure only exists to improve macro impl hiding
+#[doc(hidden)]
+pub struct ColumnViewImplHider<T>(PhantomData<T>);
 
 // TODO(Matthias): this could probably be a custom derive macro?
 /// Functions to handle and seamlessly convert between `SubTableView` with named
@@ -34,7 +55,60 @@ pub trait NumberOfColumns {
 /// `new_columns_repr` can be seamlessly converted between each other.
 macro_rules! columns_view_impl {
     ($s: ident) => {
+        // This hides all the `unsafe` from clippy
+        impl<T> crate::columns_view::ColumnViewImplHider<$s<T>> {
+            const fn from_array(value: [T; std::mem::size_of::<$s<u8>>()]) -> $s<T> {
+                unsafe { crate::columns_view::transmute_without_compile_time_size_checks(value) }
+            }
+
+            const fn into_array(v: $s<T>) -> [T; std::mem::size_of::<$s<u8>>()] {
+                unsafe { crate::columns_view::transmute_without_compile_time_size_checks(v) }
+            }
+
+            const fn from_array_ref(value: &[T; std::mem::size_of::<$s<u8>>()]) -> &$s<T> {
+                unsafe { crate::columns_view::transmute_ref(value) }
+            }
+
+            const fn array_ref(v: &$s<T>) -> &[T; std::mem::size_of::<$s<u8>>()] {
+                unsafe { crate::columns_view::transmute_ref(v) }
+            }
+
+            pub fn from_array_mut(value: &mut [T; std::mem::size_of::<$s<u8>>()]) -> &mut $s<T> {
+                unsafe { crate::columns_view::transmute_mut(value) }
+            }
+
+            pub fn array_mut(v: &mut $s<T>) -> &mut [T; std::mem::size_of::<$s<u8>>()] {
+                unsafe { crate::columns_view::transmute_mut(v) }
+            }
+        }
+
         impl<T> $s<T> {
+            const fn from_array(value: [T; std::mem::size_of::<$s<u8>>()]) -> Self {
+                crate::columns_view::ColumnViewImplHider::<Self>::from_array(value)
+            }
+
+            const fn into_array(self) -> [T; std::mem::size_of::<$s<u8>>()] {
+                crate::columns_view::ColumnViewImplHider::<Self>::into_array(self)
+            }
+
+            const fn from_array_ref(value: &[T; std::mem::size_of::<$s<u8>>()]) -> &Self {
+                crate::columns_view::ColumnViewImplHider::<Self>::from_array_ref(value)
+            }
+
+            const fn array_ref(&self) -> &[T; std::mem::size_of::<$s<u8>>()] {
+                crate::columns_view::ColumnViewImplHider::<Self>::array_ref(self)
+            }
+
+            fn from_array_mut(value: &mut [T; std::mem::size_of::<$s<u8>>()]) -> &mut Self {
+                crate::columns_view::ColumnViewImplHider::<Self>::from_array_mut(value)
+            }
+
+            fn array_mut(&mut self) -> &mut [T; std::mem::size_of::<$s<u8>>()] {
+                crate::columns_view::ColumnViewImplHider::<Self>::array_mut(self)
+            }
+
+            pub fn iter(&self) -> std::slice::Iter<T> { self.array_ref().into_iter() }
+
             // At the moment we only use `map` Instruction,
             // so it's dead code for the other callers of `columns_view_impl`.
             // TODO(Matthias): remove this marker, once we use it for the other structs,
@@ -53,53 +127,45 @@ macro_rules! columns_view_impl {
         }
 
         impl<T> From<[T; std::mem::size_of::<$s<u8>>()]> for $s<T> {
-            fn from(value: [T; std::mem::size_of::<$s<u8>>()]) -> Self {
-                unsafe { crate::columns_view::transmute_without_compile_time_size_checks(value) }
-            }
+            fn from(value: [T; std::mem::size_of::<$s<u8>>()]) -> Self { Self::from_array(value) }
         }
 
         impl<T> From<$s<T>> for [T; std::mem::size_of::<$s<u8>>()] {
-            fn from(value: $s<T>) -> Self {
-                unsafe { crate::columns_view::transmute_without_compile_time_size_checks(value) }
-            }
+            fn from(value: $s<T>) -> Self { value.into_array() }
         }
 
-        impl<T> From<&[T]> for &$s<T> {
-            fn from(value: &[T]) -> Self {
-                let value: &[T; std::mem::size_of::<$s<u8>>()] =
-                    value.try_into().expect("slice of correct length");
-                unsafe { crate::columns_view::transmute_without_compile_time_size_checks(value) }
+        impl<'a, T> From<&'a [T]> for &'a $s<T> {
+            fn from(value: &'a [T]) -> Self {
+                const LEN: usize = std::mem::size_of::<$s<u8>>();
+                fn helper<'a, T: 'a>(value: &'a [T]) -> &'a $s<T> {
+                    let value = value.get(..LEN).expect("slice of correct length");
+                    let value = unsafe { &*(value.as_ptr().cast::<[T; LEN]>()) };
+                    $s::from_array_ref(value)
+                }
+                helper::<'a, T>(value)
             }
         }
 
         impl<T> std::borrow::Borrow<$s<T>> for [T; std::mem::size_of::<$s<u8>>()] {
-            fn borrow(&self) -> &$s<T> {
-                unsafe { &*(self as *const [T; std::mem::size_of::<$s<u8>>()]).cast::<$s<T>>() }
-            }
+            fn borrow(&self) -> &$s<T> { $s::from_array_ref(self) }
         }
 
         impl<T> std::borrow::BorrowMut<$s<T>> for [T; std::mem::size_of::<$s<u8>>()] {
-            fn borrow_mut(&mut self) -> &mut $s<T> {
-                unsafe { &mut *(self as *mut [T; std::mem::size_of::<$s<u8>>()]).cast::<$s<T>>() }
-            }
+            fn borrow_mut(&mut self) -> &mut $s<T> { $s::from_array_mut(self) }
         }
 
         impl<T> std::borrow::Borrow<[T; std::mem::size_of::<$s<u8>>()]> for $s<T> {
-            fn borrow(&self) -> &[T; std::mem::size_of::<$s<u8>>()] {
-                unsafe { &*(self as *const $s<T>).cast::<[T; std::mem::size_of::<$s<u8>>()]>() }
-            }
+            fn borrow(&self) -> &[T; std::mem::size_of::<$s<u8>>()] { self.array_ref() }
         }
-
         impl<T> std::borrow::BorrowMut<[T; std::mem::size_of::<$s<u8>>()]> for $s<T> {
-            fn borrow_mut(&mut self) -> &mut [T; std::mem::size_of::<$s<u8>>()] {
-                unsafe { &mut *(self as *mut $s<T>).cast::<[T; std::mem::size_of::<$s<u8>>()]>() }
-            }
+            fn borrow_mut(&mut self) -> &mut [T; std::mem::size_of::<$s<u8>>()] { self.array_mut() }
         }
 
         impl<T> std::borrow::Borrow<[T]> for $s<T> {
-            fn borrow(&self) -> &[T] {
-                unsafe { &*(self as *const $s<T>).cast::<[T; std::mem::size_of::<$s<u8>>()]>() }
-            }
+            fn borrow(&self) -> &[T] { self.array_ref() }
+        }
+        impl<T> std::borrow::BorrowMut<[T]> for $s<T> {
+            fn borrow_mut(&mut self) -> &mut [T] { self.array_mut() }
         }
 
         impl<T, I> std::ops::Index<I> for $s<T>
@@ -108,56 +174,36 @@ macro_rules! columns_view_impl {
         {
             type Output = <[T] as std::ops::Index<I>>::Output;
 
-            fn index(&self, index: I) -> &Self::Output {
-                use std::borrow::Borrow;
-                let arr: &[T; std::mem::size_of::<$s<u8>>()] = self.borrow();
-                <[T] as std::ops::Index<I>>::index(arr, index)
-            }
+            fn index(&self, index: I) -> &Self::Output { &self.array_ref()[index] }
         }
 
         impl<T, I> std::ops::IndexMut<I> for $s<T>
         where
             [T]: std::ops::IndexMut<I>,
         {
-            fn index_mut(&mut self, index: I) -> &mut Self::Output {
-                use std::borrow::BorrowMut;
-                let arr: &mut [T; std::mem::size_of::<$s<u8>>()] = self.borrow_mut();
-                <[T] as std::ops::IndexMut<I>>::index_mut(arr, index)
-            }
+            fn index_mut(&mut self, index: I) -> &mut Self::Output { &mut self.array_mut()[index] }
         }
 
         impl<T> std::iter::IntoIterator for $s<T> {
             type IntoIter = std::array::IntoIter<T, { std::mem::size_of::<$s<u8>>() }>;
             type Item = T;
 
-            fn into_iter(self) -> Self::IntoIter {
-                let array: [T; std::mem::size_of::<$s<u8>>()] = self.into();
-                array.into_iter()
-            }
+            fn into_iter(self) -> Self::IntoIter { self.into_array().into_iter() }
         }
 
         impl<'a, T> std::iter::IntoIterator for &'a $s<T> {
             type IntoIter = std::slice::Iter<'a, T>;
             type Item = &'a T;
 
-            fn into_iter(self) -> Self::IntoIter {
-                use std::borrow::Borrow;
-                let array: &[T; std::mem::size_of::<$s<u8>>()] = self.borrow();
-                array.into_iter()
-            }
-        }
-
-        impl<'a, T> $s<T> {
-            pub fn iter(&'a self) -> std::slice::Iter<'a, T> {
-                <&'a Self as IntoIterator>::into_iter(self)
-            }
+            fn into_iter(self) -> Self::IntoIter { self.iter() }
         }
 
         impl<T: std::fmt::Debug> std::iter::FromIterator<T> for $s<T> {
             fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
-                let vec: Vec<T> = iter.into_iter().collect();
-                let array: [T; std::mem::size_of::<$s<u8>>()] = vec.try_into().unwrap();
-                array.into()
+                const LEN: usize = std::mem::size_of::<$s<u8>>();
+                let vec: arrayvec::ArrayVec<T, LEN> = iter.into_iter().collect();
+                let array = vec.into_inner().expect("iterator of correct length");
+                Self::from_array(array)
             }
         }
     };
@@ -170,17 +216,18 @@ pub(crate) use columns_view_impl;
 /// array with the column name rather than the column index.
 macro_rules! make_col_map {
     ($s: ident) => {
-        use std::sync::OnceLock;
-        // TODO(Matthias): sort out const'ness of from_fn, and declare as a const
-        // instead of static:
-        pub(crate) fn col_map() -> &'static $s<usize> {
-            static MAP: OnceLock<$s<usize>> = OnceLock::new();
-            MAP.get_or_init(|| {
+        pub(crate) const fn col_map() -> &'static $s<usize> {
+            const MAP: $s<usize> = {
                 use crate::columns_view::NumberOfColumns;
-                const COLUMNS: usize = $s::<()>::NUMBER_OF_COLUMNS;
-                let indices_arr: [usize; COLUMNS] = core::array::from_fn(|i| i);
-                unsafe { std::mem::transmute::<[usize; COLUMNS], $s<usize>>(indices_arr) }
-            })
+                let mut indices_arr = [0usize; $s::<()>::NUMBER_OF_COLUMNS];
+                let mut i = 0;
+                while i < indices_arr.len() {
+                    indices_arr[i] = i;
+                    i += 1;
+                }
+                $s::from_array(indices_arr)
+            };
+            &MAP
         }
     };
 }
--- circuits3/src/columns_view.rs
+++ circuits3/src/columns_view.rs
@@ -4,19 +4,40 @@
 //!
 //! This way, they can be nested to group columns by logic they handle.
 
-use std::mem::{size_of, transmute_copy, ManuallyDrop};
+use std::marker::PhantomData;
+use std::mem::{size_of, ManuallyDrop};
 
-pub(crate) unsafe fn transmute_without_compile_time_size_checks<T, U>(t: T) -> U {
-    debug_assert_eq!(size_of::<T>(), size_of::<U>());
+pub(crate) const unsafe fn transmute_without_compile_time_size_checks<T, U>(t: T) -> U {
+    #[repr(C)]
+    union MyUnion<T, U> {
+        t: ManuallyDrop<T>,
+        u: ManuallyDrop<U>,
+    }
+
+    debug_assert!(size_of::<T>() == size_of::<U>());
+
     // We need to avoid `t` being dropped automatically, so we use ManuallyDrop.
     // We copy the bit pattern.  The original `t` is no longer safe to use,
     // (and that's why we pass it by move, not by reference).
-    transmute_copy(&ManuallyDrop::<T>::new(t))
+    let t = ManuallyDrop::new(t);
+    ManuallyDrop::into_inner(MyUnion { t }.u)
+}
+pub(crate) const unsafe fn transmute_ref<T, U>(t: &T) -> &U {
+    debug_assert!(size_of::<T>() == size_of::<U>());
+    &*((t as *const T).cast::<U>())
+}
+pub(crate) unsafe fn transmute_mut<T, U>(t: &mut T) -> &mut U {
+    debug_assert!(size_of::<T>() == size_of::<U>());
+    &mut *((t as *mut T).cast::<U>())
 }
 
 pub trait NumberOfColumns {
     const NUMBER_OF_COLUMNS: usize;
 }
+
+/// This structure only exists to improve macro impl hiding
+#[doc(hidden)]
+pub struct ColumnViewImplHider<T>(PhantomData<T>);
 
 // TODO(Matthias): this could probably be a custom derive macro?
 /// Functions to handle and seamlessly convert between `SubTableView` with named
@@ -29,7 +50,60 @@ pub trait NumberOfColumns {
 /// `new_columns_repr` can be seamlessly converted between each other.
 macro_rules! columns_view_impl {
     ($s: ident) => {
+        // This hides all the `unsafe` from clippy
+        impl<T> crate::columns_view::ColumnViewImplHider<$s<T>> {
+            const fn from_array(value: [T; std::mem::size_of::<$s<u8>>()]) -> $s<T> {
+                unsafe { crate::columns_view::transmute_without_compile_time_size_checks(value) }
+            }
+
+            const fn into_array(v: $s<T>) -> [T; std::mem::size_of::<$s<u8>>()] {
+                unsafe { crate::columns_view::transmute_without_compile_time_size_checks(v) }
+            }
+
+            const fn from_array_ref(value: &[T; std::mem::size_of::<$s<u8>>()]) -> &$s<T> {
+                unsafe { crate::columns_view::transmute_ref(value) }
+            }
+
+            const fn array_ref(v: &$s<T>) -> &[T; std::mem::size_of::<$s<u8>>()] {
+                unsafe { crate::columns_view::transmute_ref(v) }
+            }
+
+            pub fn from_array_mut(value: &mut [T; std::mem::size_of::<$s<u8>>()]) -> &mut $s<T> {
+                unsafe { crate::columns_view::transmute_mut(value) }
+            }
+
+            pub fn array_mut(v: &mut $s<T>) -> &mut [T; std::mem::size_of::<$s<u8>>()] {
+                unsafe { crate::columns_view::transmute_mut(v) }
+            }
+        }
+
         impl<T> $s<T> {
+            const fn from_array(value: [T; std::mem::size_of::<$s<u8>>()]) -> Self {
+                crate::columns_view::ColumnViewImplHider::<Self>::from_array(value)
+            }
+
+            const fn into_array(self) -> [T; std::mem::size_of::<$s<u8>>()] {
+                crate::columns_view::ColumnViewImplHider::<Self>::into_array(self)
+            }
+
+            const fn from_array_ref(value: &[T; std::mem::size_of::<$s<u8>>()]) -> &Self {
+                crate::columns_view::ColumnViewImplHider::<Self>::from_array_ref(value)
+            }
+
+            const fn array_ref(&self) -> &[T; std::mem::size_of::<$s<u8>>()] {
+                crate::columns_view::ColumnViewImplHider::<Self>::array_ref(self)
+            }
+
+            fn from_array_mut(value: &mut [T; std::mem::size_of::<$s<u8>>()]) -> &mut Self {
+                crate::columns_view::ColumnViewImplHider::<Self>::from_array_mut(value)
+            }
+
+            fn array_mut(&mut self) -> &mut [T; std::mem::size_of::<$s<u8>>()] {
+                crate::columns_view::ColumnViewImplHider::<Self>::array_mut(self)
+            }
+
+            pub fn iter(&self) -> std::slice::Iter<T> { self.array_ref().into_iter() }
+
             // At the moment we only use `map` Instruction,
             // so it's dead code for the other callers of `columns_view_impl`.
             // TODO(Matthias): remove this marker, once we use it for the other structs,
@@ -48,87 +122,64 @@ macro_rules! columns_view_impl {
         }
 
         impl<T> From<[T; std::mem::size_of::<$s<u8>>()]> for $s<T> {
-            fn from(value: [T; std::mem::size_of::<$s<u8>>()]) -> Self {
-                unsafe { crate::columns_view::transmute_without_compile_time_size_checks(value) }
-            }
+            fn from(value: [T; std::mem::size_of::<$s<u8>>()]) -> Self { Self::from_array(value) }
         }
 
         impl<T> From<$s<T>> for [T; std::mem::size_of::<$s<u8>>()] {
-            fn from(value: $s<T>) -> Self {
-                unsafe { crate::columns_view::transmute_without_compile_time_size_checks(value) }
-            }
+            fn from(value: $s<T>) -> Self { value.into_array() }
         }
 
         impl<T> From<&[T]> for &$s<T> {
             fn from(value: &[T]) -> Self {
-                let value: &[T; std::mem::size_of::<$s<u8>>()] =
-                    value.try_into().expect("slice of correct length");
-                unsafe { crate::columns_view::transmute_without_compile_time_size_checks(value) }
+                const LEN: usize = std::mem::size_of::<$s<u8>>();
+                let value = value.get(..LEN).expect("slice of correct length");
+                let value = unsafe { &*(value.as_ptr().cast::<[T; LEN]>()) };
+                $s::from_array_ref(value)
             }
         }
 
         impl<T> std::borrow::Borrow<$s<T>> for [T; std::mem::size_of::<$s<u8>>()] {
-            fn borrow(&self) -> &$s<T> {
-                unsafe { &*(self as *const [T; std::mem::size_of::<$s<u8>>()]).cast::<$s<T>>() }
-            }
+            fn borrow(&self) -> &$s<T> { $s::from_array_ref(self) }
         }
 
         impl<T> std::borrow::BorrowMut<$s<T>> for [T; std::mem::size_of::<$s<u8>>()] {
-            fn borrow_mut(&mut self) -> &mut $s<T> {
-                unsafe { &mut *(self as *mut [T; std::mem::size_of::<$s<u8>>()]).cast::<$s<T>>() }
-            }
+            fn borrow_mut(&mut self) -> &mut $s<T> { $s::from_array_mut(self) }
         }
 
         impl<T> std::borrow::Borrow<[T; std::mem::size_of::<$s<u8>>()]> for $s<T> {
-            fn borrow(&self) -> &[T; std::mem::size_of::<$s<u8>>()] {
-                unsafe { &*(self as *const $s<T>).cast::<[T; std::mem::size_of::<$s<u8>>()]>() }
-            }
+            fn borrow(&self) -> &[T; std::mem::size_of::<$s<u8>>()] { self.array_ref() }
         }
-
         impl<T> std::borrow::BorrowMut<[T; std::mem::size_of::<$s<u8>>()]> for $s<T> {
-            fn borrow_mut(&mut self) -> &mut [T; std::mem::size_of::<$s<u8>>()] {
-                unsafe { &mut *(self as *mut $s<T>).cast::<[T; std::mem::size_of::<$s<u8>>()]>() }
-            }
+            fn borrow_mut(&mut self) -> &mut [T; std::mem::size_of::<$s<u8>>()] { self.array_mut() }
         }
 
         impl<T> std::borrow::Borrow<[T]> for $s<T> {
-            fn borrow(&self) -> &[T] {
-                unsafe { &*(self as *const $s<T>).cast::<[T; std::mem::size_of::<$s<u8>>()]>() }
-            }
+            fn borrow(&self) -> &[T] { self.array_ref() }
+        }
+        impl<T> std::borrow::BorrowMut<[T]> for $s<T> {
+            fn borrow_mut(&mut self) -> &mut [T] { self.array_mut() }
         }
 
         impl<T> std::iter::IntoIterator for $s<T> {
             type IntoIter = std::array::IntoIter<T, { std::mem::size_of::<$s<u8>>() }>;
             type Item = T;
 
-            fn into_iter(self) -> Self::IntoIter {
-                let array: [T; std::mem::size_of::<$s<u8>>()] = self.into();
-                array.into_iter()
-            }
+            fn into_iter(self) -> Self::IntoIter { self.into_array().into_iter() }
         }
 
         impl<'a, T> std::iter::IntoIterator for &'a $s<T> {
             type IntoIter = std::slice::Iter<'a, T>;
             type Item = &'a T;
 
-            fn into_iter(self) -> Self::IntoIter {
-                use std::borrow::Borrow;
-                let array: &[T; std::mem::size_of::<$s<u8>>()] = self.borrow();
-                array.into_iter()
-            }
-        }
-
-        impl<'a, T> $s<T> {
-            pub fn iter(&'a self) -> std::slice::Iter<'a, T> {
-                <&'a Self as IntoIterator>::into_iter(self)
-            }
+            fn into_iter(self) -> Self::IntoIter { self.iter() }
         }
 
         impl<T: std::fmt::Debug> std::iter::FromIterator<T> for $s<T> {
             fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
-                let vec: Vec<T> = iter.into_iter().collect();
-                let array: [T; std::mem::size_of::<$s<u8>>()] = vec.try_into().unwrap();
-                array.into()
+                const LEN: usize = std::mem::size_of::<$s<u8>>();
+                let vec: arrayvec::ArrayVec<T, LEN> = iter.into_iter().collect();
+                let array = vec.into_inner().expect("iterator of correct length");
+                Self::from_array(array)
             }
         }
     };
