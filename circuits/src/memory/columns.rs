7c57a21f2b82db531ff320c904255a1b9a7e92a6
bing <bingcicle@proton.me>
2023-12-06 14:12:40 +0800

    clearer comment
    Co-authored-by: Matthias Görgens <matthias.goergens@mozak.com>

--- circuits/src/memory/columns.rs
+++ circuits/src/memory/columns.rs
@@ -56,7 +56,7 @@ make_col_map!(Memory);
 
 impl<F: RichField> From<&MemoryInit<F>> for Option<Memory<F>> {
     /// All other fields are intentionally set to defaults, and clk is
-    /// deliberately set to 1
+    /// deliberately set to one, to come after any zero-init rows.
     fn from(row: &MemoryInit<F>) -> Self {
         row.filter.is_one().then(|| Memory {
             is_writable: row.is_writable,
