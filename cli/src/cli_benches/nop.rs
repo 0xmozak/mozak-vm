ffdcc966ec5969aaf9bfd738764552dc5623765c
Kapil <39694928+codeblooded1729@users.noreply.github.com>
2023-12-06 11:51:37 +0530

    XOR benches in perftool (#909)
    Update: I decided to implement XOR and NOP benches separately for a
    while. Will implement functionality to combine the plots in a separate
    PR, and will have the plot comparing two benches featured there.
    
    This PR introduces XOR and NOP benches. This is mainly to measure how
    much extra overhead is incurred by a XOR instruction by comparing it to
    same set of instructions with XOR replaced by NOP
    
    ---------
    
    Co-authored-by: Matthias Görgens <matthias.goergens@mozak.com>

---
+++ cli/src/cli_benches/nop.rs
@@ -0,0 +1,51 @@
+use mozak_circuits::test_utils::prove_and_verify_mozak_stark;
+use mozak_runner::instruction::{Args, Instruction, Op, NOP};
+use mozak_runner::test_utils::simple_test_code;
+use starky::config::StarkConfig;
+
+#[allow(clippy::module_name_repetitions)]
+pub fn nop_bench(iterations: u32) -> Result<(), anyhow::Error> {
+    let instructions = [
+        Instruction {
+            op: Op::ADD,
+            args: Args {
+                rd: 1,
+                rs1: 1,
+                imm: 1_u32.wrapping_neg(),
+                ..Args::default()
+            },
+        },
+        NOP,
+        Instruction {
+            op: Op::BLT,
+            args: Args {
+                rs1: 0,
+                rs2: 1,
+                imm: 0,
+                ..Args::default()
+            },
+        },
+    ];
+    let (program, record) = simple_test_code(instructions, &[], &[(1, iterations)]);
+    prove_and_verify_mozak_stark(&program, &record, &StarkConfig::standard_fast_config())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::cli_benches::benches::{BenchArgs, BenchFunction};
+
+    #[test]
+    fn test_nop_bench() {
+        let iterations = 10;
+        super::nop_bench(iterations).unwrap();
+    }
+
+    #[test]
+    fn test_nop_bench_with_run() {
+        let iterations = 10;
+        let bench = BenchArgs {
+            function: BenchFunction::NopBench { iterations },
+        };
+        bench.run().unwrap();
+    }
+}
