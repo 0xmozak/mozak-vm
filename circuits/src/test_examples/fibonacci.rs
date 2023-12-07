8fff2ea0d520903a93f8b0da57321059834c0076
Supragya Raj <supragyaraj@gmail.com>
2023-12-07 12:25:48 +0530

    rename: MozakRunTimeArguments

--- circuits/src/test_examples/fibonacci.rs
+++ circuits/src/test_examples/fibonacci.rs
@@ -1,4 +1,4 @@
-use mozak_runner::elf::{MozakRunTimeArguments, Program};
+use mozak_runner::elf::{RunTimeArguments, Program};
 use mozak_runner::state::State;
 use mozak_runner::vm::step;
 use plonky2::field::goldilocks_field::GoldilocksField;
@@ -10,7 +10,7 @@ use crate::test_utils::ProveAndVerify;
 fn test_fibonacci() {
     let program = Program::load_program(
         mozak_examples::FIBONACCI_ELF,
-        &MozakRunTimeArguments::new(&[0; 32], &[], &[]),
+        &RunTimeArguments::new(&[0; 32], &[], &[]),
     )
     .unwrap();
     let state = State::<GoldilocksField>::new(program.clone());
