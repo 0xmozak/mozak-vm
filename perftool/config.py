189cd20dead5e2c3d6e43d6b89dbd035e3a9ff0a
codeblooded1729 <kapilpause007@gmail.com>
2023-12-07 19:37:23 +0530

    make config non global

--- perftool/config.py
+++ perftool/config.py
@@ -3,27 +3,32 @@ import json
 
 from path import CONFIG_JSON
 
-global config
-with open(CONFIG_JSON) as f:
-    config = json.load(f)
-    f.close()
+
+def load_config():
+    with open(CONFIG_JSON) as f:
+        return json.load(f)
 
 
 def get_benches_with_commit(bench_name: str) -> Dict[str, Dict[str, str]]:
+    config = load_config()
     return config[bench_name]["benches"]
 
 
 def get_elf(bench_name: str, bench_description: str) -> str | None:
+    config = load_config()
     return config[bench_name]["benches"][bench_description].get("elf")
 
 
 def get_parameter_name(bench_name: str) -> str:
+    config = load_config()
     return config[bench_name]["parameter"]
 
 
 def get_output_name(bench_name: str) -> str:
+    config = load_config()
     return config[bench_name]["output"]
 
 
 def get_description(bench_name: str) -> str:
+    config = load_config()
     return config[bench_name]["description"]
