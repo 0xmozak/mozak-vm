9a5458ade63b2050e4367aa68d2689e93f7e9d9c
Vivek Pandya <vivekvpandya@gmail.com>
2023-12-08 14:18:11 +0530

    Add conditional compilation checks for zkvm related code (#991)
    Part of PR #940

--- guest/src/env.rs
+++ guest/src/env.rs
@@ -1,15 +1,20 @@
+#[cfg(target_os = "zkvm")]
 extern crate alloc;
 
+#[cfg(target_os = "zkvm")]
 use alloc::vec::Vec;
 
+#[cfg(target_os = "zkvm")]
 static mut OUTPUT_BYTES: Option<Vec<u8>> = None;
 
+#[cfg(target_os = "zkvm")]
 pub fn init() {
     unsafe {
         OUTPUT_BYTES = Some(Vec::new());
     }
 }
 
+#[cfg(target_os = "zkvm")]
 pub fn finalize() {
     unsafe {
         let output_bytes_vec = OUTPUT_BYTES.as_ref().unwrap_unchecked();
@@ -19,6 +24,9 @@ pub fn finalize() {
 }
 
 pub fn write(output_data: &[u8]) {
+#[cfg(target_os = "zkvm")]
+    {
     let output_bytes_vec = unsafe { OUTPUT_BYTES.as_mut().unwrap_unchecked() };
     output_bytes_vec.extend_from_slice(output_data);
+    }
 }
--- guest/src/lib.rs
+++ guest/src/lib.rs
@@ -2,6 +2,7 @@
 
 extern crate alloc as rust_alloc;
 
+#[cfg(target_os = "zkvm")]
 mod alloc;
 pub mod env;
 pub mod hash;
@@ -10,8 +11,10 @@ pub mod hash;
 macro_rules! entry {
     ($path:path) => {
         // Type check the given path
+#[cfg(target_os = "zkvm")]
         const MOZAK_ENTRY: fn() = $path;
 
+#[cfg(target_os = "zkvm")]
         mod mozak_generated_main {
             #[no_mangle]
             fn main() { super::MOZAK_ENTRY() }
@@ -19,6 +22,7 @@ macro_rules! entry {
     };
 }
 
+#[cfg(target_os = "zkvm")]
 #[no_mangle]
 unsafe extern "C" fn __start() {
     env::init();
@@ -39,9 +43,11 @@ unsafe extern "C" fn __start() {
 //
 // For more details:
 // https://github.com/riscv-non-isa/riscv-elf-psabi-doc/blob/master/riscv-cc.adoc
+#[cfg(target_os = "zkvm")]
 static STACK_TOP: u32 = 0xFFFF_FFFF;
 
 // Entry point; sets up stack pointer and passes to __start.
+#[cfg(target_os = "zkvm")]
 core::arch::global_asm!(
 r#"
 .section .text._start;
