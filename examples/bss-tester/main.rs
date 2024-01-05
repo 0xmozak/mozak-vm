7c38f1ed333f20499faae73c969e3ddd7f68d40d
bing <bingcicle@keemail.me>
2024-01-05 10:05:26 +0800

    bss-tester: use guest::env::write to force halt

--- examples/bss-tester/main.rs
+++ examples/bss-tester/main.rs
@@ -1,13 +1,15 @@
 #![no_main]
 #![no_std]
-use core::{assert_eq};
+use core::assert_eq;
+
 // For BSS / SBSS
 // ref: https://stackoverflow.com/questions/40465933/how-do-i-write-rust-code-that-places-globals-statics-in-a-populated-bss-segmen
 static mut XYZ: [u8; 20] = [51; 20];
 
 pub fn main() {
-    unsafe{
+    unsafe {
         assert_eq!(XYZ[2], 51);
+        guest::env::write(&XYZ);
     }
 }
 
