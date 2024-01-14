ef53f29a13f0d6b2421e537ebe7c79b2309bdbfd
Ro Ma <dimdumon@outlook.com>
2024-01-14 12:43:43 +0200

    feat: mozak-ro-memory integration into memory-init

--- circuits/src/generation/memoryinit.rs
+++ circuits/src/generation/memoryinit.rs
@@ -8,6 +8,52 @@ use crate::utils::pad_trace_with_default;
 #[must_use]
 pub fn generate_memory_init_trace<F: RichField>(program: &Program) -> Vec<MemoryInit<F>> {
     let mut memory_inits: Vec<MemoryInit<F>> =
+        generate_memory_init_trace_without_mozak_ro_memory(program);
+    // extend memory init with new io-tapes mechanism
+    #[allow(clippy::unnecessary_operation)]
+    let _ = &program.mozak_ro_memory.is_some().then(|| {
+        let mozak_memory_inits: Vec<MemoryInit<F>> = [
+            &program
+                .mozak_ro_memory
+                .as_ref()
+                .unwrap()
+                .io_tape_public
+                .data,
+            &program
+                .mozak_ro_memory
+                .as_ref()
+                .unwrap()
+                .io_tape_private
+                .data,
+        ]
+        .iter()
+        .flat_map(|mem| {
+            mem.iter().map(move |(&addr, &value)| MemoryInit {
+                filter: F::ONE,
+                is_writable: F::ZERO,
+                element: MemElement {
+                    address: F::from_canonical_u32(addr),
+                    value: F::from_canonical_u8(value),
+                },
+            })
+        })
+        .collect();
+
+        memory_inits.extend(mozak_memory_inits.iter());
+    });
+
+    memory_inits.sort_by_key(|init| init.element.address.to_canonical_u64());
+
+    let trace = pad_trace_with_default(memory_inits);
+    log::trace!("MemoryInit trace {:?}", trace);
+    trace
+}
+
+#[must_use]
+pub fn generate_memory_init_trace_without_mozak_ro_memory<F: RichField>(
+    program: &Program,
+) -> Vec<MemoryInit<F>> {
+    let mut memory_inits: Vec<MemoryInit<F>> =
         [(F::ZERO, &program.ro_memory), (F::ONE, &program.rw_memory)]
             .iter()
             .flat_map(|&(is_writable, mem)| {
@@ -21,7 +67,6 @@ pub fn generate_memory_init_trace<F: RichField>(program: &Program) -> Vec<Memory
                 })
             })
             .collect();
-
     memory_inits.sort_by_key(|init| init.element.address.to_canonical_u64());
 
     let trace = pad_trace_with_default(memory_inits);
