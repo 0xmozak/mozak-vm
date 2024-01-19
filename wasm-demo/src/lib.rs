500cbeda075667e1e01d2f1f2fe4589d14f8a27a
Vivek Pandya <vivekvpandya@gmail.com>
2024-01-19 10:52:46 +0530

    Add support for running the demo with wasmtime (#1117)
    Co-authored-by: Jakub Zalewski <jakub.zalewski@mozak.com>

--- wasm-demo/src/lib.rs
+++ wasm-demo/src/lib.rs
@@ -32,3 +32,23 @@ pub fn wasm_demo(a: u32, b: u32) {
     let proving_res = prove_and_verify_mozak_stark(&e.0, &e.1, &config);
     alert(&format!("Proving :{}", proving_res.is_ok()));
 }
+
+
+pub fn wasm_demo_(a: u32, b: u32) {
+    let e = mozak_runner::util::execute_code(
+        [Instruction::new(Op::ADD, Args {
+            rd: 3,
+            rs1: 1,
+            rs2: 2,
+            ..Args::default()
+        })],
+        &[],
+        &[(1, a), (2, b)],
+    );
+    let res = mozak_runner::util::state_before_final(&e.1).get_register_value(3);
+    let config = StarkConfig::standard_fast_config();
+
+    println!("ADD {} {}, RES {}", a, b, res);
+    let proving_res = prove_and_verify_mozak_stark(&e.0, &e.1, &config);
+    println!("Proving :{}", proving_res.is_ok());
+}
