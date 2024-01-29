907fcc36e0d674c49aa62bd60d849287a77d977a
bing <bingcicle@keemail.me>
2024-01-29 16:27:59 +0800

    fix input

--- sdk/src/cpc.rs
+++ sdk/src/cpc.rs
@@ -11,7 +11,7 @@ lazy_static::lazy_static! {
 #[allow(unused_variables)]
 #[allow(unreachable_code)]
 #[must_use]
-pub fn cross_program_call<T>(program: ProgramIdentifier, method: u8, calldata: &[u8]) -> T
+pub fn cross_program_call<T>(program: ProgramIdentifier, method: u8, calldata: Vec<u8>) -> T
 where
     T: Sized + Default, {
     #[cfg(not(target_os = "zkvm"))]
@@ -23,7 +23,7 @@ where
 }
 
 #[cfg(not(target_os = "zkvm"))]
-pub fn globaltrace_add_message(program: ProgramIdentifier, method: u8, calldata: &[u8]) {
+pub fn globaltrace_add_message(program: ProgramIdentifier, method: u8, calldata: Vec<u8>) {
     use crate::coretypes::CPCMessage;
     let msg = CPCMessage {
         recipient_program: program,
