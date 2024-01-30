2aed536c95903315c0478858cbd5a5b1027ebc72
Vivek Pandya <vivekvpandya@gmail.com>
2024-01-30 14:33:03 +0530

    Add p3 ADD circuit and add BabyBearConfig

---
+++ circuits3/src/cpu/columns.rs
@@ -0,0 +1,13 @@
+use crate::columns_view::columns_view_impl;
+
+#[repr(C)]
+#[derive(Clone, Copy, Eq, PartialEq, Debug, Default)]
+pub struct Add<T> {
+    pub op1: [T; 4],
+    pub op2: [T; 4],
+    pub carry: [T; 3],
+    pub out: [T; 4],
+}
+
+// pub const NUM_ADD_COLS :usize = 15;
+columns_view_impl!(Add);
