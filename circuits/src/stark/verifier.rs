76845031d60f4171ad85413ea41b60de8cf9221f
Ro Ma <roman.melnikov@mozak.com>
2024-02-01 09:35:20 +0200

    feat: mozak-memory-init-trace cap (#1180)

--- circuits/src/stark/verifier.rs
+++ circuits/src/stark/verifier.rs
@@ -49,6 +49,14 @@ where
         "Mismatch between ElfMemoryInit trace caps"
     );
 
+    ensure!(
+        all_proof.proofs_with_metadata[TableKind::MozakMemoryInit]
+            .proof
+            .trace_cap
+            == all_proof.mozak_memory_init_trace_cap,
+        "Mismatch between MozakMemoryInit trace caps"
+    );
+
     let ctl_vars_per_table = CtlCheckVars::from_proofs(
         &all_proof.proofs_with_metadata,
         &mozak_stark.cross_table_lookups,
