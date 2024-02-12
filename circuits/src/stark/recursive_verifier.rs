cd3ae6ed481e421b2e6199a07ef31d1e655674dd
Sai Deng <deng.sai@gmail.com>
2024-02-12 12:22:30 -0800

    add ignore test

--- circuits/src/stark/recursive_verifier.rs
+++ circuits/src/stark/recursive_verifier.rs
@@ -626,7 +626,7 @@ mod tests {
     }
 
     #[test]
-    // #[ignore]
+    #[ignore]
     #[allow(clippy::too_many_lines)]
     fn same_circuit_verify_different_vm_proofs() -> Result<()> {
         let stark = S::default();
