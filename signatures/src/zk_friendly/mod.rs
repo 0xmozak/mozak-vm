2a095ffdd7112616c356a1746252188c7fb50934
codeblooded1729 <kapilpause007@gmail.com>
2024-02-12 18:54:49 +0530

    refactor poseidon

--- signatures/src/zk_friendly/mod.rs
+++ signatures/src/zk_friendly/mod.rs
@@ -1,5 +1,15 @@
+use anyhow::Result;
+use itertools::chain;
+use plonky2::field::extension::Extendable;
 use plonky2::field::goldilocks_field::GoldilocksField;
 use plonky2::field::types::Field;
+use plonky2::hash::hash_types::{HashOut, RichField};
+use plonky2::iop::target::Target;
+use plonky2::iop::witness::{PartialWitness, WitnessWrite};
+use plonky2::plonk::circuit_builder::CircuitBuilder;
+use plonky2::plonk::circuit_data::{CircuitConfig, CircuitData};
+use plonky2::plonk::config::{AlgebraicHasher, GenericConfig, GenericHashOut};
+use plonky2::plonk::proof::ProofWithPublicInputs;
 
 pub mod keccak256;
 pub mod poseidon;
@@ -10,6 +20,12 @@ pub const NUM_LIMBS_U8: usize = 32;
 /// This would be hash of 256 bit long private key
 pub struct PublicKey {
     limbs: [u8; NUM_LIMBS_U8],
+}
+
+impl From<HashOut<GoldilocksField>> for PublicKey {
+    fn from(hash: HashOut<GoldilocksField>) -> Self {
+        Self::new(hash.to_bytes().try_into().expect("should be 8 bytes long"))
+    }
 }
 
 /// 256 bit private key
@@ -38,3 +54,61 @@ macro_rules! impl_limbs {
 impl_limbs!(PublicKey);
 impl_limbs!(PrivateKey);
 impl_limbs!(Message);
+
+pub trait Signature<F, C, const D: usize>
+where
+    F: RichField + Extendable<D>,
+    C: GenericConfig<D, F = F>, {
+    type Sig: From<ProofWithPublicInputs<F, C, D>> + Into<ProofWithPublicInputs<F, C, D>>;
+    fn hash_private_key(private_key: &PrivateKey) -> HashOut<GoldilocksField>;
+    fn hash_circuit(
+        builder: &mut CircuitBuilder<F, D>,
+        private_key_target: [Target; NUM_LIMBS_U8],
+        public_key_target: [Target; NUM_LIMBS_U8],
+    );
+    fn sign(
+        config: CircuitConfig,
+        private_key: &PrivateKey,
+        public_key: &PublicKey,
+        msg: &Message,
+    ) -> (CircuitData<F, C, D>, Self::Sig)
+    where
+        C::Hasher: AlgebraicHasher<F>, {
+        let mut witness = PartialWitness::<F>::new();
+        let mut builder = CircuitBuilder::<F, D>::new(config);
+
+        // create targets
+        let private_key_target = builder.add_virtual_target_arr::<NUM_LIMBS_U8>();
+        let public_key_target = builder.add_virtual_target_arr::<NUM_LIMBS_U8>();
+        let msg_target = builder.add_virtual_target_arr::<NUM_LIMBS_U8>();
+
+        // convert inputs slices to field slices.
+        let private_key_field = private_key.get_limbs().map(|x| F::from_canonical_u8(x));
+        let public_key_field = public_key.get_limbs().map(|x| F::from_canonical_u8(x));
+        let msg_field = msg.get_limbs().map(|x| F::from_canonical_u8(x));
+
+        // set target values
+        witness.set_target_arr(&private_key_target, &private_key_field);
+        witness.set_target_arr(&public_key_target, &public_key_field);
+        witness.set_target_arr(&msg_target, &msg_field);
+
+        // range check each limb to be 8 bits
+        chain!(private_key_target, public_key_target, msg_target)
+            .for_each(|target_limb| builder.range_check(target_limb, 8));
+
+        Self::hash_circuit(&mut builder, private_key_target, public_key_target);
+
+        // public key and msg are public inputs
+        builder.register_public_inputs(&public_key_target);
+        builder.register_public_inputs(&msg_target);
+
+        builder.print_gate_counts(0);
+        let circuit = builder.build::<C>();
+        let proof = circuit.prove(witness).unwrap();
+        (circuit, proof.into())
+    }
+
+    fn verify(circuit: CircuitData<F, C, D>, sig: Self::Sig) -> Result<()> {
+        circuit.verify(sig.into())
+    }
+}
--- signatures/src/zk_friendly/poseidon.rs
+++ signatures/src/zk_friendly/poseidon.rs
@@ -1,89 +1,64 @@
-use itertools::chain;
+use std::marker::PhantomData;
+
 use plonky2::field::extension::Extendable;
 use plonky2::field::goldilocks_field::GoldilocksField;
 use plonky2::hash::hash_types::{HashOut, HashOutTarget, RichField};
 use plonky2::hash::poseidon::PoseidonHash;
 use plonky2::iop::target::Target;
-use plonky2::iop::witness::{PartialWitness, WitnessWrite};
 use plonky2::plonk::circuit_builder::CircuitBuilder;
-use plonky2::plonk::circuit_data::{CircuitConfig, CircuitData};
-use plonky2::plonk::config::{AlgebraicHasher, GenericConfig, GenericHashOut, Hasher};
+use plonky2::plonk::config::{GenericConfig, Hasher};
 use plonky2::plonk::proof::ProofWithPublicInputs;
 
-use super::{Message, PrivateKey, PublicKey, NUM_LIMBS_U8};
+use super::{PrivateKey, Signature, NUM_LIMBS_U8};
 
-impl From<HashOut<GoldilocksField>> for PublicKey {
-    fn from(hash: HashOut<GoldilocksField>) -> Self {
-        Self::new(hash.to_bytes().try_into().expect("should be 8 bytes long"))
-    }
-}
-
-impl PrivateKey {
-    // todo: customize hash
-    pub fn get_public_key(&self) -> PublicKey {
-        PoseidonHash::hash_or_noop(&self.get_limbs_field()).into()
-    }
-}
-
-pub fn sign_circuit<F: RichField + Extendable<D>, C: GenericConfig<D>, const D: usize>(
-    builder: &mut CircuitBuilder<F, D>,
-    private_key_target: [Target; NUM_LIMBS_U8],
-    public_key_target: [Target; NUM_LIMBS_U8],
-    msg_target: [Target; NUM_LIMBS_U8],
-) where
-    C::Hasher: AlgebraicHasher<F>, {
-    // range check each limb to be 8 bits
-    chain!(private_key_target, msg_target)
-        .for_each(|target_limb| builder.range_check(target_limb, 8));
-
-    // hash the private key
-    let hash_private_key = builder.hash_or_noop::<C::Hasher>(private_key_target.to_vec());
-    let public_key_as_hash = get_hashout(builder, &public_key_target);
-    // check hash(private_key) == public key
-    builder.connect_hashes(hash_private_key, public_key_as_hash);
-
-    // public key and msg are public inputs
-    builder.register_public_inputs(&public_key_target);
-    builder.register_public_inputs(&msg_target);
-}
-
-pub fn prove_sign<F: RichField + Extendable<D>, C: GenericConfig<D, F = F>, const D: usize>(
-    config: CircuitConfig,
-    private_key: &PrivateKey,
-    public_key: &PublicKey,
-    msg: &Message,
-) -> (CircuitData<F, C, D>, ProofWithPublicInputs<F, C, D>)
+pub struct ZkSigPoseidon<F, C, const D: usize>
 where
-    C::Hasher: AlgebraicHasher<F>, {
-    let mut witness = PartialWitness::<F>::new();
-    let mut builder = CircuitBuilder::<F, D>::new(config);
+    F: RichField + Extendable<D>,
+    C: GenericConfig<D, F = F>, {
+    signature: ProofWithPublicInputs<F, C, D>,
+}
 
-    // create targets
-    let private_key_target = builder.add_virtual_target_arr::<NUM_LIMBS_U8>();
-    let public_key_target = builder.add_virtual_target_arr::<NUM_LIMBS_U8>();
-    let msg_target = builder.add_virtual_target_arr::<NUM_LIMBS_U8>();
+impl<F, C, const D: usize> From<ProofWithPublicInputs<F, C, D>> for ZkSigPoseidon<F, C, D>
+where
+    F: RichField + Extendable<D>,
+    C: GenericConfig<D, F = F>,
+{
+    fn from(signature: ProofWithPublicInputs<F, C, D>) -> Self { Self { signature } }
+}
 
-    // convert inputs slices to field slices.
-    let private_key_field = private_key.get_limbs().map(|x| F::from_canonical_u8(x));
-    let public_key_field = public_key.get_limbs().map(|x| F::from_canonical_u8(x));
-    let msg_field = msg.get_limbs().map(|x| F::from_canonical_u8(x));
+impl<F, C, const D: usize> Into<ProofWithPublicInputs<F, C, D>> for ZkSigPoseidon<F, C, D>
+where
+    F: RichField + Extendable<D>,
+    C: GenericConfig<D, F = F>,
+{
+    fn into(self) -> ProofWithPublicInputs<F, C, D> { self.signature }
+}
 
-    // set target values
-    witness.set_target_arr(&private_key_target, &private_key_field);
-    witness.set_target_arr(&public_key_target, &public_key_field);
-    witness.set_target_arr(&msg_target, &msg_field);
+pub struct ZkSigPoseidonSigner<F, C, const D: usize> {
+    _phantom: (PhantomData<F>, PhantomData<C>),
+}
+impl<F, C, const D: usize> Signature<F, C, D> for ZkSigPoseidonSigner<F, C, D>
+where
+    F: RichField + Extendable<D>,
+    C: GenericConfig<D, F = F>,
+{
+    type Sig = ZkSigPoseidon<F, C, D>;
 
-    sign_circuit::<F, C, D>(
-        &mut builder,
-        private_key_target,
-        public_key_target,
-        msg_target,
-    );
+    fn hash_circuit(
+        builder: &mut CircuitBuilder<F, D>,
+        private_key_target: [Target; NUM_LIMBS_U8],
+        public_key_target: [Target; NUM_LIMBS_U8],
+    ) {
+        // hash the private key
+        let hash_private_key = builder.hash_or_noop::<PoseidonHash>(private_key_target.to_vec());
+        let public_key_as_hash = get_hashout(builder, &public_key_target);
+        // check hash(private_key) == public key
+        builder.connect_hashes(hash_private_key, public_key_as_hash);
+    }
 
-    builder.print_gate_counts(0);
-    let data = builder.build::<C>();
-    let proof = data.prove(witness).unwrap();
-    (data, proof)
+    fn hash_private_key(private_key: &PrivateKey) -> HashOut<GoldilocksField> {
+        PoseidonHash::hash_or_noop(&private_key.get_limbs_field())
+    }
 }
 
 pub fn get_hashout<F: RichField + Extendable<D>, const D: usize>(
@@ -111,11 +86,12 @@ mod tests {
     use plonky2::plonk::config::{GenericConfig, PoseidonGoldilocksConfig};
     use rand::Rng;
 
-    use super::{Message, PrivateKey, PublicKey};
-    use crate::zk_friendly::NUM_LIMBS_U8;
+    use crate::zk_friendly::poseidon::ZkSigPoseidonSigner;
+    use crate::zk_friendly::{Message, PrivateKey, PublicKey, Signature, NUM_LIMBS_U8};
     type C = PoseidonGoldilocksConfig;
     type F = <C as GenericConfig<2>>::F;
     const D: usize = 2;
+    type Signer = ZkSigPoseidonSigner<F, C, D>;
 
     fn generate_signature_data() -> (PrivateKey, PublicKey, Message) {
         let _ = env_logger::try_init();
@@ -124,7 +100,7 @@ mod tests {
         // generate random private key
         let private_key = PrivateKey::new(rng.gen::<[u8; NUM_LIMBS_U8]>());
         // get public key associated with private key
-        let public_key = private_key.get_public_key();
+        let public_key = Signer::hash_private_key(&private_key).into();
         // generate random message
         let msg = Message::new(rng.gen::<[u8; NUM_LIMBS_U8]>());
 
@@ -135,8 +111,8 @@ mod tests {
     fn test_signature() {
         let config = CircuitConfig::standard_recursion_zk_config();
         let (private_key, public_key, msg) = generate_signature_data();
-        let (data, proof) = super::prove_sign::<F, C, 2>(config, &private_key, &public_key, &msg);
-        assert!(data.verify(proof).is_ok());
+        let (circuit, zk_signature) = Signer::sign(config, &private_key, &public_key, &msg);
+        assert!(Signer::verify(circuit, zk_signature).is_ok());
     }
 
     #[test]
@@ -144,17 +120,16 @@ mod tests {
     fn test_tampering_public_key() {
         let config = CircuitConfig::standard_recursion_zk_config();
         let (private_key, public_key, msg) = generate_signature_data();
-        let (data, mut proof) =
-            super::prove_sign::<F, C, D>(config, &private_key, &public_key, &msg);
+        let (circuit, mut zk_signature) = Signer::sign(config, &private_key, &public_key, &msg);
 
         // assert public key is there in public inputs
         assert_eq!(
-            proof.public_inputs[..NUM_LIMBS_U8],
+            zk_signature.signature.public_inputs[..NUM_LIMBS_U8],
             public_key.get_limbs_field()
         );
         // tamper with public key
-        proof.public_inputs[0] = F::rand();
-        assert!(data.verify(proof).is_ok());
+        zk_signature.signature.public_inputs[0] = F::rand();
+        assert!(Signer::verify(circuit, zk_signature).is_ok());
     }
 
     #[test]
@@ -162,13 +137,15 @@ mod tests {
     fn test_tampering_message() {
         let config = CircuitConfig::standard_recursion_zk_config();
         let (private_key, public_key, msg) = generate_signature_data();
-        let (data, mut proof) =
-            super::prove_sign::<F, C, D>(config, &private_key, &public_key, &msg);
+        let (circuit, mut zk_signature) = Signer::sign(config, &private_key, &public_key, &msg);
 
         // assert msg is there in public inputs
-        assert_eq!(proof.public_inputs[NUM_LIMBS_U8..], msg.get_limbs_field());
+        assert_eq!(
+            zk_signature.signature.public_inputs[NUM_LIMBS_U8..],
+            msg.get_limbs_field()
+        );
         // tamper with msg
-        proof.public_inputs[NUM_LIMBS_U8] = F::rand();
-        assert!(data.verify(proof).is_ok());
+        zk_signature.signature.public_inputs[NUM_LIMBS_U8] = F::rand();
+        assert!(Signer::verify(circuit, zk_signature).is_ok());
     }
 }
