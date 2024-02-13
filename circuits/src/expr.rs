d1be8276a61c079174ab28188e84e06f9c3f9617
Jakub Zalewski <jakub.zalewski@mozak.com>
2024-02-13 17:47:59 +0100

    Flesh out expr a bit more

--- circuits/src/expr.rs
+++ circuits/src/expr.rs
@@ -22,6 +22,22 @@ pub enum Expr<V> {
     },
 }
 
+impl<V> Expr<V> {
+    fn sub(left: Self, right: Self) -> Self {
+        Self::SubExpr {
+            left: Rc::new(left),
+            right: Rc::new(right),
+        }
+    }
+
+    fn mul(left: Self, right: Self) -> Self {
+        Self::MulExpr {
+            left: Rc::new(left),
+            right: Rc::new(right),
+        }
+    }
+}
+
 impl<V> From<V> for Expr<V> {
     fn from(val: V) -> Self { Self::ConstExpr { val } }
 }
@@ -51,23 +67,81 @@ impl<V> Sub for Expr<V> {
     type Output = Expr<V>;
 
     fn sub(self, rhs: Self) -> Self::Output {
-        Expr::SubExpr {
-            left: Rc::new(self),
-            right: Rc::new(rhs),
-        }
+        Self::Output::sub(self, rhs)
     }
 }
+
+impl<'a, V> Sub<&'a Expr<V>> for Expr<V>
+where
+V: Clone, {
+    type Output = Expr<V>;
+
+    fn sub(self, rhs: &'a Self) -> Self::Output {
+        Self::Output::sub(self, rhs.clone())
+    }
+}
+
+
+impl<'a, V> Sub<Expr<V>> for &'a Expr<V>
+where
+    V: Clone, {
+    type Output = Expr<V>;
+
+    fn sub(self, rhs: Expr<V>) -> Self::Output {
+        Self::Output::sub(self.clone(), rhs)
+    }
+}
+
+
+impl<'a, V> Sub for &'a Expr<V>
+where
+    V: Clone, {
+    type Output = Expr<V>;
+
+    fn sub(self, rhs: &'a Expr<V>) -> Self::Output {
+        Self::Output::sub(self.clone(), rhs.clone())
+    }
+}
+
 
 impl<V> Mul for Expr<V> {
     type Output = Expr<V>;
 
     fn mul(self, rhs: Self) -> Self::Output {
-        Expr::MulExpr {
-            left: Rc::new(self),
-            right: Rc::new(rhs),
-        }
+        Self::Output::mul(self, rhs)
     }
 }
+
+impl<'a, V> Mul<&'a Expr<V>> for Expr<V>
+where
+V: Clone, {
+    type Output = Expr<V>;
+
+    fn mul(self, rhs: &'a Expr<V>) -> Self::Output {
+        Self::Output::mul(self, rhs.clone())
+    }
+}
+
+impl<'a, V> Mul<Expr<V>> for &'a Expr<V>
+where
+V: Clone, {
+    type Output = Expr<V>;
+
+    fn mul(self, rhs: Expr<V>) -> Self::Output {
+        Self::Output::mul(self.clone(), rhs)
+    }
+}
+
+impl<'a, V> Mul for &'a Expr<V>
+where
+V: Clone, {
+    type Output = Expr<V>;
+
+    fn mul(self, rhs: &'a Expr<V>) -> Self::Output {
+        Self::Output::mul(self.clone(), rhs.clone())
+    }
+}
+
 
 pub fn constraint_first_row<F, const D: usize>(
     yield_constr: &mut RecursiveConstraintConsumer<F, D>,
@@ -106,6 +180,60 @@ pub fn constraint_transition<F, const D: usize>(
 mod tests {
     use super::*;
 
+    // simple evaluator
+    fn eval(e: &Expr<i32>) -> i32 {
+        match e {
+            Expr::ConstExpr { val } => *val,
+            Expr::SubExpr { left, right } => {
+                let a = eval(left);
+                let b = eval(right);
+                a - b
+            },
+            Expr::MulExpr { left, right } => {
+                let a = eval(left);
+                let b = eval(right);
+                a * b
+           },
+        }
+    }
+
     #[test]
-    fn multiplication() { let _: Expr<_> = Expr::from(1) * Expr::from(2); }
+    fn multiplication() {
+        let a = Expr::from(1);
+        let b = Expr::from(2);
+
+        // a and b are moved
+        let _c = a.clone() * b.clone();
+        assert_eq!(eval(&_c), 2);
+
+        // a is cloned behind the scenes
+        let _c = &a * b.clone();
+        assert_eq!(eval(&_c), 2);
+
+        // b is cloned behind the scenes
+        let _c = a.clone() * &b;
+        assert_eq!(eval(&_c), 2);
+
+        // a and b are cloned behind the scenes
+        let _c = &a * &b;
+        assert_eq!(eval(&_c), 2);
+    }
+
+    #[test]
+    fn subtraction() {
+        let a = Expr::from(1);
+        let b = Expr::from(2);
+
+        // a and b are moved
+        let _c = a.clone() - b.clone();
+
+        // a is cloned behind the scenes
+        let _c = &a - b.clone();
+
+        // b is cloned behind the scenes
+        let _c = a.clone() - &b;
+
+        // a and b are cloned behind the scenes
+        let _c = &a - &b;
+    }
 }
