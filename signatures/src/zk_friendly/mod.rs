5882da7313eed3b2e609964dafdae0b996e59699
Kapil <39694928+codeblooded1729@users.noreply.github.com>
2024-02-14 10:35:06 +0530

    Feat: Add sha256 as choice of hash for zk friendly signature (#1234)

--- signatures/src/zk_friendly/mod.rs
+++ signatures/src/zk_friendly/mod.rs
@@ -1,5 +1,6 @@
 pub mod keccak256;
 pub mod poseidon;
+pub mod sha256;
 pub mod sig;
 pub mod test_utils;
 pub mod utils;
--- signatures/src/zk_friendly/utils.rs
+++ signatures/src/zk_friendly/utils.rs
@@ -22,7 +22,8 @@ pub fn get_hashout<F: RichField + Extendable<D>, const D: usize>(
     }
     hash_out_target
 }
-pub fn biguint_target_to_u8_target<F, const D: usize>(
+
+pub fn biguint_le_u32_target_to_le_u8_target<F, const D: usize>(
     builder: &mut CircuitBuilder<F, D>,
     biguint_target: &[U32Target; 8],
 ) -> [Target; NUM_LIMBS_U8]
@@ -35,6 +36,24 @@ where
         let u32_target = target_arr[4 * i..4 * i + 4]
             .iter()
             .rev()
+            .fold(zero, |acc, limb| builder.mul_add(acc, base, *limb));
+        builder.connect(u32_target, biguint_target[i].0);
+    }
+    target_arr
+}
+
+pub fn biguint_be_u32_target_to_le_u8_target<F, const D: usize>(
+    builder: &mut CircuitBuilder<F, D>,
+    biguint_target: &[U32Target; 8],
+) -> [Target; 32]
+where
+    F: RichField + Extendable<D>, {
+    let target_arr = builder.add_virtual_target_arr::<32>();
+    let zero = builder.zero();
+    let base = builder.constant(F::from_canonical_u16(1 << 8));
+    for i in 0..8 {
+        let u32_target = target_arr[4 * i..4 * i + 4]
+            .iter()
             .fold(zero, |acc, limb| builder.mul_add(acc, base, *limb));
         builder.connect(u32_target, biguint_target[i].0);
     }
