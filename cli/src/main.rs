f435337555d0c8fc7bf8b8a383e5f0057f9c492b
bing <bingcicle@keemail.me>
2024-02-20 13:02:31 +0800

    244 -> 256 rkyv

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -144,7 +144,7 @@ pub fn tapes_to_runtime_arguments(tape_bin: Input) -> mozak_runner::elf::Runtime
 
     let public_tape_bytes = rkyv::to_bytes::<_, 256>(&sys_tapes.public_tape).unwrap();
     let private_tape_bytes = rkyv::to_bytes::<_, 256>(&sys_tapes.private_tape).unwrap();
-    let call_tape_bytes = rkyv::to_bytes::<_, 244>(&sys_tapes.call_tape).unwrap();
+    let call_tape_bytes = rkyv::to_bytes::<_, 256>(&sys_tapes.call_tape).unwrap();
     let event_tape_bytes = rkyv::to_bytes::<_, 256>(&sys_tapes.event_tape).unwrap();
     debug!(
         "Read {} of public tape data.
