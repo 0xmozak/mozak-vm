09947040e9dfe41a5701777ccda790c89f48fbb6
bing <bingcicle@keemail.me>
2024-02-26 14:54:57 +0800

    log debug -> trace

--- runner/src/ecall.rs
+++ runner/src/ecall.rs
@@ -107,7 +107,7 @@ impl<F: RichField> State<F> {
 
     #[must_use]
     pub fn ecall(self) -> (Aux<F>, Self) {
-        log::debug!(
+        log::trace!(
             "ecall '{}' at clk: {}",
             ecall::log(self.get_register_value(REG_A0)),
             self.clk
