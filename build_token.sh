3ece3a81255e8279ea14777150af1de0f937f247
bing <bingcicle@keemail.me>
2024-02-26 14:11:26 +0800

    remove scripts

--- build_token.sh
+++
@@ -1,2 +0,0 @@
-cd examples && cargo build --bin tokenbin &&
-../target/debug/mozak-cli prove-and-verify -vvv target/riscv32im-mozak-zkvm-elf/debug/tokenbin --system-tape wallet_tfr.tape_bin
--- build_token_release.sh
+++
@@ -1,2 +0,0 @@
-cd examples && cargo build --release --bin tokenbin &&
-MOZAK_STARK_DEBUG=true ../target/debug/mozak-cli prove-and-verify -vvv target/riscv32im-mozak-zkvm-elf/release/tokenbin --system-tape wallet_tfr.tape_bin
