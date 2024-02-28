46bad067d1917fda032390937edc9b79c0d68a19
Ro Ma <roman.melnikov@mozak.com>
2024-02-28 11:52:06 +0200

    feat: make cli use `mozak-loader` (#1294)
    Tiny PR, only switch the CLI
    
    ---------
    
    Co-authored-by: Matthias Görgens <matthias.goergens@mozak.com>

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -47,7 +47,7 @@ struct Cli {
     debug: bool,
 }
 
-#[derive(Clone, Debug, Args)]
+#[derive(Clone, Debug, Args, Default)]
 pub struct RuntimeArguments {
     /// Private input.
     #[arg(long)]
@@ -133,11 +133,11 @@ enum Command {
     Bench(BenchArgs),
 }
 
-fn load_program(mut elf: Input) -> Result<Program> {
+fn load_program(mut elf: Input, args: RuntimeArguments) -> Result<Program> {
     let mut elf_bytes = Vec::new();
     let bytes_read = elf.read_to_end(&mut elf_bytes)?;
     debug!("Read {bytes_read} of ELF data.");
-    Program::vanilla_load_elf(&elf_bytes)
+    Program::mozak_load_program(&elf_bytes, &args.into())
 }
 
 /// Run me eg like `cargo run -- -vvv run vm/tests/testdata/rv32ui-p-addi
@@ -151,20 +151,18 @@ fn main() -> Result<()> {
         .init();
     match cli.command {
         Command::Decode { elf } => {
-            let program = load_program(elf)?;
+            let program = load_program(elf, RuntimeArguments::default())?;
             debug!("{program:?}");
         }
         Command::Run(RunArgs { elf, args }) => {
-            let program = load_program(elf)?;
-            let state =
-                State::<GoldilocksField>::legacy_ecall_api_new(program.clone(), args.into());
+            let program = load_program(elf, args.clone())?;
+            let state = State::<GoldilocksField>::new(program.clone(), args.into());
             let state = step(&program, state)?.last_state;
             debug!("{:?}", state.registers);
         }
         Command::ProveAndVerify(RunArgs { elf, args }) => {
-            let program = load_program(elf)?;
-            let state =
-                State::<GoldilocksField>::legacy_ecall_api_new(program.clone(), args.into());
+            let program = load_program(elf, args.clone())?;
+            let state = State::<GoldilocksField>::new(program.clone(), args.into());
             let record = step(&program, state)?;
             prove_and_verify_mozak_stark(&program, &record, &config)?;
         }
@@ -174,9 +172,8 @@ fn main() -> Result<()> {
             mut proof,
             recursive_proof,
         }) => {
-            let program = load_program(elf)?;
-            let state =
-                State::<GoldilocksField>::legacy_ecall_api_new(program.clone(), args.into());
+            let program = load_program(elf, args.clone())?;
+            let state = State::<GoldilocksField>::new(program.clone(), args.into());
             let record = step(&program, state)?;
             let stark = if cli.debug {
                 MozakStark::default_debug()
@@ -269,7 +266,7 @@ fn main() -> Result<()> {
             println!("Recursive VM proof verified successfully!");
         }
         Command::ProgramRomHash { elf } => {
-            let program = load_program(elf)?;
+            let program = load_program(elf, RuntimeArguments::default())?;
             let trace = generate_program_rom_trace(&program);
             let trace_poly_values = trace_rows_to_poly_values(trace);
             let rate_bits = config.fri_config.rate_bits;
@@ -286,7 +283,7 @@ fn main() -> Result<()> {
             println!("{trace_cap:?}");
         }
         Command::MemoryInitHash { elf } => {
-            let program = load_program(elf)?;
+            let program = load_program(elf, RuntimeArguments::default())?;
             let trace = generate_elf_memory_init_trace(&program);
             let trace_poly_values = trace_rows_to_poly_values(trace);
             let rate_bits = config.fri_config.rate_bits;
