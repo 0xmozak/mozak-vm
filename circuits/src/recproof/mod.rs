8d722a02cfdf96e1190ee19abb666639f814a4a0
Daniel Bloom <7810950-Daniel.Aaron.Bloom@users.noreply.gitlab.com>
2024-03-01 23:06:56 -0800

    feat: sub-circuit for accumulating events

--- circuits/src/recproof/mod.rs
+++ circuits/src/recproof/mod.rs
@@ -9,6 +9,7 @@ use plonky2::plonk::circuit_data::VerifierCircuitTarget;
 
 pub mod make_tree;
 pub mod merge;
+pub mod state_from_event;
 pub mod state_update;
 pub mod summarized;
 pub mod unbounded;
