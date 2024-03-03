636248825dae248af3a785e81ce8299317903e24
bing <bingcicle@keemail.me>
2024-03-03 23:09:30 +0800

    token(mozakvm): use new ArgsReturn

--- examples/token/main_mozak.rs
+++ examples/token/main_mozak.rs
@@ -10,9 +10,9 @@ use rkyv::Deserialize;
 
 pub fn main() {
     while let Some((msg, _idx)) = call_receive() {
-        let archived_args = unsafe { rkyv::archived_root::<MethodArgs>(&msg.args.0[..]) };
+        let archived_args = unsafe { rkyv::archived_root::<MethodArgs>(&msg.args_ret.args.0[..]) };
         let args: MethodArgs = archived_args.deserialize(&mut rkyv::Infallible).unwrap();
-        let archived_ret = unsafe { rkyv::archived_root::<MethodReturns>(&msg.ret.0[..]) };
+        let archived_ret = unsafe { rkyv::archived_root::<MethodReturns>(&msg.args_ret.ret.0[..]) };
         let ret: MethodReturns = archived_ret.deserialize(&mut rkyv::Infallible).unwrap();
 
         assert!(dispatch(args) == ret);
