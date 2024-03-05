d160488419d40a9fb9c6aba2050ce045a6c5a133
Matthias Görgens <matthias.goergens@gmail.com>
2024-03-05 11:27:58 +0800

    Works

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use rkyv::{Archive, Deserialize, Serialize};
 
-#[derive(Archive, Deserialize, Serialize, Debug, PartialEq)]
+#[derive(Archive, Clone, Deserialize, Serialize, Debug, PartialEq)]
 #[archive(
     // This will generate a PartialEq impl between our unarchived and archived
     // types:
@@ -17,6 +17,21 @@ struct Test {
     option: Option<Vec<i32>>,
 }
 
+#[derive(Archive, Clone, Deserialize, Serialize, Debug, PartialEq)]
+#[archive(
+    // This will generate a PartialEq impl between our unarchived and archived
+    // types:
+    compare(PartialEq),
+    // bytecheck can be used to validate your data if you want. To use the safe
+    // API, you have to derive CheckBytes for the archived type:
+    check_bytes,
+)]
+// Derives can be passed through to the generated type:
+#[archive_attr(derive(Debug))]
+struct TupleTest(Test, Vec<Test>);
+
+// type T = (Test, Vec<Test>);
+
 fn main() {
     let value = Test {
         int: 42,
@@ -24,26 +39,31 @@ fn main() {
         option: Some(vec![1, 2, 3, 4]),
     };
 
-    // Serializing is as easy as a single function call
-    let bytes = rkyv::to_bytes::<_, 256>(&value).unwrap();
+    // // Serializing is as easy as a single function call
+    // let bytes = rkyv::to_bytes::<_, 256>(&value).unwrap();
 
-    // Or you can customize your serialization for better performance
-    // and compatibility with #![no_std] environments
-    use rkyv::ser::{serializers::AllocSerializer, Serializer};
+    let t = TupleTest(value.clone(), vec![value.clone(), value.clone()]);
+    let bytes = rkyv::to_bytes::<_, 256>(&t).unwrap();    
 
-    let mut serializer = AllocSerializer::<0>::default();
-    serializer.serialize_value(&value).unwrap();
-    let bytes = serializer.into_serializer().into_inner();
+    // // Or you can customize your serialization for better performance
+    // // and compatibility with #![no_std] environments
+    // use rkyv::ser::{serializers::AllocSerializer, Serializer};
+
+    // let mut serializer = AllocSerializer::<0>::default();
+    // serializer.serialize_value(&value).unwrap();
+    // let bytes = serializer.into_serializer().into_inner();
 
     // You can use the safe API for fast zero-copy deserialization
-    let archived = rkyv::check_archived_root::<Test>(&bytes[..]).unwrap();
-    assert_eq!(archived, &value);
+    let archived = rkyv::check_archived_root::<TupleTest>(&bytes[..]).unwrap();
+    // let (a, b) = t;
+    assert_eq!(archived, &t);
 
-    // Or you can use the unsafe API for maximum performance
-    let archived = unsafe { rkyv::archived_root::<Test>(&bytes[..]) };
-    assert_eq!(archived, &value);
+    // // Or you can use the unsafe API for maximum performance
+    // let archived = unsafe { rkyv::archived_root::<Test>(&bytes[..]) };
+    // assert_eq!(archived, &value);
 
     // And you can always deserialize back to the original type
-    let deserialized: Test = archived.deserialize(&mut rkyv::Infallible).unwrap();
-    assert_eq!(deserialized, value);
+    let deserialized: TupleTest = archived.deserialize(&mut rkyv::Infallible).unwrap();
+    dbg!(&deserialized);
+    assert_eq!(deserialized, t);
 }
