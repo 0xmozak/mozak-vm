472d01c39c0c47e07c306d02f408708613f86099
Matthias Görgens <matthias.goergens@gmail.com>
2024-03-17 14:44:33 +0800

    Fix doc

--- circuits/src/columns_view.rs
+++ circuits/src/columns_view.rs
@@ -219,9 +219,8 @@ macro_rules! columns_view_impl {
 
 pub(crate) use columns_view_impl;
 
-/// Implement a static `MAP` of the `ColumnsView` from an array with length
-/// [`NumberOfColumns`] of the `ColumnsView` that allows for indexing into an
-/// array with the column name rather than the column index.
+/// Implement a static `MAP` of the `ColumnsView` that allows for indexing for
+/// crosstable lookups
 macro_rules! make_col_map {
     ($s: ident) => {
         // TODO: clean this up once https://github.com/rust-lang/rust/issues/109341 is resolved.
