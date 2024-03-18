eae9c2e16f28785239cc4be5da7db4c5c1cdc8e7
Matthias Görgens <matthias.goergens@gmail.com>
2024-03-18 12:11:08 +0800

    Mark violations

--- sdk/src/lib.rs
+++ sdk/src/lib.rs
@@ -4,6 +4,22 @@
 #![feature(trait_alias)]
 #![deny(warnings)]
 #![cfg_attr(target_os = "mozakvm", feature(restricted_std))]
+
+// TODO(Matthias): Remove these once the big sdk refactor is in.
+#![allow(clippy::cast_possible_truncation)]
+#![allow(clippy::default_trait_access)]
+#![allow(clippy::deref_addrof)]
+#![allow(clippy::explicit_iter_loop)]
+#![allow(clippy::implicit_clone)]
+#![allow(clippy::must_use_candidate)]
+#![allow(clippy::needless_borrows_for_generic_args)]
+#![allow(clippy::needless_pass_by_value)]
+#![allow(clippy::needless_return)]
+#![allow(clippy::ptr_arg)]
+#![allow(clippy::single_char_pattern)]
+#![allow(clippy::uninlined_format_args)]
+#![allow(clippy::unnecessary_join)]
+
 pub mod coretypes;
 pub mod io;
 #[cfg(not(target_os = "mozakvm"))]
