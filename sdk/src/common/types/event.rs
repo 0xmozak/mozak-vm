7706f64c9a2cf2364532ef64ed33a652bddaaa8a
codeblooded1729 <kapilpause007@gmail.com>
2024-03-20 13:13:13 +0530

    feat: add canonical hash for canonical event

--- sdk/src/common/types/event.rs
+++ sdk/src/common/types/event.rs
@@ -17,7 +17,7 @@
 #[allow(clippy::module_name_repetitions)]
 #[repr(u8)]
 pub enum EventType {
-    RWrite = 0,
+    Write = 0,
     Ensure,
     Read,
     Create,
@@ -87,6 +87,31 @@ impl CanonicalEvent {
             }
         }
     }
+
+    pub fn canonical_hash(&self) -> super::poseidon2hash::Poseidon2Hash {
+        #[cfg(not(target_os = "mozakvm"))]
+        {
+            let data_to_hash: Vec<u8> = itertools::chain!(
+                self.emitter.inner(),
+                u64::from(self.type_ as u8).to_le_bytes(),
+                self.address.inner(),
+                self.value.inner(),
+            )
+            .collect();
+            crate::native::helpers::poseidon2_hash_no_pad(&data_to_hash)
+        }
+        #[cfg(target_os = "mozakvm")]
+        {
+            let data_to_hash: Vec<u8> = itertools::chain!(
+                self.emitter.inner(),
+                u64::from(self.type_ as u8).to_le_bytes(),
+                self.address.inner(),
+                self.value.inner(),
+            )
+            .collect();
+            crate::mozakvm::helpers::poseidon2_hash_no_pad(&data_to_hash)
+        }
+    }
 }
 
 #[derive(
@@ -106,3 +131,97 @@ impl CanonicalEvent {
     derive(Debug, serde::Serialize, serde::Deserialize)
 )]
 pub struct CanonicalOrderedTemporalHints(pub CanonicalEvent, pub u32);
+
+#[cfg(test)]
+mod tests {
+    use crate::common::types::{
+        CanonicalEvent, EventType, Poseidon2Hash, ProgramIdentifier, StateAddress,
+    };
+    use crate::native::helpers::poseidon2_hash_no_pad;
+
+    #[test]
+    fn check_sample_events_hash() {
+        let alice = ProgramIdentifier([4, 8, 15, 16].into());
+        let zero_val = [0, 0, 0, 0].into();
+        let non_zero_val_1 = [3, 1, 4, 15].into();
+        let non_zero_val_2 = [1, 6, 180, 33].into();
+        let read_0 = CanonicalEvent {
+            address: StateAddress(42u64.to_le_bytes()),
+            emitter: alice,
+            type_: EventType::Read,
+            value: zero_val,
+        };
+        let write_1 = CanonicalEvent {
+            address: StateAddress(42u64.to_le_bytes()),
+            emitter: alice,
+            type_: EventType::Write,
+            value: non_zero_val_1,
+        };
+        let write_2 = CanonicalEvent {
+            address: StateAddress(42u64.to_le_bytes()),
+            emitter: alice,
+            type_: EventType::Write,
+            value: non_zero_val_2,
+        };
+        let read_0_hash: Poseidon2Hash = [
+            7272290939186032751,
+            8185818005188304227,
+            17555306369107993266,
+            17187284268557234321,
+        ]
+        .into();
+
+        let write_1_hash: Poseidon2Hash = [
+            11469795294276139037,
+            799622748573506082,
+            15272809121316752941,
+            7142640452443475716,
+        ]
+        .into();
+        let write_2_hash: Poseidon2Hash = [
+            1484423020241144842,
+            17207848040428508675,
+            7995793996020726058,
+            4658801606188332384,
+        ]
+        .into();
+
+        let branch_1_hash: Poseidon2Hash = [
+            16758566829994364981,
+            15311795646108582705,
+            12773152691662485878,
+            2551708493265210224,
+        ]
+        .into();
+        let branch_2_hash: Poseidon2Hash = [
+            8577138257922146843,
+            5112874340235798754,
+            4121828782781403483,
+            12250937462246573507,
+        ]
+        .into();
+
+        assert_eq!(read_0_hash, read_0.canonical_hash());
+        assert_eq!(write_1_hash, write_1.canonical_hash());
+        assert_eq!(write_2_hash, write_2.canonical_hash());
+
+        assert_eq!(
+            branch_1_hash,
+            poseidon2_hash_no_pad(
+                &itertools::chain!(
+                    write_1.canonical_hash().inner(),
+                    write_2.canonical_hash().inner()
+                )
+                .collect::<Vec<u8>>()
+            )
+        );
+
+        assert_eq!(
+            branch_2_hash,
+            poseidon2_hash_no_pad(
+                &itertools::chain!(read_0.canonical_hash().inner(), branch_1_hash.inner())
+                    .collect::<Vec<u8>>()
+            )
+        )
+    }
+}
