fd39c788a8b7d9940d3b1b67a326f7c6a5ad2b72
Supragya Raj <supragyaraj@gmail.com>
2024-03-22 00:11:41 +0530

    skip test

--- run_examples.sh
+++ run_examples.sh
@@ -23,7 +23,7 @@ for profile in "${PROFILES[@]}"; do
                     ;;
                 # For this, we skip without writing to skipped because we
                 # run the native version along with the mozakvm version.
-                "merkleproof-trustedroot-native" | "token-native" | "wallet-native" | "walletbin" | "tokenbin")
+                "merkleproof-trustedroot-native" | "token-native" | "wallet-native" | "walletbin" | "tokenbin" | "inputtapebin")
                     echo "(mozak-cli) skipping (${profile}): ${bin}"
                     continue
                     ;;
