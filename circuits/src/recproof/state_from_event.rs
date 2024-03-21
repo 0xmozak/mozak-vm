24813ca54488495bae6b8a741f18cdb6d9b42890
Daniel Bloom <7810950-Daniel.Aaron.Bloom@users.noreply.gitlab.com>
2024-03-21 01:23:09 -0700

    fix hash

--- circuits/src/recproof/state_from_event.rs
+++ circuits/src/recproof/state_from_event.rs
@@ -1003,7 +1003,6 @@ mod test {
         LEAF.circuit.verify(proof)?;
 
         let proof = LEAF.prove(Event {
-
             owner: program_hash_1,
             ty: EventType::CreditDelta,
             address: 200,
