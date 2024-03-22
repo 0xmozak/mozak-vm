080818bc5f899d5152637a8c15e281967d3ff499
Supragya Raj <supragyaraj@gmail.com>
2024-03-22 18:27:59 +0530

    zk proof and verify: core examples

--- run_examples.py
+++ run_examples.py
@@ -34,6 +34,21 @@ def is_sdk_dependency_only_on_core_features(cargo_file: str) -> bool:
     elif 'features' in sdk_dependency:
         return len(sdk_dependency['features']) == 0
 
+
+MOZAK_CLI_LOCATION = "target/release/mozak-cli"
+
+def build_mozak_cli():
+    if os.path.exists(MOZAK_CLI_LOCATION):
+        print(f"Found {Style.BRIGHT}{Fore.BLUE}mozak-cli{Style.RESET_ALL}: skipping build")
+        return
+    
+    mozak_cli_build_command = f"cargo build --release --bin mozak-cli"
+    print(f"Building {Style.BRIGHT}{Fore.BLUE}mozak-cli{Style.RESET_ALL}: {Fore.BLUE}{mozak_cli_build_command}{Style.RESET_ALL}")
+    os.system(mozak_cli_build_command)
+
+    if not os.path.exists(MOZAK_CLI_LOCATION):
+        raise Exception("cannot build mozak-cli")
+
 class ExamplesTester(unittest.TestCase):
     def test_workspace_members(self):
         actual_directories = set(list_directories("examples"))
@@ -41,13 +56,26 @@ class ExamplesTester(unittest.TestCase):
         self.assertEqual(actual_directories, listed_workspace_members)
     
     def test_core_only_examples(self):
-        actual_directories = set(list_directories("examples"))
-        for dir in actual_directories:
+        build_mozak_cli()
+        prove_and_verify_exceptions = {"panic"}  # TODO: check why `panic` doesn't work
+
+        for dir in set(list_directories("examples")):
             if is_sdk_dependency_only_on_core_features(f"examples/{dir}/Cargo.toml"):
-                print(f"{Style.BRIGHT}{Fore.BLUE}{dir}{Style.RESET_ALL} is detected core-only example; testing build")
-                command = f"cd examples && cargo build --release --bin {dir}"
-                print(f"Running: {Fore.BLUE}{command}{Style.RESET_ALL}")
-                self.assertEqual(os.system(command), 0)
+                print(f"{Style.BRIGHT}{Fore.BLUE}{dir}{Style.RESET_ALL} is detected core-only example")
+                
+                build_command = f"cd examples && cargo build --release --bin {dir}"
+                print(f"Testing build: {Fore.BLUE}{build_command}{Style.RESET_ALL}")
+                self.assertEqual(os.system(build_command), 0)
+                
+                if dir in prove_and_verify_exceptions:
+                    print(f"{Fore.RED}ZK prove and verify skipping for {Style.BRIGHT}{dir}{Style.NORMAL} as it is marked as an exception{Style.RESET_ALL}")
+                else:
+                    prove_and_verify_command = f'''{MOZAK_CLI_LOCATION} prove-and-verify \
+                        examples/target/riscv32im-mozak-mozakvm-elf/release/{dir} \
+                        --self-prog-id MZK-0000000000000000000000000000000000000000000000000000000000000001;'''
+                    print(f"ZK prove and verify: {Fore.BLUE}{prove_and_verify_command}{Style.RESET_ALL}")
+                    self.assertEqual(os.system(prove_and_verify_command), 0)
+
                 print("\n")
 
 if __name__ == "__main__":
