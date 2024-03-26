09374596d0ffd938069b7e43310500927f97d98c
codeblooded1729 <kapilpause007@gmail.com>
2024-03-26 11:19:04 +0530

    Simplify

--- circuits/src/cross_table_lookup.rs
+++ circuits/src/cross_table_lookup.rs
@@ -67,7 +67,7 @@ pub(crate) fn verify_cross_table_lookups<F: RichField + Extendable<D>, const D: 
     let mut ctl_zs_openings = ctl_zs_lasts.each_ref().map(|v| v.iter());
     let mut reduced_public_input_openings = reduced_public_inputs
         .each_ref()
-        .map(|v| v.clone().unwrap_or_default().into_iter());
+        .map(|v| v.iter().flat_map(IntoIterator::into_iter));
     for _ in 0..config.num_challenges {
         for CrossTableLookup {
             looking_tables,
@@ -93,8 +93,8 @@ pub(crate) fn verify_cross_table_lookups<F: RichField + Extendable<D>, const D: 
     for _ in 0..config.num_challenges {
         for MakeRowsPublic { table } in make_rows_pubilc {
             ensure!(
-                reduced_public_input_openings[table.kind].next().unwrap()
-                    == *ctl_zs_openings[table.kind].next().unwrap(),
+                reduced_public_input_openings[table.kind].next()
+                    == ctl_zs_openings[table.kind].next(),
                 "Open public verification failed for {:?} ",
                 table.kind,
             );
