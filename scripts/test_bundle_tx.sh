e6222a014d59e203d465dffe523ef4a0666ebd66
bing <bingcicle@keemail.me>
2024-03-26 16:47:26 +0800

    build in release

--- scripts/test_bundle_tx.sh
+++ scripts/test_bundle_tx.sh
@@ -2,12 +2,12 @@
 # This script tests transaction bundling.
 
 # Run natives and build mozakvm binaries
-cd examples/token && cargo run --release --features="native" --bin token-native --target aarch64-apple-darwin && cargo build --bin tokenbin
-cd ../wallet && cargo run --release --features="native" --bin wallet-native --target aarch64-apple-darwin && cargo build --bin walletbin
+cd examples/token && cargo run --release --features="native" --bin token-native --target aarch64-apple-darwin && cargo build --bin tokenbin --release
+cd ../wallet && cargo run --release --features="native" --bin wallet-native --target aarch64-apple-darwin && cargo build --bin walletbin --release
 
 # Run CLI
 cd ../../
-cargo run --bin mozak-cli -- bundle-transaction \
+cargo run --bin mozak-cli -- bundle-transaction -vvv \
     --cast-list MZK-b10da48cea4c09676b8e0efcd806941465060736032bb898420d0863dca72538 \
     --cast-list MZK-c51b8a31c98b9fe13065b485c9f8658c194c430843570ccac2720a3b30b47adb \
     --bundle-plan examples/token/out/token_tfr_bundle.json \
