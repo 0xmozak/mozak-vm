8ac7d524407224408d09fefb3494287274a595f4
Daniel Bloom <7810950-Daniel.Aaron.Bloom@users.noreply.gitlab.com>
2024-03-29 10:31:43 -0700

    fix: Remove all local (stack) copies of massive tables

--- circuits/src/cpu/columns.rs
+++ circuits/src/cpu/columns.rs
@@ -182,7 +182,7 @@ pub struct CpuState<T> {
     pub poseidon2_input_addr: T,
     pub poseidon2_input_len: T,
 }
-pub(crate) const CPU: CpuState<ColumnWithTypedInput<CpuColumnsExtended<i64>>> = COL_MAP.cpu;
+pub(crate) const CPU: &CpuState<ColumnWithTypedInput<CpuColumnsExtended<i64>>> = &COL_MAP.cpu;
 
 make_col_map!(CpuColumnsExtended);
 columns_view_impl!(CpuColumnsExtended);
@@ -269,7 +269,7 @@ pub fn signed_diff_extension_target<F: RichField + Extendable<D>, const D: usize
 /// [`CpuTable`](crate::cross_table_lookup::CpuTable).
 #[must_use]
 pub fn rangecheck_looking() -> Vec<TableWithTypedOutput<RangeCheckCtl<Column>>> {
-    let ops = CPU.inst.ops;
+    let ops = &CPU.inst.ops;
     let divs = ops.div + ops.rem + ops.srl + ops.sra;
     let muls: ColumnWithTypedInput<CpuColumnsExtended<i64>> = ops.mul + ops.mulh + ops.sll;
 
