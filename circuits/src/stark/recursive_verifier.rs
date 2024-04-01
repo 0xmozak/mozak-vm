71e698877e8b2c663ee18c6a5061b50c639968ef
Sai Deng <deng.sai@gmail.com>
2024-04-01 10:25:07 -0700

    comments

--- circuits/src/stark/recursive_verifier.rs
+++ circuits/src/stark/recursive_verifier.rs
@@ -39,9 +39,10 @@ use crate::stark::proof::{
 /// Plonky2's recursion threshold is 2^12 gates.
 pub const VM_RECURSION_THRESHOLD_DEGREE_BITS: usize = 12;
 /// Public inputs (number of Goldilocks elements) using
-/// standard_recursion_config:
+/// `standard_recursion_config`:
 ///   `entry_point`: 1
-///   `Program trace cap`: 64
+///   `Program trace cap`: 16 (hash count with 'cap_height' = 4) * 4 (size of a
+///                          hash) = 64
 ///   `ElfMemoryInit trace cap`: 64
 ///   `MozakMemoryInit trace cap`: 64
 pub const VM_PUBLIC_INPUT_SIZE: usize = 1 + 64 + 64 + 64;
