e774442353939e7be489fde1a197a44f15bebecf
Matthias Görgens <matthias.goergens@gmail.com>
2024-04-04 13:53:55 +0800

    Clippy

--- circuits/src/stark/mozak_stark.rs
+++ circuits/src/stark/mozak_stark.rs
@@ -303,6 +303,7 @@ macro_rules! mozak_stark_helpers {
         }
         pub(crate) use all_starks;
 
+        #[allow(unused_macros)]
         macro_rules! all_starks_par {
             ($all_stark:expr, |$stark:ident, $kind:ident| $val:expr) => {{
                 use core::borrow::Borrow;
@@ -318,6 +319,7 @@ macro_rules! mozak_stark_helpers {
                 ]).par_map(|f| f())
             }};
         }
+        #[allow(unused_imports)]
         pub(crate) use all_starks_par;
 
     };
