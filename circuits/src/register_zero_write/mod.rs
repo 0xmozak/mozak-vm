517289ad11bb97b774d597d455f70fc6170fc126
Matthias Görgens <matthias.goergens@gmail.com>
2024-04-05 16:45:50 +0800

    Delet

--- circuits/src/register_zero_write/mod.rs
+++
@@ -1,7 +0,0 @@
-//! This module contains the **`RegisterZero` STARK Table**.
-//!
-//! This is a helper for the `Register` STARK Table,
-//! to deal with register 0.  Register 0 accepts any writes of any value,
-//! but always reads as 0.
-pub mod columns;
-pub mod stark;
