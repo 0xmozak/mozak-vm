cf319f236aa72521f0098715b65f965f1ed8e500
bing <bingcicle@keemail.me>
2024-04-05 15:12:01 +0800

    register_zero_read: remove needless testing

--- circuits/src/register/zero_read/stark.rs
+++ circuits/src/register/zero_read/stark.rs
@@ -53,29 +53,3 @@ impl<F: RichField + Extendable<D>, const D: usize> Stark<F, D> for RegisterZeroR
 
     fn constraint_degree(&self) -> usize { 3 }
 }
-
-#[cfg(test)]
-mod tests {
-    use anyhow::Result;
-    use plonky2::plonk::config::{GenericConfig, Poseidon2GoldilocksConfig};
-    use starky::stark_testing::{test_stark_circuit_constraints, test_stark_low_degree};
-
-    use super::RegisterZeroReadStark;
-
-    const D: usize = 2;
-    type C = Poseidon2GoldilocksConfig;
-    type F = <C as GenericConfig<D>>::F;
-    type S = RegisterZeroReadStark<F, D>;
-
-    #[test]
-    fn test_degree() -> Result<()> {
-        let stark = S::default();
-        test_stark_low_degree(stark)
-    }
-
-    #[test]
-    fn test_circuit() -> Result<()> {
-        let stark = S::default();
-        test_stark_circuit_constraints::<F, C, S, D>(stark)
-    }
-}
