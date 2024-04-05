c4cb611053767ddded7fda9e27879fb30276512d
Matthias Görgens <matthias.goergens@gmail.com>
2024-04-05 22:46:46 +0800

    Clippy

--- circuits/src/cpu/stark.rs
+++ circuits/src/cpu/stark.rs
@@ -265,7 +265,7 @@ impl<F: RichField + Extendable<D>, const D: usize> Stark<F, D> for CpuStark<F, D
         let lv: &CpuState<_> = vars.get_local_values().into();
         let nv: &CpuState<_> = vars.get_next_values().into();
         let public_inputs: &PublicInputs<_> = vars.get_public_inputs().into();
-        let _challenge = self.conjunctive_challenge;
+        // let _challenge = self.conjunctive_challenge;
 
         yield_constr.constraint_first_row(lv.inst.pc - public_inputs.entry_point);
         clock_ticks(lv, nv, yield_constr);
@@ -308,7 +308,7 @@ impl<F: RichField + Extendable<D>, const D: usize> Stark<F, D> for CpuStark<F, D
         let lv: &CpuState<_> = vars.get_local_values().into();
         let nv: &CpuState<_> = vars.get_next_values().into();
         let public_inputs: &PublicInputs<_> = vars.get_public_inputs().into();
-        let _alphas = self.conjunctive_challenge;
+        // let _alphas = self.conjunctive_challenge;
 
         let inst_pc_sub_public_inputs_entry_point =
             builder.sub_extension(lv.inst.pc, public_inputs.entry_point);
