ecc334e143640d1ad7f2b4115795f94175304d60
Matthias Görgens <matthias.goergens@gmail.com>
2024-04-05 08:44:08 +0800

    Update TODO

--- circuits/src/cpu/columns.rs
+++ circuits/src/cpu/columns.rs
@@ -106,7 +106,7 @@ pub struct CpuState<T> {
     // Represents the end of the program. Also used as the filter column for cross checking Program
     // ROM instructions.
     pub is_running: T,
-    // TODO(Matthias): see if we can remove this.
+    // TODO(Matthias): we can remove this, once our 'halt' instruction is in its own table.
     pub new_is_running: T,
 
     pub op1_value: T,
