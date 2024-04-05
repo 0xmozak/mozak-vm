13b196b006b6e47f14c05f41f9b3a1091170190f
Matthias Görgens <matthias.goergens@mozak.com>
2024-04-05 21:54:55 +0800

    Update circuits/src/cpu/stark.rs

--- circuits/src/cpu/stark.rs
+++ circuits/src/cpu/stark.rs
@@ -265,7 +265,6 @@ impl<F: RichField + Extendable<D>, const D: usize> Stark<F, D> for CpuStark<F, D
         P: PackedField<Scalar = FE>, {
         let lv: &CpuState<_> = vars.get_local_values().into();
         let nv: &CpuState<_> = vars.get_next_values().into();
-        // TODO: only one alpha at a time!
         let public_inputs: &PublicInputs<_> = vars.get_public_inputs()[1..].into();
         let _alphas = &vars.get_public_inputs()[..1];
 
