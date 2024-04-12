91954f0f716f62f933f3f74d9d8123710da6ec73
Matthias Goergens <matthias.goergens@gmail.com>
2024-04-12 17:43:16 +0800

    Convert more

--- circuits/src/ops/add/stark.rs
+++ circuits/src/ops/add/stark.rs
@@ -1,5 +1,6 @@
 use std::marker::PhantomData;
 
+use expr::{Expr, ExprBuilder, StarkFrameTyped};
 use mozak_circuits_derive::StarkNameDisplay;
 use plonky2::field::extension::{Extendable, FieldExtension};
 use plonky2::field::packed::PackedField;
@@ -7,11 +8,12 @@ use plonky2::hash::hash_types::RichField;
 use plonky2::iop::ext_target::ExtensionTarget;
 use plonky2::plonk::circuit_builder::CircuitBuilder;
 use starky::constraint_consumer::{ConstraintConsumer, RecursiveConstraintConsumer};
-use starky::evaluation_frame::{StarkEvaluationFrame, StarkFrame};
+use starky::evaluation_frame::StarkFrame;
 use starky::stark::Stark;
 
 use super::columns::Add;
 use crate::columns_view::{HasNamedColumns, NumberOfColumns};
+use crate::expr::{build_ext, build_packed, ConstraintBuilder};
 
 #[derive(Copy, Clone, Default, StarkNameDisplay)]
 #[allow(clippy::module_name_repetitions)]
@@ -26,6 +28,23 @@ impl<F, const D: usize> HasNamedColumns for AddStark<F, D> {
 const COLUMNS: usize = Add::<()>::NUMBER_OF_COLUMNS;
 const PUBLIC_INPUTS: usize = 0;
 
+fn generate_constraints<'a, T: Copy, U>(
+    vars: &StarkFrameTyped<Add<Expr<'a, T>>, Vec<U>>,
+) -> ConstraintBuilder<Expr<'a, T>> {
+    let lv = vars.local_values;
+    let mut constraints = ConstraintBuilder::default();
+
+    let added = lv.op1_value + lv.op2_value + lv.inst.imm_value;
+    let wrapped = added - (1 << 32);
+
+    // Check: the resulting sum is wrapped if necessary.
+    // As the result is range checked, this make the choice deterministic,
+    // even for a malicious prover.
+    constraints.always((lv.dst_value - added) * (lv.dst_value - wrapped));
+
+    constraints
+}
+
 impl<F: RichField + Extendable<D>, const D: usize> Stark<F, D> for AddStark<F, D> {
     type EvaluationFrame<FE, P, const D2: usize> = StarkFrame<P, P::Scalar, COLUMNS, PUBLIC_INPUTS>
 
@@ -38,28 +57,25 @@ impl<F: RichField + Extendable<D>, const D: usize> Stark<F, D> for AddStark<F, D
     fn eval_packed_generic<FE, P, const D2: usize>(
         &self,
         vars: &Self::EvaluationFrame<FE, P, D2>,
-        yield_constr: &mut ConstraintConsumer<P>,
+        constraint_consumer: &mut ConstraintConsumer<P>,
     ) where
         FE: FieldExtension<D2, BaseField = F>,
         P: PackedField<Scalar = FE>, {
-        let lv: &Add<P> = vars.get_local_values().into();
-        let wrap_at = P::Scalar::from_noncanonical_u64(1 << 32);
-        let added = lv.op1_value + lv.op2_value + lv.inst.imm_value;
-        let wrapped = added - wrap_at;
-
-        // Check: the resulting sum is wrapped if necessary.
-        // As the result is range checked, this make the choice deterministic,
-        // even for a malicious prover.
-        yield_constr.constraint((lv.dst_value - added) * (lv.dst_value - wrapped));
+        let expr_builder = ExprBuilder::default();
+        let vars = expr_builder.to_typed_starkframe(vars);
+        let constraints = generate_constraints(&vars);
+        build_packed(constraints, constraint_consumer);
     }
 
     fn eval_ext_circuit(
         &self,
-        _builder: &mut CircuitBuilder<F, D>,
-        _vars: &Self::EvaluationFrameTarget,
-        _yield_constr: &mut RecursiveConstraintConsumer<F, D>,
+        circuit_builder: &mut CircuitBuilder<F, D>,
+        vars: &Self::EvaluationFrameTarget,
+        constraint_consumer: &mut RecursiveConstraintConsumer<F, D>,
     ) {
-        todo!()
+        let expr_builder = ExprBuilder::default();
+        let constraints = generate_constraints(&expr_builder.to_typed_starkframe(vars));
+        build_ext(constraints, circuit_builder, constraint_consumer);
     }
 
     fn constraint_degree(&self) -> usize { 3 }
