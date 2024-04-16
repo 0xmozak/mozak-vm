7fcd7dbcaeed8294c0dc4127c65ea6b0664d9be6
bing <bingcicle@mozak.com>
2024-04-16 15:51:50 +0800

    [suggestion] runner: use fixed size tape commitments everywhere (#1566)
    Based on #1499 
    
    Fixes usage of non-fixed size commitment tapes.

--- runner/src/elf.rs
+++ runner/src/elf.rs
@@ -12,6 +12,7 @@ use elf::symbol::SymbolTable;
 use elf::ElfBytes;
 use im::hashmap::HashMap;
 use itertools::{chain, iproduct, izip, Itertools};
+use mozak_sdk::core::ecall::COMMITMENT_SIZE;
 use serde::{Deserialize, Serialize};
 
 use crate::code::Code;
@@ -217,8 +218,8 @@ impl MozakMemory {
 #[derive(Clone, Debug, Default, Serialize, Deserialize)]
 pub struct RuntimeArguments {
     pub self_prog_id: Vec<u8>,
-    pub events_commitment_tape: Vec<u8>,
-    pub cast_list_commitment_tape: Vec<u8>,
+    pub events_commitment_tape: [u8; COMMITMENT_SIZE],
+    pub cast_list_commitment_tape: [u8; COMMITMENT_SIZE],
     pub cast_list: Vec<u8>,
     pub io_tape_private: Vec<u8>,
     pub io_tape_public: Vec<u8>,
@@ -623,12 +624,13 @@ mod test {
     #[test]
     fn test_mozak_load_program() {
         let data = vec![0, 1, 2, 3];
+        let commitment_data = [0; 32];
 
         let mozak_ro_memory =
             Program::mozak_load_program(mozak_examples::EMPTY_ELF, &RuntimeArguments {
                 self_prog_id: data.clone(),
-                events_commitment_tape: data.clone(),
-                cast_list_commitment_tape: data.clone(),
+                events_commitment_tape: commitment_data,
+                cast_list_commitment_tape: commitment_data,
                 cast_list: data.clone(),
                 io_tape_private: data.clone(),
                 io_tape_public: data.clone(),
@@ -642,11 +644,11 @@ mod test {
         assert_eq!(mozak_ro_memory.self_prog_id.data.len(), data.len());
         assert_eq!(
             mozak_ro_memory.events_commitment_tape.data.len(),
-            data.len()
+            commitment_data.len()
         );
         assert_eq!(
             mozak_ro_memory.cast_list_commitment_tape.data.len(),
-            data.len()
+            commitment_data.len()
         );
         assert_eq!(mozak_ro_memory.cast_list.data.len(), data.len());
         assert_eq!(mozak_ro_memory.io_tape_private.data.len(), data.len());
