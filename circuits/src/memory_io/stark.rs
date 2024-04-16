89e089a6aea97da82bbee0cff3edae321f9c1aea
bing <bingcicle@keemail.me>
2024-04-16 10:52:49 +0800

    fix(memory-io): read public tape, not call tape

--- circuits/src/memory_io/stark.rs
+++ circuits/src/memory_io/stark.rs
@@ -260,7 +260,7 @@ mod tests {
             &[(address, 0)],
             &[
                 // TODO: this looks like a bug, it should be IO_READ_PUBLIC?
-                (REG_A0, ecall::IO_READ_CALL_TAPE),
+                (REG_A0, ecall::IO_READ_PUBLIC),
                 (REG_A1, address), // A1 - address
                 (REG_A2, 1),       // A2 - size
             ],
