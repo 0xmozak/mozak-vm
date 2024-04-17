1720ee4fc921cd475e57d9c12301100b6abf512a
Matthias Goergens <matthias.goergens@gmail.com>
2024-04-17 14:09:09 +0800

    Create random and sort bench

---
+++ examples/mozak-sort/main.rs
@@ -0,0 +1,25 @@
+#![cfg_attr(target_os = "mozakvm", no_main)]
+#![cfg_attr(not(feature = "std"), no_std)]
+
+
+use core::hint::black_box;
+// import vec in no std
+extern crate alloc;
+use alloc::vec::Vec;
+
+extern crate rand;
+use rand::{Rng, rngs::SmallRng, SeedableRng};
+
+fn main() {
+    // let mut rng = rand::thread_rng();
+    let mut rng = SmallRng::seed_from_u64(0xdead_beef_feed_cafe);
+
+    // TODO: take n from tape.
+    let n = 100;
+    let mut v: Vec<u32> = (0..n).map(|_| rng.gen()).collect();
+
+    black_box(v.sort());
+}
+
+
+mozak_sdk::entry!(main);
