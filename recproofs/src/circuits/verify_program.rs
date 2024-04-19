e90e1f5fcdd72d3929ae36d0b1fb5785be6c0dfe
Daniel Bloom <7810950-Daniel.Aaron.Bloom@users.noreply.gitlab.com>
2024-04-19 01:19:19 -0700

    cast

--- recproofs/src/circuits/verify_program.rs
+++ recproofs/src/circuits/verify_program.rs
@@ -1,19 +1,20 @@
 //! Circuits for proving events correspond to a proof
 
 use plonky2::field::extension::Extendable;
-use plonky2::hash::hash_types::{HashOutTarget, RichField};
+use plonky2::hash::hash_types::{HashOutTarget, RichField, NUM_HASH_OUT_ELTS};
 use plonky2::plonk::circuit_builder::CircuitBuilder;
 use plonky2::plonk::circuit_data::{CircuitConfig, CircuitData};
 use plonky2::plonk::config::{AlgebraicHasher, GenericConfig};
 use plonky2::plonk::proof::ProofWithPublicInputsTarget;
 
-use crate::subcircuits::{unbounded, unpruned};
+use super::build_event_root;
+use crate::subcircuits::{propagate, unbounded, unpruned};
 
 pub mod core;
 
 pub struct LeafTargets<const D: usize> {
     /// The proof of event accumulation
-    pub build_event_root: ProofWithPublicInputsTarget<D>,
+    pub event_root: ProofWithPublicInputsTarget<D>,
 }
 
 pub struct LeafCircuit<F, C, const D: usize>
@@ -29,6 +30,9 @@ where
     /// The program identifier
     pub program_id: unpruned::LeafSubCircuit,
 
+    /// The cast list
+    pub cast_list: propagate::LeafSubCircuit<NUM_HASH_OUT_ELTS>,
+
     pub circuit: CircuitData<F, C, D>,
 }
 
@@ -39,17 +43,23 @@ where
     <C as GenericConfig<D>>::Hasher: AlgebraicHasher<F>,
 {
     #[must_use]
-    pub fn new(circuit_config: &CircuitConfig, program: &impl core::Circuit<F, C, D>) -> Self {
+    pub fn new(
+        circuit_config: &CircuitConfig,
+        program: &impl core::Circuit<F, C, D>,
+        _event_root: build_event_root::BranchCircuit<F, C, D>,
+    ) -> Self {
         let mut builder = CircuitBuilder::<F, D>::new(circuit_config.clone());
 
         let unbounded_inputs = unbounded::SubCircuitInputs::default(&mut builder);
         let program_verifier_inputs = core::SubCircuitInputs::default(&mut builder);
         let program_id_inputs = unpruned::SubCircuitInputs::default(&mut builder);
+        let cast_list_inputs = propagate::SubCircuitInputs::default(&mut builder);
 
         let unbounded_targets = unbounded_inputs.build_leaf::<F, C, D>(&mut builder);
         let program_verifier_targets =
             program_verifier_inputs.build_leaf::<F, C, D>(&mut builder, program);
         let program_id_targets = program_id_inputs.build_leaf::<F, D>(&mut builder);
+        let cast_list_targets = cast_list_inputs.build_leaf::<F, D>(&mut builder);
 
         builder.connect_hashes(
             HashOutTarget {
@@ -64,11 +74,13 @@ where
         let unbounded = unbounded_targets.build(public_inputs);
         let program_verifier = program_verifier_targets.build(public_inputs);
         let program_id = program_id_targets.build(public_inputs);
+        let cast_list = cast_list_targets.build(public_inputs);
 
         Self {
             unbounded,
             program_verifier,
             program_id,
+            cast_list,
             circuit,
         }
     }
