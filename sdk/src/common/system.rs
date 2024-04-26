7a7a389db784955ad9edddf031b5a829fe2b181e
bing <bingcicle@keemail.me>
2024-04-26 10:11:40 +0800

    sdk(systemtapes): do not init with preinit memory
    Also delete all code related to initialization with preinit memory -
    this includes all helpers and other rkyv code.

--- sdk/src/common/system.rs
+++ sdk/src/common/system.rs
@@ -4,20 +4,11 @@ use core::cell::RefCell;
 use std::rc::Rc;
 
 use once_cell::unsync::Lazy;
-#[cfg(target_os = "mozakvm")]
-use rkyv::Deserialize;
 
 use super::types::{
     CallTapeType, EventTapeType, PrivateInputTapeType, PublicInputTapeType, SystemTape,
 };
 #[cfg(target_os = "mozakvm")]
-use crate::common::types::{CanonicalOrderedTemporalHints, CrossProgramCall, ProgramIdentifier};
-#[cfg(target_os = "mozakvm")]
-use crate::mozakvm::helpers::{
-    archived_repr, get_rkyv_archived, get_rkyv_deserialized, get_self_prog_id,
-};
-#[cfg(target_os = "mozakvm")]
-use crate::mozakvm::linker_symbols::{_mozak_call_tape, _mozak_cast_list, _mozak_event_tape};
 
 /// `SYSTEM_TAPE` is a global singleton for interacting with
 /// all the `IO-Tapes`, `CallTape` and the `EventTape` both in
@@ -61,23 +52,11 @@ pub(crate) static mut SYSTEM_TAPE: Lazy<SystemTape> = Lazy::new(|| {
     // pre-populated data elements
     #[cfg(target_os = "mozakvm")]
     {
-        let events = get_rkyv_archived!(Vec<CanonicalOrderedTemporalHints>, _mozak_event_tape);
-
         SystemTape {
             private_input_tape: PrivateInputTapeType::default(),
             public_input_tape: PublicInputTapeType::default(),
-            call_tape: CallTapeType {
-                self_prog_id: get_self_prog_id(),
-                cast_list: get_rkyv_deserialized!(Vec<ProgramIdentifier>, _mozak_cast_list),
-                reader: Some(get_rkyv_archived!(Vec<CrossProgramCall>, _mozak_call_tape)),
-                index: 0,
-            },
-            event_tape: EventTapeType {
-                self_prog_id: get_self_prog_id(),
-                reader: Some(events),
-                seen: vec![false; events.len()],
-                index: 0,
-            },
+            call_tape: CallTapeType::default(),
+            event_tape: EventTapeType::default(),
         }
     }
 });
