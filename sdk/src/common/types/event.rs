87a3c28450303a4d182869215bdf3fe8ae622dfc
Kapil <39694928+codeblooded1729@users.noreply.github.com>
2024-04-26 17:58:03 +0530

    feat: Canonical hashing of EventTape in sdk (#1400)
    This PR introduces a `Canonical` way to hash event tape, that is
    ingested into recproofs. It has two components, which include hashing an
    `Event` itself, and imposing an order on hashes and a way to `merkelize`
    it to root hash.
    
    ### Canonical hashing an `Event`
    This hash is obtained as flat hash  in that order, of
    - `ProgramIdentifer` of event emitter which is `[u8; 32]`
    - `EventType` represented as `[u8; 8]` (To be consistent with recproofs
    for a while, namely `split_bytes` function, which is used to split a
    `Target` into 8 bytes)
    - `Address` of state object being referenced in the event. For now, to
    be in sync with recproofs, we let this to be a `u64` represented as
    `[u8; 8]` in little endian order.
    - Hash of event's `value`, whichis `[u8; 32]`
    
    The flat hash is without padding, since it fits into `WIDTH` already. We
    don't worry about hash collisions, since we are more interested in
    proving knowledge of preimage of hashes for this use case.
    
    ### Hashing the `Event` hashes themselves
    For each `event`, their hashes are sorted according to following, in
    that order
    - `event.address` in increasing order.
    - `event.type` and the following fields need not be ordered in any
    certain way in order to be consistent with recproofs.
    But to maintain uniformity, we impose the order given by enum. Note that
    this is flexible as long as recproofs uses the same order.
    ```rust
    pub enum EventType { Write, Ensure, Read, Create, Delete }
    ```
    Once we have sorted the hashes, we proceed to `Merkleize` them. At each
    level, we would have a node with address `addr` whose subtree contains
    nodes which share their prefix to `addr`. Except for the nodes created
    from the bottom layer, all other nodes would be created by hashing two
    children nodes. At the bottom layer, we might have a group of hashes
    which share the same `addr`, in which case, we create its parent node as
    a merkle root of the group treated as leaves in that order.
    
    ---------
    
    Co-authored-by: Matthias Görgens <matthias.goergens@gmail.com>
    Co-authored-by: Supragya Raj <supragyaraj@gmail.com>

--- sdk/src/common/types/event.rs
+++ sdk/src/common/types/event.rs
@@ -1,3 +1,7 @@
+#[cfg(target_os = "mozakvm")]
+use crate::mozakvm::helpers::poseidon2_hash_no_pad;
+#[cfg(not(target_os = "mozakvm"))]
+use crate::native::helpers::poseidon2_hash_no_pad;
 #[derive(
     Copy,
     Clone,
@@ -17,9 +21,9 @@
 #[allow(clippy::module_name_repetitions)]
 #[repr(u8)]
 pub enum EventType {
-    Read = 0,
-    Write,
+    Write = 0,
     Ensure,
+    Read,
     Create,
     Delete,
 }
@@ -87,6 +91,18 @@ impl CanonicalEvent {
             }
         }
     }
+
+    #[must_use]
+    pub fn canonical_hash(&self) -> super::poseidon2hash::Poseidon2Hash {
+        let data_to_hash: Vec<u8> = itertools::chain!(
+            self.emitter.inner(),
+            u64::from(self.type_ as u8).to_le_bytes(),
+            self.address.inner(),
+            self.value.inner(),
+        )
+        .collect();
+        poseidon2_hash_no_pad(&data_to_hash)
+    }
 }
 
 #[derive(
@@ -106,3 +122,100 @@ impl CanonicalEvent {
     derive(Debug, serde::Serialize, serde::Deserialize)
 )]
 pub struct CanonicalOrderedTemporalHints(pub CanonicalEvent, pub u32);
+
+#[cfg(test)]
+mod tests {
+    use crate::common::types::{
+        CanonicalEvent, EventType, Poseidon2Hash, ProgramIdentifier, StateAddress,
+    };
+    use crate::native::helpers::poseidon2_hash_no_pad;
+
+    #[test]
+    fn check_sample_events_hash() {
+        // uses test vectors from tests in
+        // mozak-vm/circuits/src/recproof/verify_event.rs
+        let program_hash_1 = ProgramIdentifier([4, 8, 15, 16].into());
+
+        let zero_val: Poseidon2Hash = [0; 4].into();
+        let non_zero_val_1: Poseidon2Hash = [3, 1, 4, 15].into();
+        let non_zero_val_2: Poseidon2Hash = [1, 6, 180, 33].into();
+        let read_0 = CanonicalEvent {
+            address: StateAddress(42u64.to_le_bytes()),
+            emitter: program_hash_1,
+            type_: EventType::Read,
+            value: zero_val,
+        };
+        let write_1 = CanonicalEvent {
+            address: StateAddress(42u64.to_le_bytes()),
+            emitter: program_hash_1,
+            type_: EventType::Write,
+            value: non_zero_val_1,
+        };
+        let write_2 = CanonicalEvent {
+            address: StateAddress(42u64.to_le_bytes()),
+            emitter: program_hash_1,
+            type_: EventType::Write,
+            value: non_zero_val_2,
+        };
+        let read_0_hash: Poseidon2Hash = [
+            7272290939186032751,
+            8185818005188304227,
+            17555306369107993266,
+            17187284268557234321,
+        ]
+        .into();
+
+        let write_1_hash: Poseidon2Hash = [
+            11469795294276139037,
+            799622748573506082,
+            15272809121316752941,
+            7142640452443475716,
+        ]
+        .into();
+        let write_2_hash: Poseidon2Hash = [
+            1484423020241144842,
+            17207848040428508675,
+            7995793996020726058,
+            4658801606188332384,
+        ]
+        .into();
+
+        let branch_1_hash: Poseidon2Hash = [
+            16758566829994364981,
+            15311795646108582705,
+            12773152691662485878,
+            2551708493265210224,
+        ]
+        .into();
+        let branch_2_hash: Poseidon2Hash = [
+            8577138257922146843,
+            5112874340235798754,
+            4121828782781403483,
+            12250937462246573507,
+        ]
+        .into();
+
+        assert_eq!(read_0_hash, read_0.canonical_hash());
+        assert_eq!(write_1_hash, write_1.canonical_hash());
+        assert_eq!(write_2_hash, write_2.canonical_hash());
+
+        assert_eq!(
+            branch_1_hash,
+            poseidon2_hash_no_pad(
+                &itertools::chain!(
+                    write_1.canonical_hash().inner(),
+                    write_2.canonical_hash().inner()
+                )
+                .collect::<Vec<u8>>()
+            )
+        );
+
+        assert_eq!(
+            branch_2_hash,
+            poseidon2_hash_no_pad(
+                &itertools::chain!(read_0.canonical_hash().inner(), branch_1_hash.inner())
+                    .collect::<Vec<u8>>()
+            )
+        )
+    }
+}
