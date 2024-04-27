65ed90cf387e7c8d1de8db1d95450f26af43de7d
Matthias Goergens <matthias.goergens@gmail.com>
2024-04-27 10:31:24 +0800

    Remove obsolete tests

--- circuits/src/program/stark.rs
+++ circuits/src/program/stark.rs
@@ -5,24 +5,3 @@ use crate::unstark::Unstark;
 #[allow(clippy::module_name_repetitions)]
 pub type ProgramStark<F, const D: usize> =
     Unstark<F, D, ProgramRom<F>, { ProgramRom::<()>::NUMBER_OF_COLUMNS }>;
-
-#[cfg(test)]
-mod tests {
-    use plonky2::plonk::config::{GenericConfig, Poseidon2GoldilocksConfig};
-    use starky::stark_testing::test_stark_circuit_constraints;
-
-    use super::*;
-
-    const D: usize = 2;
-    type C = Poseidon2GoldilocksConfig;
-    type F = <C as GenericConfig<D>>::F;
-    type S = ProgramStark<F, D>;
-
-    #[test]
-    fn test_circuit() -> anyhow::Result<()> {
-        let stark = S::default();
-        test_stark_circuit_constraints::<F, C, S, D>(stark)?;
-
-        Ok(())
-    }
-}
