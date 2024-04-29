262fd16abab6ec56ab7ce26ce03eea91a7256987
Jakub Zalewski <jakub.zalewski@mozak.com>
2024-04-29 13:07:10 +0200

    Migrate register/init stark to the new API

--- circuits/src/register/init/stark.rs
+++ circuits/src/register/init/stark.rs
@@ -1,5 +1,6 @@
 use std::marker::PhantomData;
 
+use expr::{Expr, ExprBuilder, StarkFrameTyped};
 use mozak_circuits_derive::StarkNameDisplay;
 use plonky2::field::extension::{Extendable, FieldExtension};
 use plonky2::field::packed::PackedField;
@@ -7,12 +8,13 @@ use plonky2::hash::hash_types::RichField;
 use plonky2::iop::ext_target::ExtensionTarget;
 use plonky2::plonk::circuit_builder::CircuitBuilder;
 use starky::constraint_consumer::{ConstraintConsumer, RecursiveConstraintConsumer};
-use starky::evaluation_frame::{StarkEvaluationFrame, StarkFrame};
+use starky::evaluation_frame::StarkFrame;
 use starky::stark::Stark;
 
 use super::columns::RegisterInit;
 use crate::columns_view::{HasNamedColumns, NumberOfColumns};
-use crate::stark::utils::{is_binary, is_binary_ext_circuit};
+use crate::expr::{build_ext, build_packed, ConstraintBuilder};
+use crate::unstark::NoColumns;
 
 #[derive(Clone, Copy, Default, StarkNameDisplay)]
 #[allow(clippy::module_name_repetitions)]
@@ -27,6 +29,25 @@ impl<F, const D: usize> HasNamedColumns for RegisterInitStark<F, D> {
 const COLUMNS: usize = RegisterInit::<()>::NUMBER_OF_COLUMNS;
 const PUBLIC_INPUTS: usize = 0;
 
+/// Constraints for the [`RegisterInitStark`].
+///
+/// For sanity check, we can constrain the register address column to be in
+/// a running sum from 0..=31, but since this fixed table is known to
+/// both prover and verifier, we do not need to do so here.
+// TODO(Matthias): add constraints to force registers to start at 0;
+// but make it so we can turn them off for tests.
+fn generate_constraints<'a, T: Copy>(
+    vars: &StarkFrameTyped<RegisterInit<Expr<'a, T>>, NoColumns<Expr<'a, T>>>,
+) -> ConstraintBuilder<Expr<'a, T>> {
+    let lv = vars.local_values;
+    let mut constraints = ConstraintBuilder::default();
+
+    // Check: `is_looked_up` is a binary filter column.
+    constraints.always(lv.is_looked_up.is_binary());
+
+    constraints
+}
+
 impl<F: RichField + Extendable<D>, const D: usize> Stark<F, D> for RegisterInitStark<F, D> {
     type EvaluationFrame<FE, P, const D2: usize> = StarkFrame<P, P::Scalar, COLUMNS, PUBLIC_INPUTS>
 
@@ -36,33 +57,27 @@ impl<F: RichField + Extendable<D>, const D: usize> Stark<F, D> for RegisterInitS
     type EvaluationFrameTarget =
         StarkFrame<ExtensionTarget<D>, ExtensionTarget<D>, COLUMNS, PUBLIC_INPUTS>;
 
-    /// Constraints for the [`RegisterInitStark`].
-    ///
-    /// For sanity check, we can constrain the register address column to be in
-    /// a running sum from 0..=31, but since this fixed table is known to
-    /// both prover and verifier, we do not need to do so here.
-    // TODO(Matthias): add constraints to force registers to start at 0;
-    // but make it so we can turn them off for tests.
     fn eval_packed_generic<FE, P, const D2: usize>(
         &self,
         vars: &Self::EvaluationFrame<FE, P, D2>,
-        yield_constr: &mut ConstraintConsumer<P>,
+        consumer: &mut ConstraintConsumer<P>,
     ) where
         FE: FieldExtension<D2, BaseField = F>,
         P: PackedField<Scalar = FE>, {
-        let lv: &RegisterInit<P> = vars.get_local_values().into();
-        // Check: `is_looked_up` is a binary filter column.
-        is_binary(yield_constr, lv.is_looked_up);
+        let eb = ExprBuilder::default();
+        let constraints = generate_constraints(&eb.to_typed_starkframe(vars));
+        build_packed(constraints, consumer);
     }
 
     fn eval_ext_circuit(
         &self,
         builder: &mut CircuitBuilder<F, D>,
         vars: &Self::EvaluationFrameTarget,
-        yield_constr: &mut RecursiveConstraintConsumer<F, D>,
+        consumer: &mut RecursiveConstraintConsumer<F, D>,
     ) {
-        let lv: &RegisterInit<_> = vars.get_local_values().into();
-        is_binary_ext_circuit(builder, lv.is_looked_up, yield_constr);
+        let eb = ExprBuilder::default();
+        let constraints = generate_constraints(&eb.to_typed_starkframe(vars));
+        build_ext(constraints, builder, consumer);
     }
 
     fn constraint_degree(&self) -> usize { 3 }
