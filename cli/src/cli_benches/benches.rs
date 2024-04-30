8f4941c92c1b8aac1bf3e39679f19a9e3458d427
codeblooded1729 <kapilpause007@gmail.com>
2024-04-30 17:44:58 +0530

    refactor: separate prepare and execute part of benches.

--- cli/src/cli_benches/benches.rs
+++ cli/src/cli_benches/benches.rs
@@ -1,16 +1,34 @@
+use std::time::Duration;
+
+use anyhow::Result;
 use clap::{Args as Args_, Subcommand};
 
-use super::nop::nop_bench;
-use super::omni::omni_bench;
-use super::poseidon2::poseidon2_bench;
-use super::sort::sort_bench;
-use super::xor::xor_bench;
+use super::nop::NopBench;
+use super::omni::OmniBench;
+use super::poseidon2::Poseidon2Bench;
+use super::sort::SortBench;
+use super::xor::XorBench;
 
 #[derive(Debug, Args_, Clone)]
 #[command(args_conflicts_with_subcommands = true)]
 pub struct BenchArgs {
     #[command(subcommand)]
     pub function: BenchFunction,
+}
+
+pub trait Bench {
+    type Args;
+    type Prepared;
+
+    fn prepare(&self, args: &Self::Args) -> Self::Prepared;
+    fn execute(&self, prepared: Self::Prepared) -> Result<()>;
+
+    fn bench(&self, args: &Self::Args) -> Result<Duration> {
+        let prepared = self.prepare(args);
+        let start = std::time::Instant::now();
+        self.execute(prepared)?;
+        Ok(start.elapsed())
+    }
 }
 
 #[derive(PartialEq, Debug, Subcommand, Clone)]
@@ -34,13 +52,28 @@ pub enum BenchFunction {
 }
 
 impl BenchArgs {
-    pub fn run(&self) -> Result<(), anyhow::Error> {
-        match self.function {
-            BenchFunction::XorBench { iterations } => xor_bench(iterations),
-            BenchFunction::NopBench { iterations } => nop_bench(iterations),
-            BenchFunction::Poseidon2Bench { input_len } => poseidon2_bench(input_len),
-            BenchFunction::OmniBench { iterations } => omni_bench(iterations),
-            BenchFunction::SortBench { n } => sort_bench(n),
+    pub fn bench(&self) -> Result<Duration> {
+        match &self.function {
+            BenchFunction::XorBench { iterations } => {
+                let xor_bench = XorBench;
+                xor_bench.bench(iterations)
+            }
+            BenchFunction::NopBench { iterations } => {
+                let nop_bench = NopBench;
+                nop_bench.bench(iterations)
+            }
+            BenchFunction::OmniBench { iterations } => {
+                let omni_bench = OmniBench;
+                omni_bench.bench(iterations)
+            }
+            BenchFunction::Poseidon2Bench { input_len } => {
+                let poseidon2_bench = Poseidon2Bench;
+                poseidon2_bench.bench(input_len)
+            }
+            BenchFunction::SortBench { n } => {
+                let sort_bench = SortBench;
+                sort_bench.bench(n)
+            }
         }
     }
 }
