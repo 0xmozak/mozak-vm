f257dfac282a68fc15a18439a356f5b0d6b59aee
bing <bingcicle@keemail.me>
2024-05-02 17:04:21 +0800

    example(sort): rearrange things around

--- examples/mozak-sort/main.rs
+++ examples/mozak-sort/main.rs
@@ -1,24 +1,26 @@
 #![cfg_attr(target_os = "mozakvm", no_main)]
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use core::hint::black_box;
 extern crate alloc;
-use alloc::vec::Vec;
 
 #[cfg(target_os = "mozakvm")]
-use mozak_sdk::core::ecall::ioread_public;
+use {
+    alloc::vec::Vec,
+    core::hint::black_box,
+    mozak_sdk::core::ecall::ioread_public,
+    rand::rngs::SmallRng,
+    rand::{Rng, SeedableRng},
+};
 
 extern crate rand;
-use rand::rngs::SmallRng;
-use rand::{Rng, SeedableRng};
 
 #[allow(clippy::unit_arg)]
-fn main() {
+#[cfg(target_os = "mozakvm")]
+fn sort() {
     let mut rng = black_box(SmallRng::seed_from_u64(0xdead_beef_feed_cafe));
 
     let n = {
         let mut bytes = [0u8; 4];
-        #[cfg(target_os = "mozakvm")]
         ioread_public(bytes.as_mut_ptr(), bytes.len());
         u32::from_le_bytes(bytes)
     };
@@ -26,6 +28,11 @@ fn main() {
     let mut v: Vec<u32> = (0..n).map(|_| black_box(rng.gen())).collect();
 
     black_box(v.sort());
+}
+
+fn main() {
+    #[cfg(target_os = "mozakvm")]
+    sort();
 }
 
 mozak_sdk::entry!(main);
