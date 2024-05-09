19c59c664c175e91780934c4a9ef04712cbb50f6
Matthias Goergens <matthias.goergens@gmail.com>
2024-05-09 13:58:10 +0800

    Read at runtime

--- circuits/tests/riscv_tests.rs
+++ circuits/tests/riscv_tests.rs
@@ -1,10 +1,11 @@
+use std::fs;
+
 use anyhow::{Ok, Result};
 use mozak_circuits::test_utils::prove_and_verify_mozak_stark;
 use mozak_runner::elf::Program;
 use mozak_runner::state::State;
 use mozak_runner::vm::step;
 use plonky2::field::goldilocks_field::GoldilocksField;
-use plonky2_maybe_rayon::*;
 use starky::config::StarkConfig;
 
 /// This function takes the contents of a compiled ELF and runs it through
@@ -37,9 +38,65 @@ fn run_test(elf: &[u8]) -> Result<()> {
     Ok(())
 }
 
-#[test]
-fn riscv_tests() {
-    mozak_examples::riscv_tests.into_par_iter().for_each(|elf| {
-        run_test(elf).unwrap();
-    });
+/// This macro takes in an identifier as the test name and the file name of a
+/// compiled ELF, and sets up a `run_test` for it.
+macro_rules! test_elf {
+    ($test_name:ident, $file_name:tt) => {
+        #[test]
+        fn $test_name() -> Result<()> {
+            let elf = fs::read(concat!("riscv-testdata/testdata/", $file_name))
+                .expect("Should have been able to read the file");
+            run_test(&elf)
+        }
+    };
 }
+
+// Base instruction set
+test_elf!(add, "rv32ui-p-add");
+test_elf!(addi, "rv32ui-p-addi");
+test_elf!(and, "rv32ui-p-and");
+test_elf!(andi, "rv32ui-p-andi");
+test_elf!(auipc, "rv32ui-p-auipc");
+test_elf!(beq, "rv32ui-p-beq");
+test_elf!(bge, "rv32ui-p-bge");
+test_elf!(bgeu, "rv32ui-p-bgeu");
+test_elf!(blt, "rv32ui-p-blt");
+test_elf!(bltu, "rv32ui-p-bltu");
+test_elf!(bne, "rv32ui-p-bne");
+test_elf!(jal, "rv32ui-p-jal");
+test_elf!(jalr, "rv32ui-p-jalr");
+test_elf!(lb, "rv32ui-p-lb");
+test_elf!(lbu, "rv32ui-p-lbu");
+test_elf!(lh, "rv32ui-p-lh");
+test_elf!(lhu, "rv32ui-p-lhu");
+test_elf!(lui, "rv32ui-p-lui");
+test_elf!(lw, "rv32ui-p-lw");
+test_elf!(or, "rv32ui-p-or");
+test_elf!(ori, "rv32ui-p-ori");
+test_elf!(sb, "rv32ui-p-sb");
+test_elf!(sh, "rv32ui-p-sh");
+test_elf!(simple, "rv32ui-p-simple");
+test_elf!(sll, "rv32ui-p-sll");
+test_elf!(slli, "rv32ui-p-slli");
+test_elf!(slt, "rv32ui-p-slt");
+test_elf!(slti, "rv32ui-p-slti");
+test_elf!(sltiu, "rv32ui-p-sltiu");
+test_elf!(sltu, "rv32ui-p-sltu");
+test_elf!(sra, "rv32ui-p-sra");
+test_elf!(srai, "rv32ui-p-srai");
+test_elf!(srl, "rv32ui-p-srl");
+test_elf!(srli, "rv32ui-p-srli");
+test_elf!(sub, "rv32ui-p-sub");
+test_elf!(sw, "rv32ui-p-sw");
+test_elf!(xor, "rv32ui-p-xor");
+test_elf!(xori, "rv32ui-p-xori");
+
+// M extension
+test_elf!(div, "rv32um-p-div");
+test_elf!(divu, "rv32um-p-divu");
+test_elf!(mul, "rv32um-p-mul");
+test_elf!(mulh, "rv32um-p-mulh");
+test_elf!(mulhsu, "rv32um-p-mulhsu");
+test_elf!(mulhu, "rv32um-p-mulhu");
+test_elf!(rem, "rv32um-p-rem");
+test_elf!(remu, "rv32um-p-remu");
