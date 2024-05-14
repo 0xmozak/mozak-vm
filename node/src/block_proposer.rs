3b7572f6b47dc5511651d6af0759ed38cadc27a7
Daniel Bloom <7810950-Daniel.Aaron.Bloom@users.noreply.gitlab.com>
2024-05-14 12:59:14 -0700

    refactor: remove all hashing from state update driver

--- node/src/block_proposer.rs
+++ node/src/block_proposer.rs
@@ -1,7 +1,1 @@
-// TODO(daniel): remove all these
-#![allow(unreachable_code)]
-#![allow(dead_code)]
-#![allow(unused_variables)]
-#![allow(clippy::diverging_sub_expression)]
-
-mod state;
+pub mod state;
