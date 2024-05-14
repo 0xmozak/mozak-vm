dec705eabb8c86389d0d25ed2d73cb0d5b1c1b0e
bing <bingcicle@keemail.me>
2024-05-14 09:46:44 +0800

    circuits(generation): remove dedicated generation module

--- circuits/src/cpu/columns.rs
+++ circuits/src/cpu/columns.rs
@@ -1,8 +1,13 @@
 use core::ops::{Add, Mul, Sub};
 
+use itertools::izip;
+use mozak_runner::instruction::Op;
+use plonky2::hash::hash_types::RichField;
+
 use crate::bitshift::columns::Bitshift;
 use crate::columns_view::{columns_view_impl, make_col_map};
 use crate::cross_table_lookup::{Column, ColumnWithTypedInput};
+use crate::generation::ascending_sum;
 use crate::memory::columns::MemoryCtl;
 use crate::poseidon2_sponge::columns::Poseidon2SpongeCtl;
 use crate::program::columns::ProgramRom;
@@ -84,6 +89,87 @@ pub struct Instruction<T> {
     pub rd_selected: T,
     /// Special immediate value used for code constants
     pub imm_value: T,
+}
+
+impl From<(u32, mozak_runner::instruction::Instruction)> for Instruction<u32> {
+    fn from((pc, inst): (u32, mozak_runner::instruction::Instruction)) -> Self {
+        let mut cols: Instruction<u32> = Self {
+            pc,
+            imm_value: inst.args.imm,
+            is_op1_signed: matches!(
+                inst.op,
+                Op::SLT | Op::DIV | Op::REM | Op::MULH | Op::MULHSU | Op::BLT | Op::BGE | Op::SRA
+            )
+            .into(),
+            is_op2_signed: matches!(
+                inst.op,
+                Op::SLT | Op::DIV | Op::REM | Op::MULH | Op::BLT | Op::BGE
+            )
+            .into(),
+            is_dst_signed: matches!(inst.op, Op::LB | Op::LH).into(),
+            ..Self::default()
+        };
+        *match inst.op {
+            Op::ADD => &mut cols.ops.add,
+            Op::LBU | Op::LB => &mut cols.ops.lb,
+            Op::LH | Op::LHU => &mut cols.ops.lh,
+            Op::LW => &mut cols.ops.lw,
+            Op::SLL => &mut cols.ops.sll,
+            Op::SLT | Op::SLTU => &mut cols.ops.slt,
+            Op::SB => &mut cols.ops.sb,
+            Op::SH => &mut cols.ops.sh,
+            Op::SW => &mut cols.ops.sw,
+            Op::SRL => &mut cols.ops.srl,
+            Op::SRA => &mut cols.ops.sra,
+            Op::SUB => &mut cols.ops.sub,
+            Op::DIV | Op::DIVU => &mut cols.ops.div,
+            Op::REM | Op::REMU => &mut cols.ops.rem,
+            Op::MUL => &mut cols.ops.mul,
+            Op::MULH | Op::MULHU | Op::MULHSU => &mut cols.ops.mulh,
+            Op::JALR => &mut cols.ops.jalr,
+            Op::BEQ => &mut cols.ops.beq,
+            Op::BNE => &mut cols.ops.bne,
+            Op::BLT | Op::BLTU => &mut cols.ops.blt,
+            Op::BGE | Op::BGEU => &mut cols.ops.bge,
+            Op::ECALL => &mut cols.ops.ecall,
+            Op::XOR => &mut cols.ops.xor,
+            Op::OR => &mut cols.ops.or,
+            Op::AND => &mut cols.ops.and,
+        } = 1;
+        cols.rs1_selected = u32::from(inst.args.rs1);
+        cols.rs2_selected = u32::from(inst.args.rs2);
+        cols.rd_selected = u32::from(inst.args.rd);
+        cols
+    }
+}
+
+impl<F: RichField> From<Instruction<F>> for ProgramRom<F> {
+    fn from(inst: Instruction<F>) -> Self {
+        pub fn reduce_with_powers<F: RichField, I: IntoIterator<Item = F>>(
+            terms: I,
+            alpha: u64,
+        ) -> F {
+            izip!((0..).map(|i| F::from_canonical_u64(alpha.pow(i))), terms)
+                .map(|(base, val)| base * val)
+                .sum()
+        }
+
+        Self {
+            pc: inst.pc,
+            inst_data: reduce_with_powers(
+                [
+                    ascending_sum(inst.ops),
+                    inst.is_op1_signed,
+                    inst.is_op2_signed,
+                    inst.rs1_selected,
+                    inst.rs2_selected,
+                    inst.rd_selected,
+                    inst.imm_value,
+                ],
+                1 << 5,
+            ),
+        }
+    }
 }
 
 make_col_map!(CpuState);
