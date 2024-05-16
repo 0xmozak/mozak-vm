99f8fd7581e5e753d67a9e52f3cc76efa8145e7a
bing <bingcicle@keemail.me>
2024-05-16 17:08:08 +0800

    token: fix wallet usage

--- examples/token/main_native.rs
+++ examples/token/main_native.rs
@@ -11,11 +11,18 @@ fn main() {
     // We assume both wallet are the same program for now
     let remitter_program = ProgramIdentifier::new_from_rand_seed(2);
     let remittee_program = ProgramIdentifier::new_from_rand_seed(3);
-    let remitter_pub_key = wallet::PublicKey::new_from_rand_seed(4);
-    let remittee_pub_key = wallet::PublicKey::new_from_rand_seed(5);
+    let remitter_private_key = wallet::PrivateKey::new_from_rand_seed(4);
+    let remitter_public_key = wallet::PublicKey(mozak_sdk::native::helpers::poseidon2_hash_no_pad(
+        &remitter_private_key.0,
+    ));
+
+    let remittee_private_key = wallet::PrivateKey::new_from_rand_seed(5);
+    let remittee_public_key = wallet::PublicKey(mozak_sdk::native::helpers::poseidon2_hash_no_pad(
+        &remittee_private_key.0,
+    ));
 
     let token_object = wallet::TokenObject {
-        pub_key: remitter_pub_key,
+        pub_key: remitter_public_key,
         amount: 100.into(),
     };
 
@@ -33,7 +40,7 @@ fn main() {
             state_object,
             remitter_program,
             remittee_program,
-            remittee_pub_key,
+            remittee_public_key,
         ),
         dispatch,
     );
