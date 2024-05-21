eec3ccb1390130e2aef6158805ff6a0fe8a91d30
Sai Deng <deng.sai@gmail.com>
2024-05-21 15:21:26 -0700

    add benches

--- cli/src/cli_benches/sort.rs
+++ cli/src/cli_benches/sort.rs
@@ -1,9 +1,12 @@
 use anyhow::Result;
-use mozak_circuits::stark::mozak_stark::{MozakStark, PublicInputs};
-use mozak_circuits::stark::proof::AllProof;
+use mozak_circuits::stark::batch_prover::batch_prove;
+use mozak_circuits::stark::batch_verifier::batch_verify_proof;
+use mozak_circuits::stark::mozak_stark::{MozakStark, PublicInputs, PUBLIC_TABLE_KINDS};
+use mozak_circuits::stark::proof::{AllProof, BatchProof};
 use mozak_circuits::stark::prover::prove;
 use mozak_circuits::stark::recursive_verifier::{
-    recursive_mozak_stark_circuit, MozakStarkVerifierCircuit,
+    recursive_batch_stark_circuit, recursive_mozak_stark_circuit, MozakBatchStarkVerifierCircuit,
+    MozakStarkVerifierCircuit,
 };
 use mozak_circuits::stark::verifier::verify_proof;
 use mozak_circuits::test_utils::{
@@ -81,6 +84,50 @@ pub fn batch_starks_sort_execute(result: Result<(Program, ExecutionRecord<F>)>) 
     prove_and_verify_batch_mozak_stark(&program, &record, &StarkConfig::standard_fast_config())
 }
 
+pub fn batch_starks_sort_recursive_prepare(
+    n: u32,
+) -> Result<(MozakBatchStarkVerifierCircuit<F, C, D>, BatchProof<F, C, D>)> {
+    let mozak_stark = MozakStark::default();
+    let stark_config = StarkConfig::standard_fast_config();
+    let (program, record) = sort_prepare(n)?;
+    let public_inputs = PublicInputs {
+        entry_point: F::from_canonical_u32(program.entry_point),
+    };
+    let (mozak_proof, degree_bits) = batch_prove::<F, C, D>(
+        &program,
+        &record,
+        &mozak_stark,
+        &PUBLIC_TABLE_KINDS,
+        &stark_config,
+        public_inputs,
+        &mut TimingTree::default(),
+    )?;
+    batch_verify_proof(
+        &mozak_stark,
+        &PUBLIC_TABLE_KINDS,
+        mozak_proof.clone(),
+        &stark_config,
+        &degree_bits,
+    )?;
+    let circuit_config = CircuitConfig::standard_recursion_config();
+    let mozak_stark_circuit = recursive_batch_stark_circuit::<F, C, D>(
+        &mozak_stark,
+        &degree_bits,
+        &PUBLIC_TABLE_KINDS,
+        &circuit_config,
+        &stark_config,
+    );
+    Ok((mozak_stark_circuit, mozak_proof))
+}
+
+pub fn batch_starks_sort_recursive_execute(
+    circuit_with_proof: Result<(MozakBatchStarkVerifierCircuit<F, C, D>, BatchProof<F, C, D>)>,
+) -> Result<()> {
+    let (mozak_stark_circuit, mozak_proof) = circuit_with_proof?;
+    let recursive_proof = mozak_stark_circuit.prove(&mozak_proof)?;
+    mozak_stark_circuit.circuit.verify(recursive_proof)
+}
+
 pub(crate) struct SortBench;
 
 impl Bench for SortBench {
@@ -116,6 +163,21 @@ impl Bench for BatchStarksSortBench {
     }
 }
 
+pub(crate) struct BatchStarksSortBenchRecursive;
+
+impl Bench for BatchStarksSortBenchRecursive {
+    type Args = u32;
+    type Prepared = Result<(MozakBatchStarkVerifierCircuit<F, C, D>, BatchProof<F, C, D>)>;
+
+    fn prepare(&self, args: &Self::Args) -> Self::Prepared {
+        batch_starks_sort_recursive_prepare(*args)
+    }
+
+    fn execute(&self, prepared: Self::Prepared) -> Result<()> {
+        batch_starks_sort_recursive_execute(prepared)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use anyhow::Result;
@@ -130,6 +192,7 @@ mod tests {
         let n = 10;
         sort_execute(sort_prepare(n))
     }
+
     #[test]
     fn test_recursive_sort_bench() -> Result<()> {
         let n = 10;
@@ -140,5 +203,11 @@ mod tests {
     fn test_batch_starks_sort_bench() -> Result<()> {
         let n = 10;
         batch_starks_sort_execute(sort_prepare(n))
+    }
+
+    #[test]
+    fn test_batch_stark_recursive_sort_bench() -> Result<()> {
+        let n = 10;
+        sort_recursive_execute(sort_recursive_prepare(n))
     }
 }
