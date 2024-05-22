c1526cd94dfb1d1af093479f9d800f8dc6dbd811
Daniel Bloom <7810950-Daniel.Aaron.Bloom@users.noreply.gitlab.com>
2024-05-22 00:10:19 -0700

    fix addressing

--- node/src/block_proposer.rs
+++ node/src/block_proposer.rs
@@ -1,2 +1,215 @@
+use std::cmp::Ordering;
+use std::fmt;
+use std::ops::{BitAnd, BitAndAssign, Shl, Sub};
+
 pub mod state;
 pub mod transactions;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Dir {
+    Left,
+    Right,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Address(pub u64);
+
+impl Address {
+    fn next(self, height: usize) -> (Option<AddressPath>, Dir) {
+        debug_assert!(self.0 < (1 << height));
+        AddressPath {
+            height,
+            addr: self.0,
+        }
+        .next()
+    }
+}
+
+/// The remaining bits of an address to be consumed as one traverses down the
+/// tree towards a leaf.
+#[derive(Debug, Clone, Copy)]
+struct AddressPath<T = u64> {
+    height: usize,
+    addr: T,
+}
+
+impl<T> AddressPath<T>
+where
+    T: Copy
+        + From<bool>
+        + Shl<usize, Output = T>
+        + BitAnd<T, Output = T>
+        + PartialEq
+        + fmt::Debug
+        + Sub<T, Output = T>
+        + BitAndAssign,
+{
+    fn path(addr: T, height: usize) -> Option<Self> {
+        (height == 0).then_some(Self { height, addr })
+    }
+
+    fn next(mut self) -> (Option<Self>, Dir) {
+        let zero = T::from(false);
+        let one = T::from(true);
+
+        // look at the MSB for the current direction
+        let bit = one << (self.height - 1);
+
+        let dir = if self.addr & bit != zero {
+            Dir::Right
+        } else {
+            Dir::Left
+        };
+
+        // Pop the MSB
+        self.addr &= bit - one;
+
+        if self.height == 0 {
+            debug_assert_eq!(self.addr, zero);
+            (None, dir)
+        } else {
+            self.height -= 1;
+            (Some(self), dir)
+        }
+    }
+}
+
+/// A partial address which is constructed starting at the root and moving
+/// downward, adding on one bit at a time based on a provided direction
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct BranchAddress {
+    height: usize,
+    addr: Address,
+}
+
+impl BranchAddress {
+    /// Initialize the `BranchAddress` to the root node
+    fn root(height: usize) -> Self {
+        Self {
+            height,
+            addr: Address(0),
+        }
+    }
+
+    /// Initialize the `BranchAddress` to a leaf node
+    fn base(a: u64) -> Self {
+        BranchAddress {
+            height: 0,
+            addr: Address(a),
+        }
+    }
+
+    /// Move downward, adding a `0|1` bit based on the dir (`Left|Right`).
+    /// If we've reached the bottom, return an `Address` instead
+    fn child(mut self, dir: Dir) -> Result<Self, Address> {
+        self.addr.0 <<= 1;
+        self.addr.0 |= (dir == Dir::Right) as u64;
+        if self.height == 0 {
+            Err(self.addr)
+        } else {
+            self.height -= 1;
+            Ok(self)
+        }
+    }
+
+    /// Move upward, adding a `0|1` bit based on the dir (`Left|Right`).
+    /// If we've reached the bottom, return an `Address` instead
+    fn parent(mut self) -> Self {
+        self.addr = Address(self.addr.0 >> 1);
+        self.height += 1;
+        self
+    }
+
+    fn compare(&self, rhs: &Self) -> Option<BranchAddressComparison> {
+        let (parent, child) = match self.height.cmp(&rhs.height) {
+            // LHS and RHS are at the same level
+            Ordering::Equal => {
+                // Check if LHS and RHS have the same parent
+                let lhs_msb = self.addr.0 >> 1;
+                let rhs_msb = rhs.addr.0 >> 1;
+                if lhs_msb != rhs_msb {
+                    return None
+                }
+
+                // Compare the final direction of LHS and RHS
+                let lhs_lsb = self.addr.0 & 1;
+                let rhs_lsb = rhs.addr.0 & 1;
+                return match lhs_lsb.cmp(&rhs_lsb) {
+                    Ordering::Less => Some(BranchAddressComparison::RightSibling),
+                    Ordering::Equal => Some(BranchAddressComparison::Equal),
+                    Ordering::Greater => Some(BranchAddressComparison::LeftSibling),
+                }
+            },
+            // LHS is a child of RHS
+            Ordering::Less => (rhs, self),
+            // RHS is a child of LHS
+            Ordering::Greater => (self, rhs),
+        };
+
+
+        // Check if child actually descends from parent
+        let delta = parent.height - child.height;
+        if child.addr.0 >> delta != parent.addr.0 {
+            return None
+        }
+
+        let addr = Address(child.addr.0 & ((1 << delta) - 1));
+        match (addr.next(delta).1, parent == self) {
+            (Dir::Left, true) => Some(BranchAddressComparison::LeftChild),
+            (Dir::Right, true) => Some(BranchAddressComparison::RightChild),
+            (Dir::Left, false) => Some(BranchAddressComparison::LeftParent),
+            (Dir::Right, false) => Some(BranchAddressComparison::RightParent),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum BranchAddressComparison {
+    /// The LHS and RHS addresses are the same
+    Equal,
+    /// The RHS address is a left-child of the LHS address
+    LeftChild,
+    /// The RHS address is a right-child of the LHS address
+    RightChild,
+    /// The RHS address is a left-sibling of the LHS address
+    LeftSibling,
+    /// The RHS address is a right-sibling of the LHS address
+    RightSibling,
+    /// The RHS address is a left-parent of the LHS address
+    LeftParent,
+    /// The RHS address is a right-parent of the LHS address
+    RightParent,
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_branch_compare() {
+        let parent = BranchAddress::root(10);
+        let child_1 = parent.child(Dir::Left).unwrap();
+        let child_2 = parent.child(Dir::Right).unwrap();
+        let child_3 = child_1.child(Dir::Right).unwrap();
+
+        assert_eq!(parent.compare(&parent), Some(BranchAddressComparison::Equal));
+        assert_eq!(child_1.compare(&child_1), Some(BranchAddressComparison::Equal));
+        assert_eq!(child_2.compare(&child_2), Some(BranchAddressComparison::Equal));
+        assert_eq!(child_3.compare(&child_3), Some(BranchAddressComparison::Equal));
+
+        assert_eq!(parent.compare(&child_1), Some(BranchAddressComparison::LeftChild));
+        assert_eq!(child_1.compare(&parent), Some(BranchAddressComparison::LeftParent));
+        assert_eq!(parent.compare(&child_3), Some(BranchAddressComparison::LeftChild));
+        assert_eq!(child_3.compare(&parent), Some(BranchAddressComparison::LeftParent));
+
+        assert_eq!(child_1.compare(&child_2), Some(BranchAddressComparison::RightSibling));
+        assert_eq!(child_2.compare(&child_1), Some(BranchAddressComparison::LeftSibling));
+
+        assert_eq!(parent.compare(&child_2), Some(BranchAddressComparison::RightChild));
+        assert_eq!(child_2.compare(&parent), Some(BranchAddressComparison::RightParent));
+
+        assert_eq!(child_2.compare(&child_3), None);
+        assert_eq!(child_3.compare(&child_2), None);
+    }
+}
--- node/src/block_proposer/transactions.rs
+++ node/src/block_proposer/transactions.rs
@@ -1,51 +1,62 @@
+use std::borrow::Cow;
+use std::convert::Infallible;
+use std::iter::repeat_with;
+
 use anyhow::{bail, Result};
 use hashbrown::hash_map::Entry;
 use hashbrown::HashMap;
-use itertools::Either;
-use mozak_recproofs::circuits::build_event_root::{
-    BranchCircuit as EventBranchCircuit, LeafCircuit as EventLeafCircuit,
-};
-use mozak_recproofs::circuits::merge::{
-    BranchCircuit as MergeBranchCircuit, LeafCircuit as MergeLeafCircuit,
-    LeafProof as MergeLeafProof,
-};
+use itertools::{chain, merge_join_by, Either, Itertools};
 use mozak_recproofs::circuits::verify_program::core::ProgramPublicIndices;
-use mozak_recproofs::circuits::verify_program::{
-    BranchCircuit as ProgramBranchCircuit, BranchProof as ProgramBranchProof,
-    LeafCircuit as ProgramLeafCircuit, LeafProof as ProgramLeafProof,
-};
-use mozak_recproofs::circuits::verify_tx::{
-    BranchCircuit as TxBranchCircuit, BranchProof as TxBranchProof, LeafCircuit as TxLeafCircuit,
-    LeafProof as TxLeafProof,
-};
+use mozak_recproofs::circuits::{build_event_root, merge, verify_program, verify_tx};
 use mozak_recproofs::{Event, EventType as ProofEventType};
-use mozak_sdk::common::types::{
-    CanonicalEvent, EventType as SdkEventType, Poseidon2Hash, ProgramIdentifier,
-};
+use mozak_sdk::common::types::{CanonicalEvent, EventType as SdkEventType, ProgramIdentifier};
 use mozak_sdk::core::constants::DIGEST_BYTES;
 use plonky2::field::types::Field;
 use plonky2::hash::hash_types::HashOut;
-use plonky2::hash::poseidon2::Poseidon2Hash as Plonky2Poseidon2Hash;
+use plonky2::hash::poseidon2::Poseidon2Hash;
 use plonky2::plonk::circuit_data::{CircuitConfig, CommonCircuitData, VerifierOnlyCircuitData};
 use plonky2::plonk::config::{GenericHashOut, Hasher};
 use plonky2::plonk::proof::ProofWithPublicInputs;
 
+use super::{AddressPath, BranchAddress, Dir};
 use crate::{C, D, F};
 
+type EventLeafCircuit = build_event_root::LeafCircuit<F, C, D>;
+type EventBranchCircuit = build_event_root::BranchCircuit<F, C, D>;
+
+type ProgramLeafCircuit = verify_program::LeafCircuit<F, C, D>;
+type ProgramBranchCircuit = verify_program::BranchCircuit<F, C, D>;
+
+type ProgramLeafProof = verify_program::LeafProof<F, C, D>;
+type ProgramBranchProof = verify_program::BranchProof<F, C, D>;
+
+type TxLeafCircuit = verify_tx::LeafCircuit<F, C, D>;
+type TxBranchCircuit = verify_tx::BranchCircuit<F, C, D>;
+
+type TxLeafProof = verify_tx::LeafProof<F, C, D>;
+type TxBranchProof = verify_tx::BranchProof<F, C, D>;
+
+type MergeLeafCircuit = merge::LeafCircuit<F, C, D>;
+type MergeBranchCircuit = merge::BranchCircuit<F, C, D>;
+
+type MergeLeafProof = merge::LeafProof<F, C, D>;
+type MergeBranchProof = merge::BranchProof<F, C, D>;
+
 pub struct AuxTransactionData {
-    event_leaf_circuit: EventLeafCircuit<F, C, D>,
-    event_branch_circuit: EventBranchCircuit<F, C, D>,
+    event_leaf_circuit: EventLeafCircuit,
+    event_branch_circuit: EventBranchCircuit,
 
-    merge_leaf_circuit: MergeLeafCircuit<F, C, D>,
-    merge_branch_circuit: MergeBranchCircuit<F, C, D>,
+    merge_leaf_circuit: MergeLeafCircuit,
+    merge_branch_circuit: MergeBranchCircuit,
 
-    program_leaf_circuit: ProgramLeafCircuit<F, C, D>,
-    program_branch_circuit: ProgramBranchCircuit<F, C, D>,
+    program_leaf_circuit: ProgramLeafCircuit,
+    program_branch_circuit: ProgramBranchCircuit,
 
-    tx_leaf_circuit: TxLeafCircuit<F, C, D>,
-    tx_branch_circuit: TxBranchCircuit<F, C, D>,
+    tx_leaf_circuit: TxLeafCircuit,
+    tx_branch_circuit: TxBranchCircuit,
 
-    empty_merge_leaf: MergeLeafProof<F, C, D>,
+    empty_merge_leaf: MergeLeafProof,
+    empty_merge_branch: MergeBranchProof,
 }
 
 impl AuxTransactionData {
@@ -82,6 +93,10 @@ impl AuxTransactionData {
             .prove(&merge_branch_circuit, None, None)
             .expect("Failed to construct leaf proof");
 
+        let empty_merge_branch = merge_branch_circuit
+            .prove(&empty_merge_leaf, &empty_merge_leaf)
+            .expect("Failed to construct branch proof");
+
         AuxTransactionData {
             event_leaf_circuit,
             event_branch_circuit,
@@ -92,7 +107,164 @@ impl AuxTransactionData {
             tx_leaf_circuit,
             tx_branch_circuit,
             empty_merge_leaf,
+            empty_merge_branch,
         }
+    }
+
+    fn insert_program(
+        &self,
+        node: &mut OngoingTxNode,
+        call_address: Option<AddressPath<usize>>,
+        events: Option<EventNode>,
+        proof: impl FnOnce() -> Result<ProgramLeafProof>,
+    ) -> Result<()> {
+        let program_branch = &self.program_branch_circuit;
+        match (node, call_address) {
+            (OngoingTxNode::UnprocessedBranch { left, right }, Some(call_address)) => {
+                let (call_address, next_node) = match call_address.next() {
+                    (call_address, Dir::Left) => (call_address, left.as_mut()),
+                    (call_address, Dir::Right) => (call_address, right.as_mut()),
+                };
+                self.insert_program(next_node, call_address, events, proof)?;
+
+                match (left.as_mut(), right.as_mut()) {
+                    (
+                        OngoingTxNode::ProcessedLeaf {
+                            events: l_events,
+                            proof: l_proof,
+                        },
+                        OngoingTxNode::ProcessedLeaf {
+                            events: r_events,
+                            proof: r_proof,
+                        },
+                    ) => {
+                        let (merge, events) =
+                            self.merge_events_branch(l_events.take(), r_events.take());
+                        let proof = program_branch.prove(&merge, l_proof, Some(r_proof))?;
+                    }
+                    _ => {}
+                }
+            }
+            _ => todo!(),
+        }
+        if let Some(call_address) = call_address {}
+        Ok(())
+    }
+
+    fn merge_events_branch(
+        &self,
+        left: Option<EventNode>,
+        right: Option<EventNode>,
+    ) -> (Cow<MergeBranchProof>, Option<EventNode>) {
+        match self.merge_events(left, right) {
+            None => (Cow::Borrowed(&self.empty_merge_branch), None),
+            Some((Either::Left(proof), event)) => {
+                let proof = self
+                    .merge_branch_circuit
+                    .prove(&proof, &self.empty_merge_leaf)
+                    .unwrap();
+                (Cow::Owned(proof), Some(event))
+            }
+            Some((Either::Right(proof), event)) => (proof, Some(event)),
+        }
+    }
+
+    fn merge_events(
+        &self,
+        left: Option<EventNode>,
+        right: Option<EventNode>,
+    ) -> Option<(
+        Either<Cow<MergeLeafProof>, Cow<MergeBranchProof>>,
+        EventNode,
+    )> {
+        let leaf_circuit = &self.merge_leaf_circuit;
+        let branch_circuit = &self.merge_branch_circuit;
+        let (left, right) = match (left, right) {
+            // Empty case
+            (None, None) => return None,
+            // Single case
+            (left @ Some(_), right @ None) | (left @ None, right @ Some(_)) => {
+                let proof = leaf_circuit
+                    .prove(
+                        branch_circuit,
+                        left.as_ref().map(EventNode::hash),
+                        right.as_ref().map(EventNode::hash),
+                    )
+                    .unwrap();
+                return left.or(right).map(|e| (Either::Left(Cow::Owned(proof)), e));
+            }
+            (Some(left), Some(right)) => (left, right),
+        };
+        match (left, right) {
+            (
+                left @ EventNode::Leaf {
+                    hash: l_hash,
+                    event: l_event,
+                },
+                right @ EventNode::Leaf {
+                    hash: r_hash,
+                    event: r_event,
+                },
+            ) => {
+                let mut l_address = BranchAddress::base(l_event.address);
+                let mut r_address = BranchAddress::base(r_event.address);
+
+                while l_address != r_address {
+                    l_address = l_address.parent();
+                    r_address = r_address.parent();
+                }
+                let address = l_address;
+
+                return if l_event.address <= r_event.address {
+                    let proof = leaf_circuit
+                        .prove(branch_circuit, Some(l_hash), Some(r_hash))
+                        .unwrap();
+                    let hash = proof.merged_hash();
+                    debug_assert_eq!(hash, Poseidon2Hash::two_to_one(l_hash, r_hash));
+                    let event = EventNode::Branch {
+                        address,
+                        hash,
+                        left: Box::new(left),
+                        right: Box::new(right),
+                    };
+                    Some((Either::Left(Cow::Owned(proof)), event))
+                } else {
+                    let l_proof = leaf_circuit
+                        .prove(branch_circuit, Some(l_hash), None)
+                        .unwrap();
+                    let r_proof = leaf_circuit
+                        .prove(branch_circuit, None, Some(r_hash))
+                        .unwrap();
+                    let proof = branch_circuit.prove(&r_proof, &l_proof).unwrap();
+                    let hash = proof.merged_hash();
+                    debug_assert_eq!(hash, Poseidon2Hash::two_to_one(r_hash, l_hash));
+                    let event = EventNode::Branch {
+                        address,
+                        hash,
+                        left: Box::new(right),
+                        right: Box::new(left),
+                    };
+                    Some((Either::Right(Cow::Owned(proof)), event))
+                };
+            },
+
+            (
+                left @ EventNode::Leaf {
+                    hash: l_hash,
+                    event: l_event,
+                },
+                EventNode::Branch {
+                    address,
+                    hash: r_hash,
+                    left: r_child_left,
+                    right: r_child_right,
+                },
+            ) => {
+            },
+            _ => todo!(),
+        }
+
+        todo!()
     }
 }
 
@@ -114,15 +286,24 @@ struct OngoingTx {
 
 pub enum EventNode {
     Branch {
-        address: u64,
+        address: BranchAddress,
         hash: HashOut<F>,
-        left: Box<OngoingTxNode>,
-        right: Box<OngoingTxNode>,
+        left: Box<EventNode>,
+        right: Box<EventNode>,
     },
     Leaf {
         hash: HashOut<F>,
         event: Event<F>,
     },
+}
+
+impl EventNode {
+    fn hash(&self) -> HashOut<F> {
+        match self {
+            Self::Leaf { hash, .. } => *hash,
+            Self::Branch { hash, .. } => *hash,
+        }
+    }
 }
 
 enum OngoingTxNode {
@@ -132,22 +313,22 @@ enum OngoingTxNode {
     },
     ProcessedBranch {
         events: Option<EventNode>,
-        proof: ProgramBranchProof<F, C, D>,
+        proof: ProgramBranchProof,
     },
     ProcessedLeaf {
         events: Option<EventNode>,
-        proof: ProgramLeafProof<F, C, D>,
+        proof: ProgramLeafProof,
     },
     UnprocessedLeaf(ProgramIdentifier),
 }
 
 enum ProcessedTx {
     Leaf {
-        proof: TxLeafProof<F, C, D>,
+        proof: TxLeafProof,
         events: Option<EventNode>,
     },
     Branch {
-        proof: TxBranchProof<F, C, D>,
+        proof: TxBranchProof,
         events: Option<EventNode>,
     },
 }
@@ -186,24 +367,51 @@ impl<'a> TransactionAccumulator<'a> {
             bail!("id {id:?} was not in cast list");
         };
 
+        let events = events.iter().map(|e| Event {
+            owner: id.0.to_u64s().map(F::from_noncanonical_u64),
+            ty: convert_event_type(e.type_),
+            address: u64::from_le_bytes(e.address.0),
+            value: e.value.to_u64s().map(F::from_noncanonical_u64),
+        });
+
+        let event_tree = events
+            .clone()
+            .map(|e| {
+                (BranchAddress::base(e.address), EventNode::Leaf {
+                    hash: e.hash(),
+                    event: e,
+                })
+            })
+            .collect();
+        let event_tree = reduce_tree_by_address(
+            event_tree,
+            |address| address.parent(),
+            |a, l, r| {
+                Ok::<_, Infallible>(EventNode::Branch {
+                    address: *a,
+                    hash: Poseidon2Hash::two_to_one(l.hash(), r.hash()),
+                    left: Box::new(l),
+                    right: Box::new(r),
+                })
+            },
+        )?;
+        let event_tree = event_tree.map(|x| x.1);
+
         // Delay the proof calculation
         let proof = || {
             let event_branch = &self.aux.event_branch_circuit;
-            let events = events.iter().map(|e| {
-                let proof = self.aux.event_leaf_circuit.prove(event_branch, Event {
-                    owner: id.0.to_u64s().map(F::from_noncanonical_u64),
-                    ty: convert_event_type(e.type_),
-                    address: u64::from_le_bytes(e.address.0),
-                    value: e.value.to_u64s().map(F::from_noncanonical_u64),
-                });
-                proof.map(Either::Left)
-            });
-            let event_root_proof = reduce_tree(
+            let events = events
+                .map(|e| {
+                    let proof = self.aux.event_leaf_circuit.prove(event_branch, e)?;
+                    Ok::<_, anyhow::Error>((e.address, Either::Left(proof)))
+                })
+                .try_collect()?;
+
+            let event_root_proof = reduce_tree_by_address(
                 events,
-                |x| x,
-                |x| x,
-                |l, r| {
-                    match (l?, r?) {
+                |address| address / 2,
+                |_, l, r| {
+                    match (l, r) {
                         (Either::Left(l), Either::Left(r)) => event_branch.prove(&l, &r),
                         (Either::Left(l), Either::Right(r)) => event_branch.prove(&l, &r),
                         (Either::Right(l), Either::Left(r)) => event_branch.prove(&l, &r),
@@ -211,10 +419,10 @@ impl<'a> TransactionAccumulator<'a> {
                     }
                     .map(Either::Right)
                 },
-            );
+            )?;
+            let event_root_proof = event_root_proof.map(|x| x.1);
 
             let storage;
-            let event_root_proof = event_root_proof.transpose()?;
             let event_root_proof = match event_root_proof.as_ref() {
                 None => Err(id.0.to_u64s().map(F::from_noncanonical_u64)),
                 Some(Either::Right(v)) => Ok(v),
@@ -244,7 +452,7 @@ impl<'a> TransactionAccumulator<'a> {
                 chain[SIZE..].copy_from_slice(&r);
                 let chain = chain.map(F::from_canonical_u8);
 
-                Plonky2Poseidon2Hash::hash_no_pad(&chain)
+                Poseidon2Hash::hash_no_pad(&chain)
             },
         )
         .unwrap()
@@ -257,16 +465,18 @@ impl<'a> TransactionAccumulator<'a> {
         let tx_entry = match self.ongoing_tx.entry(key) {
             v @ Entry::Vacant(_) => {
                 let proof = proof()?;
-                let cast_list = cast_list.into_iter().enumerate().map(|(i, p)| {
-                    if i == call_index {
+                let cast_list = merge_join_by(
+                    cast_list.into_iter().map(OngoingTxNode::UnprocessedLeaf),
+                    chain(repeat_with(|| None).take(call_index), [Some(
                         OngoingTxNode::ProcessedLeaf {
-                            events: todo!(),
+                            events: event_tree,
                             proof,
-                        }
-                    } else {
-                        OngoingTxNode::UnprocessedLeaf(p)
-                    }
-                });
+                        },
+                    )]),
+                    |_, r| r.is_some(),
+                )
+                .map(|v| v.map_right(Option::unwrap).into_inner());
+
                 let nodes = reduce_tree(
                     cast_list.map(Box::new),
                     |x| *x,
@@ -278,6 +488,7 @@ impl<'a> TransactionAccumulator<'a> {
                 v.insert(OngoingTx { nodes })
             }
             Entry::Occupied(mut o) => {
+                // TODO: fix to use address (MSB for direction)
                 let mut call_index = call_index;
                 let mut node = &mut o.get_mut().nodes;
                 loop {
@@ -293,7 +504,7 @@ impl<'a> TransactionAccumulator<'a> {
                                 bail!("Bad traversal")
                             }
                             *node = OngoingTxNode::ProcessedLeaf {
-                                events: todo!(),
+                                events: event_tree,
                                 proof: proof()?,
                             };
                             break;
@@ -306,8 +517,30 @@ impl<'a> TransactionAccumulator<'a> {
             }
         };
 
+        if let OngoingTxNode::ProcessedBranch { events, proof } = &tx_entry.get().nodes {}
+
         Ok(None)
     }
+}
+#[must_use]
+pub fn reduce_tree_by_address<A: Clone + PartialEq, T, E>(
+    mut i: Vec<(A, T)>,
+    mut a: impl FnMut(A) -> A,
+    mut f: impl FnMut(&A, T, T) -> Result<T, E>,
+) -> Result<Option<(A, T)>, E> {
+    while i.len() > 1 {
+        i = i
+            .into_iter()
+            .chunk_by(|e| e.0.clone())
+            .into_iter()
+            .map(|(address, ts)| {
+                let ts = ts.map(|x| Ok(x.1));
+                let t = reduce_tree(ts, |x| x, |x| x, |l, r| f(&address, l?, r?)).unwrap()?;
+                Ok((a(address), t))
+            })
+            .try_collect()?;
+    }
+    Ok(i.pop())
 }
 
 #[must_use]
