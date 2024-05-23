4e07c776692c3e3b15589f5c39a40d67655f6a27
Daniel-Aaron-Bloom <76709210+Daniel-Aaron-Bloom@users.noreply.github.com>
2024-05-23 10:00:58 -0700

    refactor: move some types around to be reused by driver code (#1726)
    Also fix a comment

--- node/src/lib.rs
+++ node/src/lib.rs
@@ -3,5 +3,11 @@
 #![deny(unsafe_code)]
 #![deny(unused_crate_dependencies)]
 
+use plonky2::plonk::config::{GenericConfig, Poseidon2GoldilocksConfig};
+
 pub mod block_proposer;
 pub mod types;
+
+pub const D: usize = 2;
+pub type C = Poseidon2GoldilocksConfig;
+pub type F = <C as GenericConfig<D>>::F;
