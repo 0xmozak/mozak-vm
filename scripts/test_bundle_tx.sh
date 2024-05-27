5347cf3722093b7c4af27d3774dff62f0338cd12
codeblooded1729 <kapilpause007@gmail.com>
2024-05-27 20:04:10 +0530

    test bundle works!

--- scripts/test_bundle_tx.sh
+++ scripts/test_bundle_tx.sh
@@ -4,19 +4,20 @@
 set -euo pipefail
 
 # Run native executions and build mozakvm binaries
-cd examples/token && cargo run --release \
-    --features="native" \
-    --bin token-native \
-    --target "$(rustc --verbose --version | grep host | awk '{ print $2; }')" &&
-    cargo build --bin tokenbin --release
-
-cd ../wallet && cargo run --release \
-    --features="native" \
-    --bin wallet-native \
-    --target "$(rustc --verbose --version | grep host | awk '{ print $2; }')" &&
-    cargo build --bin walletbin --release
+cd examples/token/native && cargo run --release &&
+    # --features="native" \
+    # --bin token-native \
+    # --target "$(rustc --verbose --version | grep host | awk '{ print $2; }')" &&
+    cd ../mozakvm &&
+    cargo build-mozakvm
+cd examples/wallet/native && cargo run --release &&
+    # --features="native" \
+    # --bin token-native \
+    # --target "$(rustc --verbose --version | grep host | awk '{ print $2; }')" &&
+    cd ../mozakvm &&
+    cargo build-mozakvm
 
 # Run CLI
-cd ../../
+cd ../../../
 cargo run --bin mozak-cli -- bundle-transaction -vvv \
-    --system-tape examples/token/out/tape.json
+    --system-tape examples/token/native/out/tape.json
