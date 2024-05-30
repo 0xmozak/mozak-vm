f84fd12fc10c601e074ad4e12ce64660b310ed6b
codeblooded1729 <kapilpause007@gmail.com>
2024-05-30 20:49:09 +0530

    remove

--- examples/inputtape/mozakvm/script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cargo run --release \
-          --target ../../../buildtarget/riscv32im-mozak-mozakvm-elf.json \
-          -Zbuild-std=alloc,core,compiler_builtins,std,panic_abort,proc_macro \
-          -Zbuild-std-features=compiler-builtins-mem \
