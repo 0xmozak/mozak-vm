3ebd1513c6d340166fe2566a579b850858e830cf
Matthias Goergens <matthias.goergens@gmail.com>
2024-05-30 16:49:27 +0800

    check_bytes and deriving Debug

--- sdk/src/common/types/cross_program_call.rs
+++ sdk/src/common/types/cross_program_call.rs
@@ -1,10 +1,12 @@
 #[derive(
     Default, Clone, Hash, PartialEq, PartialOrd, rkyv::Archive, rkyv::Serialize, rkyv::Deserialize,
 )]
+#[archive(check_bytes)]
 #[cfg_attr(
     not(target_os = "mozakvm"),
     derive(Debug, serde::Serialize, serde::Deserialize)
 )]
+#[archive_attr(derive(Debug))]
 #[allow(clippy::pub_underscore_fields)]
 pub struct CrossProgramCall {
     pub caller: super::ProgramIdentifier,
