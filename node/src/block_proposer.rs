f586e49d4bf8fbb03b1a122ac6e4a3f9baf55396
Daniel-Aaron-Bloom <76709210+Daniel-Aaron-Bloom@users.noreply.github.com>
2024-05-30 00:47:07 -0700

    feat: Add comparison functionality to `BranchAddress` (#1744)
    Add some comparison functionality as well as a few helpers for parents,
    children, and common ancestry to `BranchAddress` for the tx driver
    (#1713)

--- node/src/block_proposer.rs
+++ node/src/block_proposer.rs
@@ -1,3 +1,4 @@
+use std::cmp::Ordering;
 use std::fmt;
 use std::ops::{BitAnd, BitAndAssign, Shl, Sub};
 
@@ -91,6 +92,16 @@ pub struct BranchAddress {
     addr: Address,
 }
 
+impl PartialOrd<BranchAddress> for BranchAddress {
+    fn partial_cmp(&self, other: &BranchAddress) -> Option<Ordering> {
+        if self.height == other.height {
+            Some(Ord::cmp(&self.addr.0, &other.addr.0))
+        } else {
+            None
+        }
+    }
+}
+
 impl BranchAddress {
     /// Initialize the `BranchAddress` to the root node
     #[must_use]
@@ -99,6 +110,31 @@ impl BranchAddress {
             height,
             addr: Address(0),
         }
+    }
+
+    /// Initialize the `BranchAddress` to a leaf node
+    #[must_use]
+    pub fn base(a: u64) -> Self {
+        BranchAddress {
+            height: 0,
+            addr: Address(a),
+        }
+    }
+
+    /// Find the common ancestor between `self` and `rhs`
+    #[must_use]
+    pub fn common_ancestor(mut self, mut rhs: Self) -> Self {
+        // Get both to the same height
+        let d1 = self.height.saturating_sub(rhs.height);
+        let d2 = rhs.height.saturating_sub(self.height);
+        self = self.parent(d2);
+        rhs = rhs.parent(d1);
+
+        // Find where the two diverge by XORing and then taking the MSB position
+        let ancestor_diff = u64::BITS - (self.addr.0 ^ rhs.addr.0).leading_zeros();
+        self = self.parent(ancestor_diff as usize);
+        debug_assert_eq!(self, rhs.parent(ancestor_diff as usize));
+        self
     }
 
     /// Move downward, adding a `0|1` bit based on the dir (`Left|Right`).
@@ -115,5 +151,243 @@ impl BranchAddress {
             self.height -= 1;
             Ok(self)
         }
+    }
+
+    /// Move upward, removing the bottom `n` bits.
+    /// If we've reached the bottom, return an `Address` instead
+    #[must_use]
+    pub fn parent(mut self, n: usize) -> Self {
+        self.addr.0 >>= n;
+        self.height += n;
+        self
+    }
+
+    #[must_use]
+    pub fn compare(self, rhs: Self) -> BranchAddressComparison {
+        let (parent, child) = match self.height.cmp(&rhs.height) {
+            // LHS and RHS are at the same level
+            Ordering::Equal => {
+                // Check if LHS and RHS have the same parent
+                let lhs_msb = self.addr.0 >> 1;
+                let rhs_msb = rhs.addr.0 >> 1;
+                match lhs_msb.cmp(&rhs_msb) {
+                    Ordering::Less => return BranchAddressComparison::RightCousin,
+                    Ordering::Equal => {}
+                    Ordering::Greater => return BranchAddressComparison::LeftCousin,
+                }
+
+                // Compare the final direction of LHS and RHS
+                let lhs_lsb = self.addr.0 & 1;
+                let rhs_lsb = rhs.addr.0 & 1;
+                return match lhs_lsb.cmp(&rhs_lsb) {
+                    Ordering::Less => BranchAddressComparison::RightSibling,
+                    Ordering::Equal => BranchAddressComparison::Equal,
+                    Ordering::Greater => BranchAddressComparison::LeftSibling,
+                };
+            }
+            // LHS is a child of RHS
+            Ordering::Less => (rhs, self),
+            // RHS is a child of LHS
+            Ordering::Greater => (self, rhs),
+        };
+
+        let lhs_is_parent = parent == self;
+
+        // Check if child actually descends from parent
+        let delta = parent.height - child.height;
+        let child_addr = child.addr.0 >> delta;
+        match (child_addr.cmp(&parent.addr.0), lhs_is_parent) {
+            (Ordering::Less, false) | (Ordering::Greater, true) =>
+                return BranchAddressComparison::RightCousin,
+            (Ordering::Greater, false) | (Ordering::Less, true) =>
+                return BranchAddressComparison::LeftCousin,
+            (Ordering::Equal, _) => {}
+        }
+
+        let addr = Address(child.addr.0 & ((1 << delta) - 1));
+        match (addr.next(delta - 1).1, lhs_is_parent) {
+            (Dir::Left, true) => BranchAddressComparison::LeftChild,
+            (Dir::Right, true) => BranchAddressComparison::RightChild,
+            (Dir::Left, false) => BranchAddressComparison::LeftParent,
+            (Dir::Right, false) => BranchAddressComparison::RightParent,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum BranchAddressComparison {
+    /// The LHS and RHS addresses are the same
+    Equal,
+
+    /// The RHS address is a left-child of the LHS address
+    LeftChild,
+    /// The RHS address is a right-child of the LHS address
+    RightChild,
+
+    /// The RHS address is a left-sibling of the LHS address
+    LeftSibling,
+    /// The RHS address is a right-sibling of the LHS address
+    RightSibling,
+
+    /// The RHS address is a left-parent of the LHS address
+    LeftParent,
+    /// The RHS address is a right-parent of the LHS address
+    RightParent,
+
+    /// The RHS address is a cousin somewhere to the left of the LHS address
+    LeftCousin,
+    /// The RHS address is a cousin somewhere to the right of the LHS address
+    RightCousin,
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_common_ancestor() {
+        let dirs = [Dir::Left, Dir::Right];
+        let parent = BranchAddress::root(10);
+        let children = dirs.map(|d| parent.child(d).unwrap());
+        let grandchildren = children.map(|c| dirs.map(|d| c.child(d).unwrap()));
+        let great_grandchildren =
+            grandchildren.map(|c| c.map(|c| dirs.map(|d| c.child(d).unwrap())));
+
+        let a = great_grandchildren[0][0][0];
+        let b = great_grandchildren[0][0][0];
+        let c = great_grandchildren[0][0][0];
+        assert_eq!(a.common_ancestor(b), c);
+        assert_eq!(b.common_ancestor(a), c);
+
+        let a = great_grandchildren[0][0][0];
+        let b = great_grandchildren[0][0][1];
+        let c = grandchildren[0][0];
+        assert_eq!(a.common_ancestor(b), c);
+        assert_eq!(b.common_ancestor(a), c);
+
+        let a = great_grandchildren[0][0][0];
+        let b = great_grandchildren[0][1][1];
+        let c = children[0];
+        assert_eq!(a.common_ancestor(b), c);
+        assert_eq!(b.common_ancestor(a), c);
+
+        let a = great_grandchildren[0][0][0];
+        let b = great_grandchildren[1][0][0];
+        let c = parent;
+        assert_eq!(a.common_ancestor(b), c);
+        assert_eq!(b.common_ancestor(a), c);
+    }
+
+    #[test]
+    fn test_branch_compare() {
+        let dirs = [Dir::Left, Dir::Right];
+        let parent = BranchAddress::root(10);
+        let children = dirs.map(|d| parent.child(d).unwrap());
+        let grandchildren = children.map(|c| dirs.map(|d| c.child(d).unwrap()));
+        let great_grandchildren =
+            grandchildren.map(|c| c.map(|c| dirs.map(|d| c.child(d).unwrap())));
+
+        // Test all self equality
+        assert_eq!(parent.compare(parent), BranchAddressComparison::Equal);
+        for c in children {
+            assert_eq!(c.compare(c), BranchAddressComparison::Equal);
+        }
+        for c in grandchildren.into_iter().flatten() {
+            assert_eq!(c.compare(c), BranchAddressComparison::Equal);
+        }
+        for c in great_grandchildren.into_iter().flatten().flatten() {
+            assert_eq!(c.compare(c), BranchAddressComparison::Equal);
+        }
+
+        // Parent LHS
+        assert_eq!(
+            parent.compare(children[0]),
+            BranchAddressComparison::LeftChild
+        );
+        assert_eq!(
+            parent.compare(children[1]),
+            BranchAddressComparison::RightChild
+        );
+        for c in grandchildren[0] {
+            assert_eq!(parent.compare(c), BranchAddressComparison::LeftChild);
+        }
+        for c in grandchildren[1] {
+            assert_eq!(parent.compare(c), BranchAddressComparison::RightChild);
+        }
+        for c in great_grandchildren[0].into_iter().flatten() {
+            assert_eq!(parent.compare(c), BranchAddressComparison::LeftChild);
+        }
+        for c in great_grandchildren[1].into_iter().flatten() {
+            assert_eq!(parent.compare(c), BranchAddressComparison::RightChild);
+        }
+
+        // children[0] LHS
+        assert_eq!(
+            children[0].compare(parent),
+            BranchAddressComparison::LeftParent
+        );
+        assert_eq!(
+            children[0].compare(children[1]),
+            BranchAddressComparison::RightSibling
+        );
+        assert_eq!(
+            children[0].compare(grandchildren[0][0]),
+            BranchAddressComparison::LeftChild
+        );
+        assert_eq!(
+            children[0].compare(grandchildren[0][1]),
+            BranchAddressComparison::RightChild
+        );
+        for c in grandchildren[1] {
+            assert_eq!(children[0].compare(c), BranchAddressComparison::RightCousin);
+        }
+        for c in great_grandchildren[0][0] {
+            assert_eq!(children[0].compare(c), BranchAddressComparison::LeftChild);
+        }
+        for c in great_grandchildren[0][1] {
+            assert_eq!(children[0].compare(c), BranchAddressComparison::RightChild);
+        }
+        for c in great_grandchildren[1].into_iter().flatten() {
+            assert_eq!(children[0].compare(c), BranchAddressComparison::RightCousin);
+        }
+
+        // children[1] LHS
+        assert_eq!(
+            children[1].compare(parent),
+            BranchAddressComparison::RightParent
+        );
+        assert_eq!(
+            children[1].compare(children[0]),
+            BranchAddressComparison::LeftSibling
+        );
+        assert_eq!(
+            children[1].compare(grandchildren[1][0]),
+            BranchAddressComparison::LeftChild
+        );
+        assert_eq!(
+            children[1].compare(grandchildren[1][1]),
+            BranchAddressComparison::RightChild
+        );
+        for c in grandchildren[0] {
+            assert_eq!(children[1].compare(c), BranchAddressComparison::LeftCousin);
+        }
+        for c in great_grandchildren[1][0] {
+            assert_eq!(children[1].compare(c), BranchAddressComparison::LeftChild);
+        }
+        for c in great_grandchildren[1][1] {
+            assert_eq!(children[1].compare(c), BranchAddressComparison::RightChild);
+        }
+        for c in great_grandchildren[0].into_iter().flatten() {
+            assert_eq!(children[1].compare(c), BranchAddressComparison::LeftCousin);
+        }
+
+        assert_eq!(
+            grandchildren[0][1].compare(parent),
+            BranchAddressComparison::LeftParent
+        );
+        assert_eq!(
+            grandchildren[0][1].compare(children[1]),
+            BranchAddressComparison::RightCousin
+        );
     }
 }
