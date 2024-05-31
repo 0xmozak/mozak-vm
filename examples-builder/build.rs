26b5bfb44cc82b932a14d9eec05ca24b4b979d25
Matthias Goergens <matthias.goergens@gmail.com>
2024-05-31 14:23:30 +0800

    Build.rs

--- examples-builder/build.rs
+++ examples-builder/build.rs
@@ -8,14 +8,13 @@ struct Crate {
     elf_path: &'static str,
     glob_name: &'static str,
     enabled: bool,
-    uses_std: bool,
 }
 
 macro_rules! ecrate {
-    ($name:literal, $glob:literal, $uses_std:expr) => {
-        ecrate!($name, $name, $glob, $uses_std)
+    ($name:literal, $glob:literal) => {
+        ecrate!($name, $name, $glob)
     };
-    ($name:literal, $file:literal, $glob:literal, $uses_std:expr) => {
+    ($name:literal, $file:literal, $glob:literal) => {
         Crate {
             crate_path: concat!("../examples/", $name),
             elf_path: concat!(
@@ -24,39 +23,34 @@ macro_rules! ecrate {
             ),
             glob_name: $glob,
             enabled: cfg!(feature = $name),
-            uses_std: $uses_std,
         }
     };
 }
 
 const CRATES: &[Crate] = &[
-    ecrate!("bss-tester", "BSS_ELF", false),
-    ecrate!("fibonacci", "FIBONACCI_ELF", false),
-    ecrate!("memory-access", "MEMORY_ACCESS_ELF", false),
-    ecrate!("min-max", "MIN_MAX_ELF", false),
-    ecrate!("panic", "PANIC_ELF", false),
-    ecrate!("rkyv-serialization", "RKYV_SERIALIZATION_ELF", false),
-    ecrate!("sha2", "SHA2_ELF", false),
-    ecrate!("static-mem-access", "STATIC_MEM_ACCESS_ELF", false),
-    ecrate!("empty", "EMPTY_ELF", false),
-    ecrate!("mozak-sort", "MOZAK_SORT_ELF", false),
-    ecrate!("tokenbin", "TOKENBIN", false),
-    ecrate!("walletbin", "WALLETBIN", false),
-    ecrate!("inputtapebin", "INPUTTAPEBIN", false),
+    ecrate!("bss-tester", "BSS_ELF"),
+    ecrate!("fibonacci", "FIBONACCI_ELF"),
+    ecrate!("memory-access", "MEMORY_ACCESS_ELF"),
+    ecrate!("min-max", "MIN_MAX_ELF"),
+    ecrate!("panic", "PANIC_ELF"),
+    ecrate!("rkyv-serialization", "RKYV_SERIALIZATION_ELF"),
+    ecrate!("sha2", "SHA2_ELF"),
+    ecrate!("static-mem-access", "STATIC_MEM_ACCESS_ELF"),
+    ecrate!("empty", "EMPTY_ELF"),
+    ecrate!("mozak-sort", "MOZAK_SORT_ELF"),
+    ecrate!("tokenbin", "TOKENBIN"),
+    ecrate!("walletbin", "WALLETBIN"),
+    ecrate!("inputtapebin", "INPUTTAPEBIN"),
 ];
 const CARGO_MANIFEST_DIR: &str = env!("CARGO_MANIFEST_DIR");
 
-fn build_elf(dest: &mut File, crate_path: &str, elf_path: &str, glob_name: &str, uses_std: bool) {
+fn build_elf(dest: &mut File, crate_path: &str, elf_path: &str, glob_name: &str) {
     // Use a dummy array for clippy, since not building the elf is faster than
     // building the elf
     if cfg!(feature = "cargo-clippy") {
         writeln!(dest, r#"pub const {glob_name}: &[u8] = &[];"#)
     } else {
-        let args = if uses_std {
-            vec!["build", "--release", "--features=std"]
-        } else {
-            vec!["build", "--release"]
-        };
+        let args = vec!["build", "--release"];
         let output = Command::new("cargo")
             .args(args)
             .current_dir(crate_path)
@@ -90,7 +84,7 @@ fn main() {
 
     for c in CRATES {
         if c.enabled {
-            build_elf(&mut dest, c.crate_path, c.elf_path, c.glob_name, c.uses_std)
+            build_elf(&mut dest, c.crate_path, c.elf_path, c.glob_name)
         }
     }
 }
