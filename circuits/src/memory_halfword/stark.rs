fe43ff12f263951ef0fb0428de8557bf8160d08e
Matthias Goergens <matthias.goergens@gmail.com>
2024-05-31 16:03:53 +0800

    Convert more

--- circuits/src/memory_halfword/stark.rs
+++ circuits/src/memory_halfword/stark.rs
@@ -12,7 +12,7 @@ use starky::evaluation_frame::StarkFrame;
 use starky::stark::Stark;
 
 use crate::columns_view::HasNamedColumns;
-use crate::expr::{build_ext, build_packed, ConstraintBuilder};
+use crate::expr::{build_ext, build_packed, ConstraintBuilder, GenerateConstraints};
 use crate::memory_halfword::columns::{HalfWordMemory, NUM_HW_MEM_COLS};
 use crate::unstark::NoColumns;
 
@@ -26,26 +26,31 @@ impl<F, const D: usize> HasNamedColumns for HalfWordMemoryStark<F, D> {
     type Columns = HalfWordMemory<F>;
 }
 
-// Design description - https://docs.google.com/presentation/d/1J0BJd49BMQh3UR5TrOhe3k67plHxnohFtFVrMpDJ1oc/edit?usp=sharing
-fn generate_constraints<'a, T: Copy>(
-    vars: &StarkFrameTyped<HalfWordMemory<Expr<'a, T>>, NoColumns<Expr<'a, T>>>,
-) -> ConstraintBuilder<Expr<'a, T>> {
-    let lv = vars.local_values;
-    let mut constraints = ConstraintBuilder::default();
+impl<'a, F, T: Copy, U, const D: usize>
+    GenerateConstraints<'a, T, U, HalfWordMemory<Expr<'a, T>>, NoColumns<U>>
+    for HalfWordMemoryStark<F, { D }>
+{
+    // Design description - https://docs.google.com/presentation/d/1J0BJd49BMQh3UR5TrOhe3k67plHxnohFtFVrMpDJ1oc/edit?usp=sharing
+    fn generate_constraints(
+        vars: &StarkFrameTyped<HalfWordMemory<Expr<'a, T>>, NoColumns<U>>,
+    ) -> ConstraintBuilder<Expr<'a, T>> {
+        let lv = vars.local_values;
+        let mut constraints = ConstraintBuilder::default();
 
-    constraints.always(lv.ops.is_store.is_binary());
-    constraints.always(lv.ops.is_load.is_binary());
-    constraints.always(lv.is_executed().is_binary());
+        constraints.always(lv.ops.is_store.is_binary());
+        constraints.always(lv.ops.is_load.is_binary());
+        constraints.always(lv.is_executed().is_binary());
 
-    let added = lv.addrs[0] + 1;
-    let wrapped = added - (1 << 32);
+        let added = lv.addrs[0] + 1;
+        let wrapped = added - (1 << 32);
 
-    // Check: the resulting sum is wrapped if necessary.
-    // As the result is range checked, this make the choice deterministic,
-    // even for a malicious prover.
-    constraints.always(lv.is_executed() * (lv.addrs[1] - added) * (lv.addrs[1] - wrapped));
+        // Check: the resulting sum is wrapped if necessary.
+        // As the result is range checked, this make the choice deterministic,
+        // even for a malicious prover.
+        constraints.always(lv.is_executed() * (lv.addrs[1] - added) * (lv.addrs[1] - wrapped));
 
-    constraints
+        constraints
+    }
 }
 
 const COLUMNS: usize = NUM_HW_MEM_COLS;
@@ -67,7 +72,7 @@ impl<F: RichField + Extendable<D>, const D: usize> Stark<F, D> for HalfWordMemor
         FE: FieldExtension<D2, BaseField = F>,
         P: PackedField<Scalar = FE>, {
         let eb = ExprBuilder::default();
-        let constraints = generate_constraints(&eb.to_typed_starkframe(vars));
+        let constraints = Self::generate_constraints(&eb.to_typed_starkframe(vars));
         build_packed(constraints, consumer);
     }
 
@@ -78,7 +83,7 @@ impl<F: RichField + Extendable<D>, const D: usize> Stark<F, D> for HalfWordMemor
         consumer: &mut RecursiveConstraintConsumer<F, D>,
     ) {
         let eb = ExprBuilder::default();
-        let constraints = generate_constraints(&eb.to_typed_starkframe(vars));
+        let constraints = Self::generate_constraints(&eb.to_typed_starkframe(vars));
         build_ext(constraints, builder, consumer);
     }
 
