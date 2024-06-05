a313c73b6340dbc2298836ffa71b83d479308bc9
Matthias Goergens <matthias.goergens@gmail.com>
2024-06-05 16:12:58 +0800

    Remove indirection

--- examples/vector-alloc/main.rs
+++ examples/vector-alloc/main.rs
@@ -5,7 +5,7 @@ use core::hint::black_box;
 
 use mozak_sdk::core::ecall::ioread_public;
 
-fn alloc_me() {
+fn main() {
     let n = {
         let mut bytes = [0u8; 4];
         ioread_public(&mut bytes);
@@ -13,11 +13,6 @@ fn alloc_me() {
     };
 
     let _v: Vec<u32> = black_box(vec![0; n as usize]);
-}
-
-fn main() {
-    #[cfg(target_os = "mozakvm")]
-    alloc_me();
 }
 
 mozak_sdk::entry!(main);
