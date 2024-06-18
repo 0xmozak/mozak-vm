40a4d373fa6d0a92f7f427d9a5352c347f19c305
Jakub Zalewski <jakub.zalewski@mozak.com>
2024-06-18 14:15:46 +0200

    Generalise NoColumns to ShadowColumns
    Adds ShadowColumns<T, N> which can hold N columns, so that NoColumns<T> is redefined as ShadowColumns<T, 0>.
    
    Removes the previous hack on FromIterator.
    
    Unfortunately, needed to manually expand columns_view_impl! macro for ShadowColumns.

--- circuits/src/unstark.rs
+++ circuits/src/unstark.rs
@@ -12,17 +12,17 @@ use starky::constraint_consumer::{ConstraintConsumer, RecursiveConstraintConsume
 use starky::evaluation_frame::StarkFrame;
 use starky::stark::Stark;
 
-use crate::columns_view::{columns_view_impl, HasNamedColumns, NumberOfColumns};
+use crate::columns_view::{HasNamedColumns, NumberOfColumns};
 use crate::expr::{ConstraintBuilder, GenerateConstraints};
 
 impl<'a, F, NAME, T: 'a, const D: usize, Columns, const COLUMNS: usize> GenerateConstraints<'a, T>
     for Unstark<F, NAME, { D }, Columns, { COLUMNS }>
 {
     type PublicInputs<E: 'a> = NoColumns<E>;
-    type View<E: 'a> = NoColumns<E>;
+    type View<E: 'a> = ShadowColumns<E, { COLUMNS }>;
 
     fn generate_constraints(
-        _vars: &StarkFrameTyped<NoColumns<Expr<'a, T>>, NoColumns<Expr<'a, T>>>,
+        _vars: &StarkFrameTyped<ShadowColumns<Expr<'a, T>, { COLUMNS }>, NoColumns<Expr<'a, T>>>,
     ) -> ConstraintBuilder<Expr<'a, T>> {
         ConstraintBuilder::default()
     }
@@ -105,9 +105,180 @@ macro_rules! impl_name {
 
 pub(crate) use impl_name;
 
+pub type NoColumns<T> = ShadowColumns<T, 0>;
+
 #[repr(C)]
 #[derive(Clone, Copy, Eq, PartialEq, Debug)]
-pub struct NoColumns<T> {
-    _phantom: PhantomData<T>,
+pub struct ShadowColumns<T, const N: usize> {
+    _columns: [T; N],
 }
-columns_view_impl!(NoColumns);
+
+// Manually implement columns_view_impl! for Columns
+impl<T, const N: usize> crate::columns_view::ColumnViewImplHider<ShadowColumns<T, N>> {
+    const fn from_array(value: [T; N]) -> ShadowColumns<T, N> {
+        unsafe { crate::columns_view::transmute_without_compile_time_size_checks(value) }
+    }
+
+    const fn into_array(v: ShadowColumns<T, N>) -> [T; N] {
+        unsafe { crate::columns_view::transmute_without_compile_time_size_checks(v) }
+    }
+
+    const fn from_array_ref(value: &[T; N]) -> &ShadowColumns<T, N> {
+        unsafe { crate::columns_view::transmute_ref(value) }
+    }
+
+    const fn array_ref(v: &ShadowColumns<T, N>) -> &[T; N] {
+        unsafe { crate::columns_view::transmute_ref(v) }
+    }
+}
+
+impl<T, const N: usize> ShadowColumns<T, N> {
+    pub const fn from_array(value: [T; N]) -> Self {
+        crate::columns_view::ColumnViewImplHider::<Self>::from_array(value)
+    }
+
+    #[must_use]
+    pub const fn into_array(self) -> [T; N] {
+        crate::columns_view::ColumnViewImplHider::<Self>::into_array(self)
+    }
+
+    pub const fn from_array_ref(value: &[T; N]) -> &Self {
+        crate::columns_view::ColumnViewImplHider::<Self>::from_array_ref(value)
+    }
+
+    #[must_use]
+    pub const fn array_ref(&self) -> &[T; N] {
+        crate::columns_view::ColumnViewImplHider::<Self>::array_ref(self)
+    }
+
+    pub fn iter(&self) -> std::slice::Iter<T> { self.array_ref().into_iter() }
+
+    // At the moment we only use `map` Instruction,
+    // so it's dead code for the other callers of `columns_view_impl`.
+    // TODO(Matthias): remove this marker, once we use it for the other structs,
+    // too.
+    #[allow(dead_code)]
+    pub fn map<B, F>(self, f: F) -> ShadowColumns<B, N>
+    where
+        F: FnMut(T) -> B, {
+        ShadowColumns::from_array(self.into_array().map(f))
+    }
+}
+
+impl<Item, const N: usize> crate::columns_view::Zip<Item> for ShadowColumns<Item, N> {
+    fn zip_with<F>(self, other: Self, mut f: F) -> Self
+    where
+        F: FnMut(Item, Item) -> Item, {
+        ShadowColumns::from_array({
+            let mut a = self.into_iter();
+            let mut b = other.into_iter();
+            core::array::from_fn(move |_| f(a.next().unwrap(), b.next().unwrap()))
+        })
+    }
+}
+
+impl<T, const N: usize> crate::columns_view::NumberOfColumns for ShadowColumns<T, N> {
+    // `u8` is guaranteed to have a `size_of` of 1.
+    const NUMBER_OF_COLUMNS: usize = N;
+}
+
+impl<T, const N: usize> From<[T; N]> for ShadowColumns<T, N> {
+    fn from(value: [T; N]) -> Self { Self::from_array(value) }
+}
+
+impl<T, const N: usize> From<ShadowColumns<T, N>> for [T; N] {
+    fn from(value: ShadowColumns<T, N>) -> Self { value.into_array() }
+}
+
+impl<'a, T, const N: usize> From<&'a [T]> for &'a ShadowColumns<T, N> {
+    fn from(value: &'a [T]) -> Self {
+        let value: &[T; N] =
+            value.try_into().expect("slice of correct length");
+        ShadowColumns::from_array_ref(value)
+    }
+}
+
+impl<T, const N: usize> std::borrow::Borrow<[T]> for ShadowColumns<T, N> {
+    fn borrow(&self) -> &[T] { self.array_ref() }
+}
+
+impl<T, I, const N: usize> std::ops::Index<I> for ShadowColumns<T, N>
+where
+    [T]: std::ops::Index<I>,
+{
+    type Output = <[T] as std::ops::Index<I>>::Output;
+
+    fn index(&self, index: I) -> &Self::Output { &self.array_ref()[index] }
+}
+
+impl<T, const N: usize> std::iter::IntoIterator for ShadowColumns<T, N> {
+    type IntoIter = std::array::IntoIter<T, { N }>;
+    type Item = T;
+
+    fn into_iter(self) -> Self::IntoIter { self.into_array().into_iter() }
+}
+
+impl<'a, T, const N: usize> std::iter::IntoIterator for &'a ShadowColumns<T, N> {
+    type IntoIter = std::slice::Iter<'a, T>;
+    type Item = &'a T;
+
+    fn into_iter(self) -> Self::IntoIter { self.iter() }
+}
+
+impl<T: std::fmt::Debug, const N: usize> std::iter::FromIterator<T> for ShadowColumns<T, N> {
+    fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
+        let vec: arrayvec::ArrayVec<T, N> = iter.into_iter().collect();
+        let array = vec.into_inner().expect("iterator of correct length");
+        Self::from_array(array)
+    }
+}
+
+impl<const N: usize> core::ops::Neg for ShadowColumns<i64, N> {
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        self.map(|x| x.checked_neg().expect("negation overflow"))
+    }
+}
+
+impl<const N: usize> core::ops::Add<ShadowColumns<i64, N>> for ShadowColumns<i64, N> {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self::Output {
+        crate::columns_view::Zip::zip_with(self, other, |a, b| {
+            a.checked_add(b).expect("addition overflow")
+        })
+    }
+}
+
+impl<const N: usize> core::ops::Sub<ShadowColumns<i64, N>> for ShadowColumns<i64, N> {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self::Output {
+        crate::columns_view::Zip::zip_with(self, other, |a, b| {
+            a.checked_sub(b).expect("subtraction overflow")
+        })
+    }
+}
+
+impl<const N: usize> core::ops::Mul<i64> for ShadowColumns<i64, N> {
+    type Output = Self;
+
+    fn mul(self, other: i64) -> Self::Output {
+        self.map(|x| x.checked_mul(other).expect("multiplication overflow"))
+    }
+}
+
+impl<const N: usize> core::iter::Sum<ShadowColumns<i64, N>> for ShadowColumns<i64, N> {
+    #[inline]
+    fn sum<I: Iterator<Item = Self>>(iter: I) -> Self {
+        iter.fold(Self::default(), core::ops::Add::add)
+    }
+}
+
+// Some of our tables have columns that are specifiecd as arrays that are bigger
+// than 32 elements.  Thus default derivation doesn't work, so we do it manually
+// here.
+impl<F: Default, const N: usize> Default for ShadowColumns<F, N> {
+    fn default() -> Self { ShadowColumns::from_array(core::array::from_fn(|_| Default::default())) }
+}
