81a281abe1650622d9c7017fc352feefd0b52e10
codeblooded1729 <kapilpause007@gmail.com>
2024-06-19 15:50:31 +0530

    clippy: happy

--- sdk/src/core/trace.rs
+++ sdk/src/core/trace.rs
@@ -4,16 +4,22 @@ pub struct Tracer {
     enabled: bool,
 }
 
-pub static mut GLOBAL_TRACER: Lazy<Tracer> = Lazy::new(|| Tracer::new());
+pub static mut GLOBAL_TRACER: Lazy<Tracer> = Lazy::new(Tracer::new);
+
+impl Default for Tracer {
+    fn default() -> Self { Self::new() }
+}
 
 impl Tracer {
+    #[must_use]
     pub fn new() -> Self { Self { enabled: false } }
 
+    #[must_use]
     pub fn is_enabled(&self) -> bool { self.enabled }
 
     pub fn trace<F>(&mut self, code: F)
     where
-        F: FnOnce() -> (), {
+        F: FnOnce(), {
         // enable the tracer, effectively enabling the `trace` ecalls,
         // if any, which are present in `code`
         self.enabled = true;
