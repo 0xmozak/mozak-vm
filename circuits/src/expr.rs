715e41dd64d4ab7de7c30ff2bac8561a8e4dc9ff
Jakub Zalewski <jakub.zalewski@mozak.com>
2024-06-21 12:08:00 +0200

    Remove passthrough to G from StarkFrom

--- circuits/src/expr.rs
+++ circuits/src/expr.rs
@@ -231,23 +231,6 @@ pub struct StarkFrom<F, G, const D: usize, const COLUMNS: usize, const PUBLIC_IN
     pub _f: PhantomData<F>,
 }
 
-// Passthrough to G
-impl<F, G, const D: usize, const COLUMNS: usize, const PUBLIC_INPUTS: usize>
-    GenerateConstraints<COLUMNS, PUBLIC_INPUTS> for StarkFrom<F, G, D, COLUMNS, PUBLIC_INPUTS>
-where
-    G: GenerateConstraints<COLUMNS, PUBLIC_INPUTS>,
-{
-    type PublicInputs<E: Debug> = G::PublicInputs<E>;
-    type View<E: Debug> = G::View<E>;
-
-    fn generate_constraints<'a, T: Copy + Debug>(
-        &self,
-        vars: &Vars<'a, Self, T, COLUMNS, PUBLIC_INPUTS>,
-    ) -> ConstraintBuilder<Expr<'a, T>> {
-        self.witness.generate_constraints(vars)
-    }
-}
-
 impl<G: Display, F, const D: usize, const COLUMNS: usize, const PUBLIC_INPUTS: usize> Display
     for StarkFrom<F, G, D, COLUMNS, PUBLIC_INPUTS>
 {
